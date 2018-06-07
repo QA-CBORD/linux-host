10563ba6edac7de860f074b9250efbf8d7d89f8c
Ben Pantzis <bsp@cbord.com>
2018-06-07 11:06:53 -0400

    HTML and CSS cleanup

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,5 +1,7 @@
 export class Environment {
     public static environment: string = "development";
+    
+    // public static servicesBaseURL: string = "https://services.get.dev.cbord.com";
     public static servicesBaseURL: string = "http://services.get-dev.rd.cbord.com/GETServices/services";
     public static fullSiteBaseURL: string = "http://get-dev.rd.cbord.com/"
 }
