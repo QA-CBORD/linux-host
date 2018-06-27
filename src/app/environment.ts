101ac981ab5a852c3f49d03e3c42c5bbb798c96d
Ben Pantzis <bsp@cbord.com>
2018-06-27 15:37:16 -0400

    Removed old url from comments

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -3,8 +3,4 @@ export class Environment {
     
     public static servicesBaseURL: string = "https://services.get.dev.cbord.com/GETServices/services";
     public static fullSiteBaseURL: string = "http://get.dev.cbord.com/"
-
-    // old non-aws urls
-    // public static servicesBaseURL: string = "http://services.get-dev.rd.cbord.com/GETServices/services";
-    // public static fullSiteBaseURL: string = "http://get-dev.rd.cbord.com/"
 }
