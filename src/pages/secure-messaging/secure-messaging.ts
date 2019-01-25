0d9a36e564bf6ca3eafd10f3412114f1cbb771af
Ben Pantzis <bsp@cbord.com>
2019-01-25 11:15:28 -0500

    Fix to merge mistakes

--- src/pages/secure-messaging/secure-messaging.ts
+++ src/pages/secure-messaging/secure-messaging.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, Platform, Events, Modal, ModalController, ModalOptions, ToastController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, NavController, NavParams, Platform, Events, Modal, ModalController, ModalOptions, ToastController, Content, Scroll } from 'ionic-angular';
+
+import { fromEvent } from "rxjs/observable/fromEvent";
+import { Subscription } from "rxjs/Subscription";
 
 import * as Globals from '../../app/app.global'
 
 import { SecureMessageInfo, SecureMessageGroupInfo, SecureMessageConversation } from '../../models/secure-messaging/secure-message-info';
 import { SecureMessagingProvider } from '../../providers/secure-messaging-provider/secure-messaging-provider';
-
 
 @IonicPage({
   name: 'secure-messaging',
@@ -35,6 +37,13 @@ export class SecureMessagingPage {
   groups: SecureMessageGroupInfo[] = new Array();
   messages: SecureMessageInfo[] = new Array();
 
+
+  @ViewChild('chatScroll') chatScroll: any;
+  resizeSubscription: Subscription;
+  bIsLargeScreen: boolean = false;
+  selectedConversation: SecureMessageConversation;
+  newMessageText: string;
+
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
@@ -42,21 +51,41 @@ export class SecureMessagingPage {
     private platform: Platform,
     private events: Events,
     private toast: ToastController,
-    private secureMessageProvider: SecureMessagingProvider
+    private secureMessagingProvider: SecureMessagingProvider
   ) {
 
     platform.ready().then(() => {
       /// hide the split pane here becuase we don't need the navigation menu
       events.publish(Globals.Events.SIDEPANE_ENABLE, false);
+      this.bIsLargeScreen = this.platform.width() > 768;
     });
 
   }
 
+  ngOnDestroy() {
+    if (this.resizeSubscription != null) {
+      this.resizeSubscription.unsubscribe();
+    }
+  }
+
   ionViewDidLoad() {
+    this.resizeSubscription = fromEvent(window, 'resize')
+      .subscribe(event => {
+        let bWasPreviouslyLargeScreen = this.bIsLargeScreen;
+        this.bIsLargeScreen = window.innerWidth >= 768;
+        if (!bWasPreviouslyLargeScreen && this.bIsLargeScreen) {
+          this.scrollToBottom();
+        }
+      });
     this.loadInitialData();
   }
 
   ionViewWillEnter() {
+
+    if (this.bIsLargeScreen) {
+      return;
+    }
+
     let updatedConversation: SecureMessageConversation = this.navParams.get('updatedConversation') || null;
 
     if (updatedConversation) {
@@ -69,22 +98,10 @@ export class SecureMessagingPage {
     }
   }
 
-
-  /// USE THIS AS MY PATRON
-  // id: "045b5348-64c8-40a0-a7f4-c08501217418",
-  // type: "patron",
-  // id_field: "IDNumber",
-  // id_value: "Patron01",
-  // aux_user_id: null,
-  // name: "test user",
-  // created_date: null,
-  // version: 1
-
-
   private loadInitialData() {
     this.pageState = this.LOADING;
 
-    this.secureMessageProvider.getInitialData()
+    this.secureMessagingProvider.getInitialData()
       .subscribe(
         ([smGroupArray, smMessageArray]) => {
           console.log("GetInitialData Response:");
@@ -108,10 +125,7 @@ export class SecureMessagingPage {
         }
       );
 
-
   }
-
-
 
   private handleInitialDataResponse() {
 
@@ -165,6 +179,10 @@ export class SecureMessagingPage {
         this.conversations.push(conversation);
       }
 
+      if (this.conversations.length > 0) {
+        this.selectedConversation = this.conversations[0];
+      }
+
     }
 
     console.log("CONVOS:");
@@ -172,21 +190,111 @@ export class SecureMessagingPage {
     console.log(this.conversations);
 
     if (this.conversations.length > 0) {
+      console.log(`hIDR large screen: ${this.bIsLargeScreen}`);
+      
+      if (this.bIsLargeScreen) {        
+        this.scrollToBottom();
+      }
       this.pageState = this.SHOW_CONVOS;
     } else {
       this.pageState = this.START_CONVO;
     }
 
 
-
   }
 
+  public scrollToBottom() {
+    console.log("Scroll to bottom");
+    setTimeout(() => {
+      let scroll = this.chatScroll._scrollContent.nativeElement;
+      scroll.scrollTop = scroll.scrollHeight - scroll.clientHeight;
+    }, 100);
+  }
 
   onConversationClick(conversation: SecureMessageConversation) {
     console.log("onConversationClick")
 
+    if (this.bIsLargeScreen) {
+      this.selectedConversation = conversation;
+      this.scrollToBottom();
+      return;
+    }
+
     this.openConversationPage(conversation);
 
+  }
+
+  onSendMessageClick() {
+    if (this.newMessageText && this.newMessageText.length > 0) {
+      this.scrollToBottom();
+      this.sendTestMessage();
+    }
+  }
+
+  sendTestMessage() {
+
+    let NewMessage = {
+      institution_id: this.selectedConversation.institutionId,
+      sender: {
+        type: "patron",
+        id_field: SecureMessagingProvider.GetSMAuthInfo().id_field,
+        id_value: SecureMessagingProvider.GetSMAuthInfo().id_value,
+        name: ""
+      },
+      recipient: {
+        type: "group",
+        id_value: this.selectedConversation.groupIdValue,
+        name: this.selectedConversation.groupName
+      },
+      description: "",
+      body: this.newMessageText,
+      importance: null,
+    };
+
+    this.secureMessagingProvider.sendSecureMessage(NewMessage)
+      .subscribe(
+        data => {
+          console.log("Send Message data:");
+          console.log(data);
+          this.addLocalDataToConversation();
+        },
+        error => {
+          console.log("Send Message error:");
+          console.log(error);
+          this.toast.create({
+            message: "Error sending the message",
+            duration: 3000,
+            position: 'bottom'
+          }).present();
+        },
+        () => {
+
+        }
+      );
+  }
+
+  addLocalDataToConversation() {
+    let message: SecureMessageInfo = {
+      body: this.newMessageText,
+      created_date: new Date().toLocaleString(),
+      description: "",
+      id: null,
+      importance: null,
+      institution_id: "29db894b-aecd-4cef-b515-15b0405614d7",
+      read_date: null,
+      recipient: { created_date: "2018-11-29T14:32:29.475889", id: "4af3cfd3-8efb-4383-ab20-509df4bb4023", type: "group", id_field: null, id_value: this.selectedConversation.groupIdValue, name: this.selectedConversation.groupName, aux_user_id: null, version: 1 },
+      replied_message_id: "None",
+      requires_read_receipt: null,
+      sender: { created_date: "2018-11-29T14:32:29.475889", id: "045b5348-64c8-40a0-a7f4-c08501217418", type: "patron", id_field: SecureMessagingProvider.GetSMAuthInfo().id_field, id_value: SecureMessagingProvider.GetSMAuthInfo().id_value, name: "", aux_user_id: null, version: 1 },
+      sent_date: new Date().toLocaleString(),
+      state: null,
+      ttl: null,
+      version: 1
+    }
+
+    this.newMessageText = null;
+    this.selectedConversation.messages.push(message);
+    this.scrollToBottom();
   }
 
   onAddConversationFABClick() {
@@ -238,7 +346,12 @@ export class SecureMessagingPage {
           };
         }
 
-        this.openConversationPage(newConversation);
+        if (!this.bIsLargeScreen) {
+          this.openConversationPage(newConversation);
+        } else {
+          this.selectedConversation = newConversation;
+        }
+
       }
     });
 
