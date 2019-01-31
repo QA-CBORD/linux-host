2abbb4ed2dd17dd74b28d136a3be99f2058bbb65
Ben Pantzis <bsp@cbord.com>
2019-01-31 11:55:40 -0500

    Fixed bug caused by change to lazy loading page names

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -140,13 +140,10 @@ export class HomePage {
 
     switch (this.destinationPage) {
       case 'rewards':
-        this.navCtrl.push("RewardsPage");
+        this.navCtrl.push("rewards");
         break;
       case 'openmydoor':
-        this.navCtrl.push("MobileAccessPage", this.geoData);
-        break;
-      case 'accounts':
-        this.navCtrl.push("AccountsPage");
+        this.navCtrl.push("mobile-access", this.geoData);
         break;
       case 'securemessaging':
         this.navCtrl.push("secure-messaging")
