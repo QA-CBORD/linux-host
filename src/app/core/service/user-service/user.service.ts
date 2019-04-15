9722bb8967027dd6c83a9f68de9894f4317d8738
Alex <lexakorh@gmail.com>
2019-04-15 10:21:02 +0300

    -- rewrite Observable.create methods to the plain Observables.

--- src/app/core/service/user-service/user.service.ts
+++ src/app/core/service/user-service/user.service.ts
@@ -1,42 +1,26 @@
 import { Injectable } from '@angular/core';
-
-import { Observable } from 'rxjs';
-
 import { BaseService, ServiceParameters } from '../base-service/base.service';
-
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { MUserInfo } from 'src/app/core/model/user/user-info.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService extends BaseService {
 
   private serviceUrl = '/json/user';
-
-
+  
   /**
    * Get the current User information using the current Session Id
    */
   getUser(): Observable<MUserInfo> {
+    const postParams: ServiceParameters = {};
 
-    return Observable.create((observer: any) => {
-
-      const postParams: ServiceParameters = {};
-
-      this.httpRequest(this.serviceUrl, 'retrieve', true, postParams)
-        .subscribe(
-          data => {
-            // validate data then throw error or send
-            observer.next(data.response);
-            observer.complete();
-          },
-          error => {
-            // do error stuff then push it to observer
-            observer.error(error);
-          }
-        );
-    });
-
+    return this.httpRequest(this.serviceUrl, 'retrieve', true, postParams)
+      .pipe(
+        map((data) => data.response),
+      );
   }
 
 }
