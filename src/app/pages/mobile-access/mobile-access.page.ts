99d409f55cd38d3246dbd4277da737cca58a8bc3
Ben Pantzis <bsp@cbord.com>
2019-04-25 08:22:04 -0400

    Build Issues: Member variables had incorrect access levels

--- src/app/pages/mobile-access/mobile-access.page.ts
+++ src/app/pages/mobile-access/mobile-access.page.ts
@@ -24,7 +24,7 @@ export class MobileAccessPage implements OnDestroy, OnInit, AfterViewInit {
   private readonly searchString$: BehaviorSubject<string> = new BehaviorSubject<string>('');
   private readonly toastDuration: number = 1000;
   private currentCoords: MGeoCoordinates;
-  private tempTitle: string = 'Mobile Access';
+  tempTitle: string = 'Mobile Access';
   locations: MMobileLocationInfo[];
   userInfo$: Observable<MUserInfo>;
 
