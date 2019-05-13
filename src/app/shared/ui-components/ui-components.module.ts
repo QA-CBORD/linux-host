06045ba1aae66b2317430d596003b1a93115fd41
Oleg Pryatko <oxp@cbord.com>
2019-05-13 15:35:58 +0300

    feature/GCS-730: -- progress bar done;

--- src/app/shared/ui-components/ui-components.module.ts
+++ src/app/shared/ui-components/ui-components.module.ts
@@ -5,8 +5,15 @@ import { StHeaderComponent } from './st-header/st-header.component';
 import { StSpinnerComponent } from './st-spinner/st-spinner.component';
 import { StCountdownComponent } from './st-countdown/st-countdown.component';
 import { StPopoverLayoutComponent } from './st-popover-layout/st-popover-layout.component';
+import { StProgressBarComponent } from './st-progress-bar/st-progress-bar.component';
 
-const components = [StHeaderComponent, StSpinnerComponent, StCountdownComponent, StPopoverLayoutComponent];
+const components = [
+  StHeaderComponent,
+  StSpinnerComponent,
+  StCountdownComponent,
+  StPopoverLayoutComponent,
+  StProgressBarComponent,
+];
 
 @NgModule({
   imports: [CommonModule, IonicModule],
