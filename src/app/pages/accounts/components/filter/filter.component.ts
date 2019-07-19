75e56e8dbec6937f66d400ad6d3acf3feda639ea
Oleg Pryatko <oxp@cbord.com>
2019-07-19 14:41:13 +0300

    feature/GCS-1034: -- init component;

--- src/app/pages/accounts/components/filter/filter.component.ts
+++ src/app/pages/accounts/components/filter/filter.component.ts
@@ -1,12 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { AccountsService } from '../../services/accounts.service';
 
 @Component({
   selector: 'st-filter',
   templateUrl: './filter.component.html',
   styleUrls: ['./filter.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FilterComponent implements OnInit {
-  constructor() {}
+  accountName: string = 'All accounts';
+  timeRange: string = 'Past 6 month';
+  availibleMonth: string[];
+
+  constructor(private readonly accountsService: AccountsService) {}
 
   ngOnInit() {}
+
+  setTimeRange(date?: { month: string; year: string }) {
+    const date1 = date
+      ? date
+      : {
+          year: new Date().getFullYear(),
+          month: new Date().getMonth(),
+        };
+  }
 }
