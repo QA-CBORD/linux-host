5ee2e4aaf2734e4bf1f5d39e5de142fe1163dfd3
Ben Pantzis <bsp@cbord.com>
2019-08-01 11:32:21 -0400

    Incomplete config addition

--- src/app/pages/accounts/accounts.config.ts
+++ src/app/pages/accounts/accounts.config.ts
@@ -1,3 +1,5 @@
+import * as Globals from '../../app.global';
+
 export const SYSTEM_SETTINGS_CONFIG = {
   enableOnetimeDeposits: {
     domain: 'get',
@@ -58,3 +60,56 @@ export enum TIME_PERIOD {
   pastMonth = 'past_month',
   pastSixMonth = 'past_six_month',
 }
+
+export enum CONTENT_STRINGS {
+  activateBtn = 'button_activate',
+  optInToast = 'toast_opt-in-success',
+  cancelBtn = 'button_cancel',
+  closeBtn = 'button_close',
+  retryBtn = 'button_retry',
+  retryTitle = 'dialog_header_retry',
+  headerTitle = 'header_title',
+  optInBtn = 'button_opt-in',
+  optInFailLabel = 'label_opt-in-failed',
+  levelTabTitle = 'tab_title_levels',
+  storeTabTitle = 'tab_title_store',
+  historyTabTitle = 'tab_title_history',
+  xpAwayFromRewardLabel = 'label_xp-to-unlock',
+  activeRewardLabel = 'label_active-reward',
+  rewardClaimedLabel = 'label_reward-claimed',
+  claimRewardLabel = 'dialog_header_claim-reward',
+  noOffersLabel = 'label_no-offers-available',
+  balanceLabel = 'label_balance',
+  pointsLabel = 'label_points',
+  levelLabel = 'label_level',
+  pointsCostLabel = 'label_point-cost',
+  scanLabel = 'label_scan',
+  redeemLabel = 'label_redeem',
+  claimLabel = 'label_claim',
+  claimedLabel = 'label_claimed',
+  claimButton = 'dialog_button_claim',
+  redeemButton = 'dialog_button_redeem',
+  successTitle = 'dialog_header_success',
+  claimTitle = 'dialog_header_claim-reward',
+  redeemTitle = 'dialog_header_redeem-reward',
+  scanCodeTitle = 'dialog_header_scan-code',
+  scanCodeDescription = 'dialog_description_scan-code',
+  activeRewardsLabel = 'label_active-reward-plural',
+  claimInstructionsLabel = 'label_claim-instructions',
+  emptyHistoryListMessage = '',
+}
+
+export const ContentStringsParamsAccounts = {
+  category: Globals.ContentString.CATEGORIES.accounts,
+  domain: Globals.ContentString.DOMAINS.patron,
+};
+
+export const ContentStringsParamsTransactions = {
+  category: Globals.ContentString.CATEGORIES.transactions,
+  domain: Globals.ContentString.DOMAINS.patron,
+};
+
+export const GenericContentStringsParams = {
+  category: Globals.ContentString.CATEGORIES.core,
+  domain: Globals.ContentString.DOMAINS.patron,
+};
