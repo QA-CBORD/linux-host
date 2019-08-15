4da35b7159b7be30ea756ed51df5ca19f0f09084
Ben Pantzis <bsp@cbord.com>
2019-08-15 13:49:18 -0400

    Peer review fixes and use of debug removed

--- src/app/pages/accounts/shared/ui-components/filter/pipes/time-range.pipe.ts
+++ src/app/pages/accounts/shared/ui-components/filter/pipes/time-range.pipe.ts
@@ -7,30 +7,19 @@ import { CONTENT_STRINGS, TIME_PERIOD } from 'src/app/pages/accounts/accounts.co
   name: 'timeRange',
 })
 export class TimeRangePipe implements PipeTransform {
-  private static contentString: { [key: string]: string };
 
   constructor(private readonly transactionsService: TransactionService) {
   }
 
-  transform(value: DateUtilObject): string {
-    this.setContentStrings();
-    return this.localGetUniquePeriodName(value);
+  transform(value: DateUtilObject, contentString: any): string {
+    console.log(contentString);
+    
+    return this.localGetUniquePeriodName(value, contentString);
   }
 
-  private localGetUniquePeriodName(date: DateUtilObject): string {
+  private localGetUniquePeriodName(date: DateUtilObject, contentString: any): string {
     return date.name === TIME_PERIOD.pastSixMonth || date.name === TIME_PERIOD.pastMonth
-      ? TimeRangePipe.contentString[CONTENT_STRINGS.pastSixMonthsLabel]
+      ? contentString[CONTENT_STRINGS.pastSixMonthsLabel]
       : `${date.name} ${date.year}`;
-  }
-
-  setContentStrings() {
-    if(TimeRangePipe.contentString) return;
-    const transactionStringNames: string[] = [
-      CONTENT_STRINGS.allAccountsLabel,
-      CONTENT_STRINGS.pastSixMonthsLabel,
-      CONTENT_STRINGS.filterLabel,
-    ];
-
-    TimeRangePipe.contentString = this.transactionsService.getContentStrings(transactionStringNames);
   }
 }
