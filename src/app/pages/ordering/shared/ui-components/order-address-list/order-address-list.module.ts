d1282402448b5fb40044c55780f9612f90482b67
Jacob Hargrave <jsh4@cbord.com>
2019-09-18 09:21:22 -0400

    Name spacing imports via the tsconfig.json

--- src/app/pages/ordering/shared/ui-components/order-address-list/order-address-list.module.ts
+++ src/app/pages/ordering/shared/ui-components/order-address-list/order-address-list.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 
-import { SharedModule } from '../../../../../shared/shared.module';
+import { SharedModule } from '@shared/shared.module';
 import { MerchantDistanceModule } from '../../pipes/merchant-distance/merchant-distance.module';
 import { OrderAddressListComponent } from './order-address-list.component';
 import { OrderAddressItemComponent } from './order-address-item';
