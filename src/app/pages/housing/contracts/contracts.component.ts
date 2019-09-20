59a7822dd21c940c0985e51ad061cbc8454d26ad
Sergiy Gurko <sergey.gurko.sbit@gmail.com>
2019-09-20 13:42:32 +0300

    feature/RC-1511: -- Refactoring Contracts component.

--- src/app/pages/housing/contracts/contracts.component.ts
+++ src/app/pages/housing/contracts/contracts.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ContractsService } from './contracts.service';
+
 @Component({
   selector: 'st-contracts',
   templateUrl: './contracts.component.html',
   styleUrls: ['./contracts.component.scss'],
 })
 export class ContractsComponent implements OnInit {
-  constructor() {}
+  constructor(private _contractsService: ContractsService) {}
+
+  contracts: any[] = [];
 
   ngOnInit() {}
 }
--- src/app/pages/housing/pages/housing-dashboard/housing-dashboard.page.ts
+++ src/app/pages/housing/pages/housing-dashboard/housing-dashboard.page.ts
@@ -1,17 +1,8 @@
 import { Component } from '@angular/core';
 
-import { ContractsService } from '../../contracts/contracts.service';
-
 @Component({
   selector: 'st-housing-dashboard',
   templateUrl: './housing-dashboard.page.html',
   styleUrls: ['./housing-dashboard.page.scss'],
 })
-export class HousingDashboardPage {
-  // contractsList: Contracts[] = []
-  constructor(private contractServ: ContractsService) {}
-
-  ngOnInit() {
-    // this.contractsList = this.contractServ.GetContractsFromRescenter(1);
-  }
-}
+export class HousingDashboardPage {}
