c48905029545403c606bd78b34c35e2d9d8d53dd
Sergiy Gurko <sergey.gurko.sbit@gmail.com>
2019-09-20 10:28:31 +0300

    feature/RC-1511: -- Moved code from UnitsView to UnitDetails and removed UnitsView.

--- src/app/pages/housing/pages/unit-details/unit-details.page.ts
+++ src/app/pages/housing/pages/unit-details/unit-details.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UnitslistService } from '../../services/unitslist.service';
+import { UnitsList } from '../../Models/units-list';
 
 @Component({
   selector: 'st-unit-details',
@@ -6,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./unit-details.page.scss'],
 })
 export class UnitDetailsPage implements OnInit {
+  constructor(private activatedRoute: ActivatedRoute,
+              private ucs: UnitslistService) { }
 
-  constructor() { }
+  facilityId: number;
+  unitsList: UnitsList[];
 
   ngOnInit() {
+    this.facilityId = parseInt(this.activatedRoute.snapshot.paramMap.get('facilityId'), 10);
+    this.unitsList = this.ucs.GetUnitsListForFacility(this.facilityId);
   }
 
 }
