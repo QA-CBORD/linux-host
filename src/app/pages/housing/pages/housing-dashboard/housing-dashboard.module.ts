06dd6a451fcb2e7a1f4c7a1c6b1d0957fb00315e
Sergiy Gurko <sergey.gurko.sbit@gmail.com>
2019-09-20 13:36:26 +0300

    feature/RC-1511: -- Refactoring Contracts and WorkOrders components.

--- src/app/pages/housing/pages/housing-dashboard/housing-dashboard.module.ts
+++ src/app/pages/housing/pages/housing-dashboard/housing-dashboard.module.ts
@@ -6,8 +6,8 @@ import { IonicModule } from '@ionic/angular';
 import { HousingDashboardRoutingModule } from './housing-dashboard.routing.module';
 
 import { HousingDashboardPage } from './housing-dashboard.page';
-import { ContractsComponent } from './contracts/contracts.component';
-import { WorkOrdersComponent } from './work-orders/work-orders.component';
+import { ContractsComponent } from '../../contracts/contracts.component';
+import { WorkOrdersComponent } from '../../work-orders/work-orders.component';
 import { ApplicationsComponent } from '../../applications/applications.component';
 
 const imports = [CommonModule, ReactiveFormsModule, IonicModule, HousingDashboardRoutingModule,];
