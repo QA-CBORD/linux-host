d633ef5a970a8ac6c7fbc7fe4e3cb5c64a6e1118
Jacob Hargrave <jsh4@cbord.com>
2019-09-24 21:17:12 -0400

    Ordering addresses page in modal (will change it later to remove from modal).

--- src/app/pages/ordering/shared/ui-components/order-address-list/order-address-item/order-address-item.component.ts
+++ src/app/pages/ordering/shared/ui-components/order-address-list/order-address-item/order-address-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'st-order-address-item',
@@ -6,9 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-address-item.component.scss'],
 })
 export class OrderAddressItemComponent implements OnInit {
+  @Input() line1: string;
+  @Input() line2: string;
+  @Input() iconAlt: string;
+  @Input() isDefault: boolean;
+  @Input() isSelected: boolean;
+  @Input() iconIsFile: boolean;
+  @Input() iconNameDefault: string = 'star-outline.svg';
+  @Input() iconNameSelected: string = 'star-filled.svg';
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    if(this.iconNameDefault.includes('.svg') && !this.isSelected 
+    || this.iconNameSelected.includes('.svg') && this.isSelected){
+      this.iconIsFile = true;
+    }
+  }
 
+  itemSelected(){
+    
+  }
+  get iconSrc(): string {
+    if(this.iconIsFile){
+      const name = (this.isSelected && this.iconNameSelected) ? this.iconNameSelected : this.iconNameDefault;
+      return `./assets/icon/${name}`;
+    }else{
+      return '';
+    }
+  }
+
+  get iconName(): string {
+    if(!this.iconIsFile){
+      return (this.isSelected && this.iconNameSelected) ? this.iconNameSelected : this.iconNameDefault;
+    }else{
+      return '';
+    }
+  }
+  
+  
 }
--- src/app/pages/ordering/shared/ui-components/order-address-list/order-address-list.component.ts
+++ src/app/pages/ordering/shared/ui-components/order-address-list/order-address-list.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { UserService } from '@core/service/user-service/user.service';
+import { AddressInfo } from '@core/model/user/user-address.model';
+import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
+import { NgZone  } from '@angular/core';
 
 @Component({
   selector: 'st-order-address-list',
@@ -6,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-address-list.component.scss'],
 })
 export class OrderAddressListComponent implements OnInit {
+  
+  userAddresses$: Observable<AddressInfo[]>;
+  
+  constructor(private userService: UserService, private zone: NgZone, private ref: ChangeDetectorRef) { }
 
-  constructor() { }
-
-  ngOnInit() {}
-
+  items:AddressInfo[];
+  ngOnInit() {
+    this.userAddresses$ = this.userService.getUserAddresses()
+    this.userAddresses$
+      .pipe(take(1))
+      .subscribe(
+        (data) => {
+          this.items = data;
+          console.log('addresses', this.items);
+          // debugger;
+          /* this.zone.run(() => {
+            console.log('force update the screen');
+          }); */
+          this.ref.detectChanges();
+        },
+        (e) => {
+          console.log('error', e);
+        }
+      );
+  }
+  itemSelected(){
+    
+  }
 }
