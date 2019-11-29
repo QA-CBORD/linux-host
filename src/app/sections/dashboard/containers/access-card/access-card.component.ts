0b865f6d86af6f095e77ecc843fb876518fbcbe3
Alex <lexakorh@gmail.com>
2019-11-29 11:23:30 +0200

    feature/GCS-1415_1: fix user avatar

--- src/app/sections/dashboard/containers/access-card/access-card.component.ts
+++ src/app/sections/dashboard/containers/access-card/access-card.component.ts
@@ -2,7 +2,7 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { Component, OnInit, Input } from '@angular/core';
 
 import { Observable } from 'rxjs';
-import { map, startWith } from 'rxjs/operators';
+import { map, startWith, tap, first } from 'rxjs/operators';
 
 import { AccessCardService } from './services/access-card.service';
 
@@ -13,22 +13,28 @@ import { AccessCardService } from './services/access-card.service';
 })
 export class AccessCardComponent implements OnInit {
   userName$: Observable<string>;
-  userPhoto$: Observable<string>;
+  userPhoto$: Promise<string>;
   institutionName$: Observable<string>;
   institutionPhoto$: Observable<SafeResourceUrl>;
   institutionBackgroundImage$: Observable<string>;
   getMyCardEnabled$: Observable<boolean>;
   applePayEnabled$: Observable<boolean>;
-  defaultAvatarUrl = '../../../../../assets/images/no_photo.svg';
+  isLoadingImg: boolean;
   @Input() isMobileAccessButtonEnabled: boolean;
 
-  constructor(private readonly accessCardService: AccessCardService, private readonly sanitizer: DomSanitizer) {}
+  constructor(private readonly accessCardService: AccessCardService, private readonly sanitizer: DomSanitizer) { }
 
   ngOnInit() {
-    this.userName$ = this.accessCardService.getUserName();
-    this.userPhoto$ = this.accessCardService.getUserPhoto().pipe(startWith(this.defaultAvatarUrl));
+    this.getUserData();
     this.setInstitutionData();
     this.getFeaturesEnabled();
+  }
+
+  private getUserData() {
+    this.isLoadingImg = true;
+    this.userName$ = this.accessCardService.getUserName();
+    this.userPhoto$ = this.accessCardService.getUserPhoto().pipe(first())
+      .toPromise().finally(() => this.isLoadingImg = !this.isLoadingImg);
   }
 
   private setInstitutionData() {
