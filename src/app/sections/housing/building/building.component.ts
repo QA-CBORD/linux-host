69c34318d8017e5ee548b5fcdf9fa995e32c2bab
Serhii Hurko <serhii_hurko@epam.com>
2020-01-21 14:01:37 +0200

    feature/RC-2577: Added markup for building component.

--- src/app/sections/housing/building/building.component.ts
+++ src/app/sections/housing/building/building.component.ts
@@ -1,14 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'st-building',
   templateUrl: './building.component.html',
   styleUrls: ['./building.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BuildingComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {}
-
-}
+export class BuildingComponent {}
