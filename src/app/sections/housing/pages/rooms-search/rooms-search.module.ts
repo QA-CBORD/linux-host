3320c2b6d1eedba98dd7b87282c90eff33481eaf
Serhii Hurko <serhii_hurko@epam.com>
2020-01-27 10:35:42 +0200

    feature/RC-2573: Added back button.

--- src/app/sections/housing/pages/rooms-search/rooms-search.module.ts
+++ src/app/sections/housing/pages/rooms-search/rooms-search.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 
+import { BackButtonModule } from '@shared/ui-components/back-button/back-button.module';
 import { RoomsFilterModule } from './rooms-filter/rooms-filter.module';
 import { RoomsSearchRoutingModule } from './rooms-search.routing.module';
 
 import { RoomsSearchPage } from './rooms-search.page';
 
-const imports = [CommonModule, IonicModule, RoomsFilterModule, RoomsSearchRoutingModule];
+const imports = [CommonModule, IonicModule, BackButtonModule, RoomsFilterModule, RoomsSearchRoutingModule];
 const declarations = [RoomsSearchPage];
 
 @NgModule({
---
+++ src/app/shared/ui-components/back-button/back-button.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'st-back-button',
+  templateUrl: './back-button.component.html',
+  styleUrls: ['./back-button.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class BackButtonComponent {}
