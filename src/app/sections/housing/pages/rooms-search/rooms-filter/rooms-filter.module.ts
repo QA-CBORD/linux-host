3bbd088c4468725b8a2ac555f86391b343b4fc7e
Serhii Hurko <serhii_hurko@epam.com>
2020-01-28 12:19:05 +0200

    feature/RC-2573: Created separate PageTitle component.

--- src/app/sections/housing/pages/rooms-search/rooms-filter/rooms-filter.module.ts
+++ src/app/sections/housing/pages/rooms-search/rooms-filter/rooms-filter.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 
+import { PageTitleModule } from '@sections/housing/page-title/page-title.module';
+
 import { RoomsFilterComponent } from './rooms-filter.component';
 import { RoomsFilterModalComponent } from './rooms-filter-modal/rooms-filter-modal.component';
 
-export const imports = [CommonModule, IonicModule];
+export const imports = [CommonModule, IonicModule, PageTitleModule];
 export const declarations = [RoomsFilterComponent, RoomsFilterModalComponent];
 export const entryComponents = [RoomsFilterModalComponent];
 
--- src/app/sections/housing/pages/rooms-search/rooms-search.module.ts
+++ src/app/sections/housing/pages/rooms-search/rooms-search.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 
+import { PageTitleModule } from '@sections/housing/page-title/page-title.module';
 import { BackButtonModule } from '@shared/ui-components/back-button/back-button.module';
 import { RoomsFilterModule } from './rooms-filter/rooms-filter.module';
 import { RoomsSearchRoutingModule } from './rooms-search.routing.module';
 
 import { RoomsSearchPage } from './rooms-search.page';
 
-const imports = [CommonModule, IonicModule, BackButtonModule, RoomsFilterModule, RoomsSearchRoutingModule];
+const imports = [
+  CommonModule,
+  IonicModule,
+  PageTitleModule,
+  BackButtonModule,
+  RoomsFilterModule,
+  RoomsSearchRoutingModule,
+];
 const declarations = [RoomsSearchPage];
 
 @NgModule({
