2bdfeb0a45628ef244883bc84d8cc14dc679a939
Serhii Hurko <serhii_hurko@epam.com>
2020-01-28 15:10:42 +0200

    feature/RC-2573: Renamed forlder from rooms-filter to search-filter.

--- src/app/sections/housing/pages/rooms-search/rooms-search.module.ts
+++ src/app/sections/housing/pages/rooms-search/rooms-search.module.ts
@@ -5,7 +5,7 @@ import { IonicModule } from '@ionic/angular';
 import { PageTitleModule } from '@sections/housing/page-title/page-title.module';
 import { BackButtonModule } from '@shared/ui-components/back-button/back-button.module';
 import { RoomsSwitchModule } from '../../rooms-switch/rooms-switch.module';
-import { RoomsFilterModule } from '../../rooms-filter/rooms-filter.module';
+import { RoomsFilterModule } from '../../search-filter/rooms-filter.module';
 import { RoomsSearchRoutingModule } from './rooms-search.routing.module';
 
 import { RoomsSearchPage } from './rooms-search.page';
