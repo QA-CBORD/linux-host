da885c3328aef494df493ad400b001204b0201e1
Serhii Hurko <serhii_hurko@epam.com>
2020-01-29 11:42:34 +0200

    feature/RC-2576: Generated empty buildings component.

---
+++ src/app/sections/housing/empty-buildings/empty-buildings.component.ts
@@ -0,0 +1,9 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'st-empty-buildings',
+  templateUrl: './empty-buildings.component.html',
+  styleUrls: ['./empty-buildings.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class EmptyBuildingsComponent {}
