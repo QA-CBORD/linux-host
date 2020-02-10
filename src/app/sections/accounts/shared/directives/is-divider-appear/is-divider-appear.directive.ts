8cdd73ba93a1a497f554ca580095bb3788ddb0c1
Oleksii Korkh <oxk@cbord.com>
2020-02-10 08:14:43 +0000

    Squashed commit of the following
    commit b1e037ae 
    Author: Alex <lexakorh@gmail.com> 
    Date: Fri Feb 07 2020 16:31:26 GMT+0200 (Eastern European Standard Time) 
    
        bugfix/GCS-1546: Transactions for the same period are separated on history screen

--- src/app/sections/accounts/shared/directives/is-divider-appear/is-divider-appear.directive.ts
+++ src/app/sections/accounts/shared/directives/is-divider-appear/is-divider-appear.directive.ts
@@ -1,6 +1,7 @@
 import { Directive, OnInit, Input, ElementRef, Renderer2 } from '@angular/core';
 import { TransactionHistory } from '@sections/accounts/models/transaction-history.model';
 import { isSameDay } from '@core/utils/date-helper';
+import { TIMEZONE_REGEXP } from '@core/utils/regexp-patterns';
 
 @Directive({
   selector: '[stIsDividerAppear]',
@@ -10,7 +11,7 @@ export class IsDividerAppearDirective implements OnInit {
   @Input() index: number;
   @Input() transactions: TransactionHistory[];
 
-  constructor(private elem: ElementRef, private renderer: Renderer2) {}
+  constructor(private elem: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
     this.renderer.setStyle(
@@ -21,6 +22,13 @@ export class IsDividerAppearDirective implements OnInit {
   }
 
   private isDividerAppear(actualDate, i, transactions): boolean {
-    return i === 0 || !isSameDay(actualDate, transactions[i - 1].actualDate);
+    return i === 0 || !isSameDay(
+      this.formatDate(actualDate),
+      this.formatDate(transactions[i - 1].actualDate)
+    );
+  }
+
+  private formatDate(date) {
+    return date.replace(TIMEZONE_REGEXP, "$1:$2");
   }
 }
