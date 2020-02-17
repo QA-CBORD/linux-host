c9d21a60b0998c5eedf7328b5cdbb89116845840
Alex <lexakorh@gmail.com>
2020-02-17 13:51:24 +0200

    bugfix/GCS-1621: refactor trackBy

--- src/app/sections/accounts/pages/deposit-page/deposit-page.component.ts
+++ src/app/sections/accounts/pages/deposit-page/deposit-page.component.ts
@@ -389,7 +389,7 @@ export class DepositPageComponent implements OnInit, OnDestroy {
   }
 
   trackByAccountId(i: number, { id }: UserAccount): string {
-    return `${i}-${id}-${Math.random()}`;
+    return `${i}-${Math.random()}`;
   }
 
   private resetControls(controlNames: string[]) {
