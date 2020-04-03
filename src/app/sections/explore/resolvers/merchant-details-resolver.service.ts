dad79defa92f3c821a3106aa23b4242403fe9ac4
Andriy Biletskyy <axb@cbord.com>
2020-04-03 14:13:10 +0300

    mirror fix

--- src/app/sections/explore/resolvers/merchant-details-resolver.service.ts
+++ src/app/sections/explore/resolvers/merchant-details-resolver.service.ts
@@ -3,24 +3,15 @@ import { Resolve, Router } from '@angular/router';
 import { Observable, of } from 'rxjs';
 import { ExploreService } from '@sections/explore/services/explore.service';
 import { NAVIGATE } from '../../../app.global';
-import { map, finalize } from 'rxjs/operators';
-import { LoadingService } from '@core/service/loading/loading.service';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class MerchantDetailsResolverService implements Resolve<boolean> {
-  constructor(
-    private readonly exploreService: ExploreService,
-    private readonly router: Router,
-    private readonly loadingService: LoadingService
-  ) {}
+  constructor(private readonly exploreService: ExploreService, private readonly router: Router) {}
 
   resolve(): Observable<boolean> {
-    this.loadingService.showSpinner();
     return this.router.routerState.snapshot.url.includes(NAVIGATE.explore)
       ? of(true)
-      : this.exploreService.getInitialMerchantData$().pipe(
-          map(() => true),
-          finalize(() => this.loadingService.closeSpinner())
-        );
+      : this.exploreService.getInitialMerchantData$().pipe(map(() => true));
   }
 }
