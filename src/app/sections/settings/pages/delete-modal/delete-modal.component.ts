78283358f65b8405315e029e1ace7690c20460e6
John Liscar <johnliscar@Johns-MacBook-Pro.local>
2020-07-15 20:49:51 -0400

    getting photolist which has no photos yet, will set if there is photos, functions for submitting pictures added, but want to look at some more, function for deleting picture built

--- src/app/sections/settings/pages/delete-modal/delete-modal.component.ts
+++ src/app/sections/settings/pages/delete-modal/delete-modal.component.ts
@@ -11,13 +11,15 @@ export class DeleteModalComponent implements OnInit {
 
   ngOnInit() {}
 
-  async dismissModal(){
-    await this.modalController.dismiss();
+  async dismissModal(data: boolean){
+    await this.modalController.dismiss({
+      'delete': data
+    });
   }
 
   deletePhoto(){
     alert('This is where the api call will delete photo from DB and then eventually update the photo upload page accordingly');
-    this.dismissModal();
+    this.dismissModal(true);
   }
 
 }
--- src/app/sections/settings/pages/photo-upload/photo-upload.component.ts
+++ src/app/sections/settings/pages/photo-upload/photo-upload.component.ts
@@ -20,13 +20,17 @@ const { Camera } = Plugins;
   styleUrls: ['./photo-upload.component.scss'],
 })
 export class PhotoUploadComponent implements OnInit {
-  photoList: any;
   frontId: SafeResourceUrl = null;
+  frontIdPhotoInfo: UserPhotoInfo;
   backId: SafeResourceUrl = null;
+  backIdPhotoInfo: UserPhotoInfo;
   selfie: SafeResourceUrl = null;
-  //this will change according to if the have photos or not from DB api call
+  selfiePhotoInfo: UserPhotoInfo;
   submitted: boolean = false;
+  userId: any;
+  photoList: any;
   userPhoto: string;
+  photos: any;
 
   constructor(
     private readonly router: Router,
@@ -35,37 +39,72 @@ export class PhotoUploadComponent implements OnInit {
     private readonly identityFacadeService: IdentityFacadeService,
     private readonly toastController: ToastController,
     private readonly userFacadeService: UserFacadeService
-  ) {}
+  ) { }
 
   ngOnInit() {
-    //call institution settings for photos
-    this.setUserPhoto();
+    //call institution settings for photos 
+    //keeps throwing an error
+    // this.setUserPhoto();
     this.getPhotoList();
   }
 
   //sets the user photo varibale if there is a photo
-  private setUserPhoto() {
-    this.userFacadeService
-      .getAcceptedPhoto$()
-      .pipe(
-        map(({ data, mimeType }) => `data:${mimeType};base64,${data}`),
-        take(1)
-      )
-      .subscribe((url: string) => {
-        this.userPhoto = url;
-      });
-    console.log('user photo', this.userPhoto);
-  }
-
-  private setIDPhotos() {}
+  // private setUserPhoto() {
+  //   this.userFacadeService
+  //     .getAcceptedPhoto$()
+  //     .pipe(
+  //       map(({ data, mimeType }) => `data:${mimeType};base64,${data}`),
+  //       take(1)
+  //     )
+  //     .subscribe(
+  //       url => {
+  //         this.selfie = url;
+  //         console.log('userPhoto', this.selfie);
+  //       },
+  //       error => console.log('get User Photo error', error),
+  //       () => {
+  //         console.log('get User Photo Complete')
+  //       }
+  //     );
+  // }
 
   //gets the photolist by user id
   getPhotoList() {
-    this.userFacadeService
-      .getPhotoList()
-      .subscribe(list => (this.photoList = list));
-    console.log('photoList', this.photoList);
+    this.userFacadeService.getPhotoList().subscribe(
+      list => {
+        console.log('getPhotoList next', list);
+        this.photoList = list;
+      },
+      error => console.log('getPhotoList error', error),
+      () => {
+        console.log('getPhotoList complete')
+        this.sortPhotos();
+      }
+    );
   }
+
+  //sorts through the available photos and sets them appropriately
+  sortPhotos() {
+    var index;
+    for (index = 0; index < this.photoList.length; index++) {
+      if (this.photoList[index].type === 0 && this.photoList[index].status === 1 && this.photoList[index].data !== null) {
+        console.log('photo has a type of 0', this.photoList[index]);
+        this.selfiePhotoInfo = this.photoList[index];
+        this.selfie = this.domsanitizer.bypassSecurityTrustResourceUrl(this.selfiePhotoInfo.data);
+      }
+      if (this.photoList[index].type === 1 && this.photoList[index].status === 1 && this.photoList[index].data !== null) {
+        console.log('photo has a type of 1', this.photoList[index]);
+        this.frontIdPhotoInfo = this.photoList[index] ;
+        this.frontId = this.domsanitizer.bypassSecurityTrustResourceUrl(this.frontIdPhotoInfo.data);
+      }
+      if (this.photoList[index].type === 2 && this.photoList[index].status === 1 && this.photoList[index].data !== null) {
+        console.log('photo has a type of 2', this.photoList[index]);
+        this.backIdPhotoInfo = this.photoList[index];
+        this.backId = this.domsanitizer.bypassSecurityTrustResourceUrl(this.backIdPhotoInfo.data);
+      }
+    }
+  }
+
 
   //prompts to open camera or photos for front id pic
   async promptFrontPhoto() {
@@ -131,34 +170,96 @@ export class PhotoUploadComponent implements OnInit {
   }
 
   //will submit all photos that have been uploaded
+  // this needs to be changed...i think it can add photos of each type just need to create the entire userPhotoInfo object for each
   submitPhotos() {
     console.log('front id pic', this.frontId, 'back id pic', this.backId, 'selfie pic', this.selfie);
-    this.selfiePicSubmit();
-    this.frontIDPicSubmit();
-    this.backIDPicSubmit();
+    var index;
+    for (index = 0; index < this.photoList.length; index++) {
+      if (this.photoList[index].type === 0) {
+        console.log('photo has a type of 0', this.photoList[index]);
+        this.photoList[index].data = this.selfie;
+        let selfiePhotoInfo = this.photoList[index];
+        this.selfiePicSubmit(selfiePhotoInfo);
+      }
+      if (this.photoList[index].type === 1) {
+        console.log('photo has a type of 1', this.photoList[index]);
+        this.photoList[index].data = this.frontId;
+        let frontIdPhotoInfo = this.photoList[index];
+        this.frontIDPicSubmit(frontIdPhotoInfo);
+      }
+      if (this.photoList[index].type === 2) {
+        console.log('photo has a type of 2', this.photoList[index]);
+        this.photoList[index].data = this.backId;
+        let backIdPhotoInfo = this.photoList[index];
+        this.backIDPicSubmit(backIdPhotoInfo);
+      }
+    }
+    // this.selfiePicSubmit();
+    // this.frontIDPicSubmit();
+    // this.backIDPicSubmit();
     //trying to add the function for setting all the photos you have taken
 
-    //changes the condition to submitted and removes the upload ability
+    //changes the condition to submitted and removes the upload ability, probably going to need to have something that is tied to each of the image repsonses 
+    //in the next parts of the individual subscriptions
     this.submitted = true;
-    console.log('function for submitting photos to DB');
   }
 
-  selfiePicSubmit() {
+  selfiePicSubmit(photo: UserPhotoInfo) {
     //this is where the add user photo code will go for selfie
+    this.userFacadeService.addUserPhoto(photo)
+      .subscribe(
+        response => {
+          console.log('response from selfie upload', response);
+        },
+        error => {
+          console.log('selfie photo submit error', error);
+          this.presentToast('There was an issue submitting the photo - please try again');
+        },
+        () => {
+          console.log('selfie Photo submitted');
+        }
+      )
   }
 
-  frontIDPicSubmit() {
+  frontIDPicSubmit(photo: UserPhotoInfo) {
     //this is where the add user photo code will go for selfie
+    this.userFacadeService.addUserPhoto(photo)
+      .subscribe(
+        response => {
+          console.log('response from front id upload', response);
+        },
+        error => {
+          console.log('front id photo submit error', error);
+          this.presentToast('There was an issue submitting the photo - please try again');
+        },
+        () => {
+          console.log('front id Photo submitted');
+        }
+      )
+
   }
 
-  backIDPicSubmit() {
+  backIDPicSubmit(photo: UserPhotoInfo) {
     //this is where the add user photo code will go for selfie
+    this.userFacadeService.addUserPhoto(photo)
+      .subscribe(
+        response => {
+          console.log('response from back id upload', response);
+        },
+        error => {
+          console.log('back id photo submit error', error);
+          this.presentToast('There was an issue submitting the photo - please try again');
+        },
+        () => {
+          console.log('back id Photo submitted');
+        }
+      )
   }
 
-  //will delete photos from DB
+  //opens modal for delete confirmation
   deletePhoto() {
-    this.presentModal();
-    console.log('function for deleteing photos from DB');
+    //passes in the photoId of the image being deleted, so far i am just asking them to delete the selfie, since that is what the design displayed
+    this.presentModal(this.selfiePhotoInfo.id);
   }
 
   /// Camera plugin control method
@@ -174,7 +275,7 @@ export class PhotoUploadComponent implements OnInit {
         height: 90.4, //Test
         direction: CameraDirection.Front,
         resultType: CameraResultType.Base64,
-        source: CameraSource.Camera,
+        source: CameraSource.Prompt,
         presentationStyle: 'popover',
         saveToGallery: false,
       })
@@ -182,10 +283,15 @@ export class PhotoUploadComponent implements OnInit {
   }
 
   //presents the delete pic modal and will eventually catch the result on dimiss when the api call is succesfull and update the screen appropriately
-  async presentModal() {
+  async presentModal(photoId) {
     const modal = await this.modalController.create({
       component: DeleteModalComponent,
+      componentProps: {
+        'photoId': photoId,
+      }
     });
+    const { data } = await modal.onWillDismiss();
+    console.log(data);
     return await modal.present();
   }
 
