6e8aad3fac1fc9281db164f478d9aa73e8394a97
Alan Nunez <axn3@cbord.com>
2020-07-31 19:30:43 -0400

    Pairing apple watch

--- ios/App/App/Managers/WatchSessionManager.swift
+++ ios/App/App/Managers/WatchSessionManager.swift
@@ -1,12 +1,33 @@
 //
 //  WatchSessionManager.swift
-//  App
 //
-//  Created by QISM1 on 7/31/20.
-//
+//  Modified by Alan Nunez on 7/31/20.
 
 import UIKit
+import CoreData
+import WatchConnectivity
 
-class WatchSessionManager: NSObject {
-
+class WatchSessionManager: NSObject, WCSessionDelegate  {
+    static let sharedManager = WatchSessionManager()
+    private let session = WCSession.default
+    
+    public func startSession() {
+        if WCSession.isSupported() {
+            session.delegate = self
+            session.activate()
+        }
+    }
+    
+    func session(_ session: WCSession,
+                 activationDidCompleteWith activationState: WCSessionActivationState,
+                 error: Error?) {
+        let isWatchPaired = activationState == .activated && session.isPaired
+        UserDefaults.standard.set(isWatchPaired, forKey: "isWatchPaired")
+    }
+    
+    func sessionDidBecomeInactive(_ session: WCSession) {
+    }
+    
+    func sessionDidDeactivate(_ session: WCSession) {
+    }
 }
