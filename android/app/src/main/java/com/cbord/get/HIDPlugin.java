b3a419f33de0307a468d17c4fe1ce987ea641b21
Alan Nunez <axn3@cbord.com>
2020-09-02 18:44:20 -0400

    Changed libs location, added compileOptions, multiDexEnabled, buildConfigField

--- android/app/src/main/java/com/cbord/get/HIDPlugin.java
+++ android/app/src/main/java/com/cbord/get/HIDPlugin.java
@@ -5,12 +5,31 @@ import com.getcapacitor.NativePlugin;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
+import com.hid.origo.OrigoKeysApiFactory;
+import com.hid.origo.api.OrigoMobileKeys;
+import com.hid.origo.api.OrigoReaderConnectionController;
+import com.hid.origo.api.ble.OrigoScanConfiguration;
 
 @NativePlugin()
-public class HIDPlugin extends Plugin {
+public class HIDPlugin extends Plugin implements OrigoKeysApiFactory {
 
     @PluginMethod()
     public void initializeOrigo(PluginCall call) {
         Log.d("TAG", "initializeOrigo method was called.");
+    }
+
+    @Override
+    public OrigoMobileKeys getMobileKeys() {
+        return null;
+    }
+
+    @Override
+    public OrigoReaderConnectionController getReaderConnectionController() {
+        return null;
+    }
+
+    @Override
+    public OrigoScanConfiguration getOrigoScanConfiguration() {
+        return null;
     }
 }
