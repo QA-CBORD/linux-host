73513872f88619ad86b23d7a71b35a4617d4471e
Edison Sanchez <exs6@cbord.com>
2020-09-03 08:31:02 -0400

    GET Ionic: Address listings do not have a comma between Address line 1 and Address line 2
    Add , between address line 1 and 2 in address helper.

--- src/app/core/utils/address-helper.ts
+++ src/app/core/utils/address-helper.ts
@@ -4,7 +4,7 @@ export const getAddressHeader = ({ nickname, building, address1, address2 }: Add
   (nickname ? nickname : '') +
   (building ? (nickname ? '' : building) : '') +
   (address1 ? (nickname || building ? ', ' + address1 : address1) : '') +
-  (address2 ? (address1 ? ' ' + address2 : address2) : '');
+  (address2 ? (address1 ? ', ' + address2 : address2) : '');
 
 export const getAddressSubHeader = ({ city, state, postalcode }: AddressInfo): string =>
   (city ? city : '') + (state ? (city ? ', ' + state : state) : '') + (state && postalcode ? ' ' + postalcode : '');
