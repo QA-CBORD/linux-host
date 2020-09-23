84944e7a48c993314a6ec380291c025a95a4737b
Bernabe Oviedo <jbo@cbord.com>
2020-09-23 12:15:47 -0400

    Adjustments to account text pipe and accessibility.

--- src/app/shared/accessibility/directives/accessible-select.directive.ts
+++ src/app/shared/accessibility/directives/accessible-select.directive.ts
@@ -8,9 +8,15 @@ export class AccessibleSelectDirective {
   @Input('stAccessibleSelect')
   selectedText: string;
 
+  @Input('attr.aria-placeholder')
+  ariaPlaceholder: string;
+
   @HostBinding('attr.aria-label') get ariaLabel() {
-    return this.selectedText || this.host.placeholder;
+    return this.selectedText || '';
   }
 
-  constructor(private host: IonSelect) {}
+  @HostBinding('attr.placeholder') get placeholder() {
+    // To prevent Screen readers from read both value and placeholder text.
+    return this.selectedText ? '' : this.ariaPlaceholder;
+  }
 }
