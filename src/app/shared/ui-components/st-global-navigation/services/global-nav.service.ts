8ee1b95408c444f75dc50ac5c48f4f7111ff708e
Bernabe Oviedo <jbo@cbord.com>
2020-10-23 12:00:43 -0400

    Preventing screen reader from reading content when menu expanded.

--- src/app/shared/ui-components/st-global-navigation/services/global-nav.service.ts
+++ src/app/shared/ui-components/st-global-navigation/services/global-nav.service.ts
@@ -1,13 +1,20 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { mergeMap, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GlobalNavService {
   private readonly _isNavBarShown$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
+  private readonly _isNavBarMenuExpanded$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
-  constructor() {
+  constructor() {}
+
+  get isNavBarMenuExpanded$(): Observable<boolean> {
+    return this._isNavBarMenuExpanded$
+      .asObservable()
+      .pipe(mergeMap(isExpanded => this.isNavBarShown$.pipe(map(isNavBarShown => isExpanded && isNavBarShown))));
   }
 
   get isNavBarShown$(): Observable<boolean> {
@@ -20,5 +27,13 @@ export class GlobalNavService {
 
   showNavBar(): void {
     this._isNavBarShown$.next(true);
+  }
+
+  expandNavBarMenu() {
+    this._isNavBarMenuExpanded$.next(true);
+  }
+
+  collapseNavBarMenu() {
+    this._isNavBarMenuExpanded$.next(false);
   }
 }
