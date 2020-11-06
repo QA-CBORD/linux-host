6769a72ce806ccd962daab647cace3870d37f4bf
Bernabe Oviedo <jbo@cbord.com>
2020-11-06 11:44:25 -0400

    Disabling photo edit.

--- src/app/sections/settings/pages/photo-upload/photo-upload.component.ts
+++ src/app/sections/settings/pages/photo-upload/photo-upload.component.ts
@@ -397,7 +397,6 @@ export class PhotoUploadComponent implements OnInit {
       Camera.getPhoto({
         quality: 85, //Test
         correctOrientation: true,
-        allowEditing: true,
         width: uploadSettings.saveWidth ? uploadSettings.saveWidth : null,
         height: uploadSettings.saveHeight ? uploadSettings.saveHeight : null,
         direction: photoType === PhotoType.PROFILE ? CameraDirection.Front : CameraDirection.Rear,
