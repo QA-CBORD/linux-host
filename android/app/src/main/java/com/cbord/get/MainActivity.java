c0ead31ec494f19422543f4fd7a1cce992acb5a3
Alan Nunez <axn3@cbord.com>
2020-11-11 08:26:34 -0400

    Triggering a new build

--- android/app/src/main/java/com/cbord/get/MainActivity.java
+++ android/app/src/main/java/com/cbord/get/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends BridgeActivity {
         }});
     }
 
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
