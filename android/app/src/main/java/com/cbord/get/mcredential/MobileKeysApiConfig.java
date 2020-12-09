0f16455b6b97ec1c15c684724b79bb5c1bbd8b94
maquendi <mxb5@cbord.com>
2020-12-09 11:31:29 -0400

    hid sdk crash when Get mobile not running

--- android/app/src/main/java/com/cbord/get/mcredential/MobileKeysApiConfig.java
+++ android/app/src/main/java/com/cbord/get/mcredential/MobileKeysApiConfig.java
@@ -22,28 +22,28 @@ import com.hid.origo.api.hce.OrigoNfcConfiguration;
 public class MobileKeysApiConfig extends Application {
     private OrigoMobileKeysApi mobileKeysApi;
 
+    private static final int LOCK_SERVICE_CODE = 2; // default
+    private static final int TRANSACTIONS_BACKOFF = 2000; // default
+    private static final String APPLICATION_ID = "com.cbord.get";
+    private static final byte TRANSACTIONS = 1; // default
+
     private void configure() throws IllegalStateException{
         OrigoApiConfiguration origoApiConfiguration = new OrigoApiConfiguration.Builder()
                 .setApplicationId(BuildConfig.AAMK_APP_ID)
                 .setApplicationDescription(BuildConfig.AAMK_APP_ID_DESCRIPTION)
                 .setNfcParameters(new OrigoNfcConfiguration.Builder()
                         .unsafe_setAttemptNfcWithScreenOff(true)
-                        .setNumberOfNfcTransactionsNeeded(MobileKeysConfig.TRANSACTIONS)
-                        .setTransactionBackOff(MobileKeysConfig.TRANSACTIONS_BACKOFF)
-                        .setOrigoAttemptNfcPredicate(() -> true)
+                        .setNumberOfNfcTransactionsNeeded(TRANSACTIONS)
+                        .setTransactionBackOff(TRANSACTIONS_BACKOFF)
                         .build())
                 .build();
         OrigoScanConfiguration origoScanConfiguration = new OrigoScanConfiguration.Builder(
-                new OrigoOpeningTrigger[] { new OrigoTapOpeningTrigger(getApplicationContext())
-                }, MobileKeysConfig.LOCK_SERVICE_CODE)
+                new OrigoOpeningTrigger[] { new OrigoTapOpeningTrigger(this)
+                }, LOCK_SERVICE_CODE)
                 .setAllowBackgroundScanning(true)
                 .build();
         mobileKeysApi = OrigoMobileKeysApi.getInstance();
-
-        mobileKeysApi.initialize(getApplicationContext(), origoApiConfiguration, origoScanConfiguration, BuildConfig.AAMK_APP_ID);
-        if (Boolean.FALSE.equals(mobileKeysApi.isInitialized())) {
-             throw new IllegalStateException();
-        }
+        mobileKeysApi.initialize(this, origoApiConfiguration, origoScanConfiguration, APPLICATION_ID);
     }
 
     public OrigoMobileKeysApi configureMobileKeysApi() throws IllegalStateException{
--- android/app/src/main/java/com/cbord/get/mcredential/MobileKeysConfig.java
+++
@@ -1,9 +0,0 @@
-package com.cbord.get.mcredential;
-
-import com.cbord.get.BuildConfig;
-
-public class MobileKeysConfig {
-    public static final int LOCK_SERVICE_CODE = 2, TRANSACTIONS_BACKOFF = 2000;
-    public static final String APPLICATION_ID = "A0000004400001010001"; // TODO: "This is the real aid" - seos_hce_service.xml
-    public static final byte TRANSACTIONS = 2;
-}
