73916084ffd0cc07987883753ea149c2832791bc
maquendi <mxb5@cbord.com>
2021-01-25 15:41:07 -0400

    fixed wrong error message when making deposit

--- src/app/core/interceptors/server-error.interceptor.ts
+++ src/app/core/interceptors/server-error.interceptor.ts
@@ -14,26 +14,28 @@ import { ToastService } from '@core/service/toast/toast.service';
 
 @Injectable()
 export class ServerError implements HttpInterceptor {
-
-  constructor(private readonly toastService: ToastService) {
-  }
+  constructor(private readonly toastService: ToastService) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-
     return next.handle(req).pipe(
       tap(
         (res: HttpEvent<any>) => {
-          if (res instanceof HttpResponse &&  res.body instanceof Object && 'exception' in res.body && res.body.exception !== null) {
-            this.handleServerException(res.body.exception);
+          if (
+            res instanceof HttpResponse &&
+            res.body instanceof Object &&
+            'exception' in res.body &&
+            res.body.exception !== null
+          ) {
+            this.handleServerException(res.body.exception, req.body.method);
           }
         },
         async ({ error, message, status, statusText }: HttpErrorResponse) => {
           // if (status >= 500) await this.presentToast('Internal server error');
           if (status === 404) await this.presentToast('Page was not found');
           return throwError(message);
-        },
+        }
       ),
-      catchError((error) => {
+      catchError(error => {
         if (error instanceof TimeoutError) {
           return throwError('Timeout Exception');
         }
@@ -46,29 +48,42 @@ export class ServerError implements HttpInterceptor {
     await this.toastService.showToast({ message });
   }
 
-  private handleServerException(exceptionString: string = ''): never {
+  private handleServerException(exceptionString: string = '', method: string): never {
     if (exceptionString.search(NUM_DSCRPTN_REGEXP) !== -1) {
       const errorMessageParts = exceptionString.split('|');
-      throw this.determineErrorByCodeAndThrow(errorMessageParts as [string, string]);
+      throw this.determineErrorByCodeAndThrow(errorMessageParts as [string, string], method);
     } else {
       throw new Error('Unexpected error occurred.');
     }
   }
 
-  private determineErrorByCodeAndThrow([code, message = '']: [string, string]): never {
+  private determineErrorByCodeAndThrow([code, message = '']: [string, string], method: string): never {
     const newError = new Error(`${code}|${message}`);
     if (Number(code) in GENERAL_ERRORS) {
-      newError.message = GENERAL_ERRORS[code];
+      newError.message = GENERAL_ERRORS[code][method] || GENERAL_ERRORS[code].default;
     }
     throw newError;
   }
 }
 
 const GENERAL_ERRORS = {
-  9999: 'Unable to parse response',
-  4001: 'Invalid session',
-  4002: 'Invalid user session',
-  9801: 'Invalid Service Argument Exception',
-  6100: 'Unable to retrieve mobile locations.',
-  6113: 'There was an issue with the transaction',
+  9999: {
+    default: 'Unable to parse response',
+  },
+  4001: {
+    default: 'Invalid session',
+  },
+  4002: {
+    default: 'Invalid user session',
+  },
+  9801: {
+    default: 'Invalid Service Argument Exception',
+  },
+  6100: {
+    deposit: 'There was an issue with the transaction',
+    default: 'Unable to retrieve mobile locations.',
+  },
+  6113: {
+    default: 'There was an issue with the transaction',
+  },
 };
