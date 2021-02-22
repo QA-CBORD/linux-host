d9920eca016bf7e2784f9001ca7729b8fc58ffb9
Carlos Parra <cxp@cbord.com>
2021-02-22 15:13:51 -0300

    RC-4854: Expand upon question-non-assignment details models

--- src/app/sections/housing/questions/types/question-non-assignment-details.ts
+++ src/app/sections/housing/questions/types/question-non-assignment-details.ts
@@ -1,31 +1,125 @@
 import { isDefined } from '@sections/housing/utils';
 
-import { QuestionFormControl, QuestionFormControlOptions } from './question-form-control';
+import { 
+  QuestionFormControl,
+  QuestionFormControlOptions
+} from './question-form-control';
+import {
+  QuestionBase,
+  QuestionBaseOptions
+} from '@sections/housing/questions/types/question-base';
+import { AssetTypeDetailValue } from '@sections/housing/non-assignments/non-assignments.model';
 
 let counter: number = 0;
 
-export interface QuestionNonAssignmentDetailsOptions extends QuestionFormControlOptions {
-  subtype?: string;
-  source?: string;
-  assetTypeId?: string;
+export interface QuestionAssetTypeDetailsBaseOptions extends QuestionBaseOptions {
+  required: boolean;
+  inline: boolean;
+  name: string;
+  other: boolean;
+  values: AssetTypeDetailValue[];
+  consumerKey: number;
+  readonly: boolean;
+  customName?: string;
+  customMeals?: string;
+  customDining?: string;
+  customCost?: string;
 }
 
-export class QuestionNonAssignmentDetails extends QuestionFormControl implements QuestionNonAssignmentDetailsOptions {
-    subtype: string;
-    source: string;
-    assetTypeId: string;
-  
-    constructor(options: QuestionNonAssignmentDetailsOptions) {
-      if (!isDefined(options) || typeof options !== 'object') {
-        options = {} as QuestionNonAssignmentDetailsOptions;
-      }
-  
-      super(options);
-  
-      this.type = options.type || 'text';
-      this.name = options.name || `text-${counter++}`;
-      this.subtype = options.subtype || 'text';
-      this.source = String(options.source);
-      this.assetTypeId = String(options.assetTypeId);
+export class QuestionAssetTypeDetailsBase extends QuestionBase implements QuestionAssetTypeDetailsBaseOptions {
+  required: boolean;
+  inline: boolean;
+  name: string;
+  other: boolean;
+  values: AssetTypeDetailValue[];
+  consumerKey: number;
+  readonly: boolean;
+  customName?: string;
+  customMeals?: string;
+  customDining?: string;
+  customCost?: string;
+
+  constructor(options: QuestionAssetTypeDetailsBaseOptions) {
+    if (!isDefined(options) || typeof options !== 'object') {
+      options = {} as QuestionAssetTypeDetailsBaseOptions;
     }
+
+    super(options);
+
+    this.type = 'asset-types-group';
+    this.name = String(options.name);
+    this.values = Array.isArray(options.values)
+      ? options.values.map((value: any) => new AssetTypeDetailValue(value))
+      : [];
+    this.readonly = Boolean(options.readonly);
+    this.consumerKey = Number(options.consumerKey);
+    this.inline = Boolean(options.inline);
+    this.other = Boolean(options.other);
+    this.required = Boolean(options.required);
+    this.customName = String(options.customName);
+    this.customMeals = String(options.customMeals);
+    this.customDining = String(options.customDining);
+    this.customCost = String(options.customCost);
   }
+}
+
+export interface QuestionNonAssignmentAssetTypeDetailsOptions extends QuestionFormControlOptions {
+  assetTypeKey: number;
+  name: string;
+  numberOfUnits: number;
+  cost: number;
+  diningDollars: number;
+  customName?: string;
+  customMeals?: string;
+  customDining?: string;
+  customCost?: string;
+}
+
+export class QuestionNonAssignmentAssetTypeDetails extends QuestionFormControl implements QuestionNonAssignmentAssetTypeDetailsOptions {
+  assetTypeKey: number;
+  name: string;
+  numberOfUnits: number;
+  cost: number;
+  diningDollars: number;
+  customName?: string;
+  customMeals?: string;
+  customDining?: string;
+  customCost?: string;
+
+  constructor(options: QuestionNonAssignmentAssetTypeDetailsOptions) {
+    if (!isDefined(options) || typeof options !== 'object') {
+      options = {} as QuestionNonAssignmentAssetTypeDetailsOptions;
+    }
+
+    super(options);
+
+    this.type = options.type || 'text';
+    this.assetTypeKey = Number(options.assetTypeKey);
+    this.name = options.name || `text-${counter++}`;
+    this.numberOfUnits = Number(options.numberOfUnits);
+    this.cost = Number(options.cost);
+    this.diningDollars = Number(options.diningDollars);
+  }
+}
+
+export interface QuestionAssetTypeDetailsOptions extends QuestionFormControlOptions {
+  assetTypes?: AssetTypeDetailValue[][];
+}
+
+export class QuestionAssetTypeDetails extends QuestionFormControl implements QuestionAssetTypeDetailsOptions {
+  assetTypes: AssetTypeDetailValue[][];
+
+  constructor(options: QuestionAssetTypeDetailsOptions) {
+    if (!isDefined(options) || typeof options !== 'object') {
+      options = {} as QuestionAssetTypeDetailsOptions;
+    }
+
+    super(options);
+    
+    this.type = 'asset-types-group';
+    this.name = `asset-types-group-${counter++}`;
+    this.assetTypes = Array.isArray(options.assetTypes)
+      ? options.assetTypes
+      : [];
+  }
+}
