4db138a3dd2a336f0927f5617a63bd2f0bd4d972
Alan Nunez <axn3@cbord.com>
2021-03-02 16:24:15 -0400

    Refactor change password component

--- src/app/shared/ui-components/change-password/change-password.component.ts
+++ src/app/shared/ui-components/change-password/change-password.component.ts
@@ -2,18 +2,15 @@ import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@
 import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UserFacadeService } from '@core/facades/user/user.facade.service';
 import { ToastService } from '@core/service/toast/toast.service';
-import { validatePasswordDecorator } from '@core/utils/general-helpers';
-import { PASS_CHANGE_REGEXP } from '@core/utils/regexp-patterns';
 import { ModalController } from '@ionic/angular';
 import { of } from 'rxjs';
 import { catchError, map, take } from 'rxjs/operators';
 import { CONTENT_STRINGS_CATEGORIES } from 'src/app/content-strings';
+import { RegistrationService } from 'src/app/non-authorized/pages/registration/services/registration.service';
 import {
   buildPasswordValidators,
-  getDefaultPasswordValidators,
   InputValidator,
-} from 'src/app/non-authorized/pages/registration/models/password-validation';
-import { RegistrationService } from 'src/app/non-authorized/pages/registration/services/registration.service';
+} from 'src/app/password-validation/models/input-validator.model';
 
 @Component({
   selector: 'st-change-password',
@@ -26,7 +23,8 @@ export class ChangePasswordComponent implements OnInit {
   isLoading = false;
   validators: InputValidator[] = [];
   hasError: boolean;
-
+  passwordControl: any = {};
+  
   constructor(
     private readonly modalController: ModalController,
     private readonly fb: FormBuilder,
@@ -38,47 +36,8 @@ export class ChangePasswordComponent implements OnInit {
 
   ngOnInit() {
     this.initForm();
+    this.initControl();
     this.getValidators();
-    this.newPassword.valueChanges.pipe(catchError((error) => {
-      console.log('Catch error: ', error)
-      return of(false);
-    })).subscribe((value) => {
-      console.log("Value: ", value);
-      const validator = getDefaultPasswordValidators();
-      validator.forEach(item => { this.hasError = !item.test(value)
-      console.log("Has error: ", this.hasError);
-      });
-    });
-  }
-
-  private getValidators() {
-    this.backendService
-      .getString$(CONTENT_STRINGS_CATEGORIES.passwordValidation)
-      .pipe(
-        take(1),
-        map(data => buildPasswordValidators(data)),
-        catchError(() => of(buildPasswordValidators()))
-      )
-      .subscribe(data => {
-        this.validators = data;
-        this.cdRef.detectChanges();
-      });
-  }
-
-  ngOnDestroy() {}
-
-  private initForm() {
-    // const passwordValidations = [
-    //   validatePasswordDecorator(Validators.required, { required: true }),
-    //   validatePasswordDecorator(Validators.pattern(PASS_CHANGE_REGEXP), { number: true })
-    // ];
-
-    this.changePasswordForm = this.fb.group({
-      [PASSWORD_FORM_CONTROL_NAMES.currentPassword]: ['', Validators.required],
-      [PASSWORD_FORM_CONTROL_NAMES.newPassword]: ['', Validators.required],
-    });
-
-    this.cdRef.detectChanges();
   }
 
   close() {
@@ -91,7 +50,7 @@ export class ChangePasswordComponent implements OnInit {
       .changePassword$(this.currentPassword.value, this.newPassword.value)
       .pipe(
         take(1),
-        catchError(err => {
+        catchError(() => {
           return of(false);
         })
       )
@@ -115,6 +74,39 @@ export class ChangePasswordComponent implements OnInit {
 
   get controlsNames() {
     return PASSWORD_FORM_CONTROL_NAMES;
+  }
+  
+  get controller() {
+    return this.passwordControl;
+  }
+ 
+  get disabled() {
+    return this.changePasswordForm.invalid || this.passwordControl.hasError;
+  }
+
+  private initForm() {
+    this.changePasswordForm = this.fb.group({
+      [PASSWORD_FORM_CONTROL_NAMES.currentPassword]: ['', Validators.required],
+      [PASSWORD_FORM_CONTROL_NAMES.newPassword]: ['', Validators.required],
+    });
+  }
+
+  private getValidators() {
+    this.backendService
+      .getString$(CONTENT_STRINGS_CATEGORIES.passwordValidation)
+      .pipe(
+        take(1),
+        map(data => buildPasswordValidators(data)),
+        catchError(() => of(buildPasswordValidators()))
+      )
+      .subscribe(data => {
+        this.validators = data;
+        this.cdRef.detectChanges();
+      });
+  }
+
+  private initControl() {
+    this.passwordControl = { control: this.newPassword, hasError: false };
   }
 }
 
