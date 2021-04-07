666154342fe0a4e52a5ea5695b13b161d62ebf24
Alan Nunez <axn3@cbord.com>
2021-04-07 07:40:01 -0400

    More add funds cleaning

--- src/app/sections/guest/guest-dashboard/components/guest-add-funds/guest-add-funds.component.ts
+++ src/app/sections/guest/guest-dashboard/components/guest-add-funds/guest-add-funds.component.ts
@@ -15,37 +15,33 @@ import { DepositService } from '@sections/accounts/services/deposit.service';
 import { MerchantAccountInfoList } from '@sections/ordering';
 import { GlobalNavService } from '@shared/ui-components/st-global-navigation/services/global-nav.service';
 import { from, iif, Observable, Subscription, throwError } from 'rxjs';
-import {  map, switchMap, take, tap } from 'rxjs/operators';
+import { map, switchMap, take, tap } from 'rxjs/operators';
 import { AccountType, DisplayName, PATRON_NAVIGATION, Settings } from 'src/app/app.global';
 
-export enum GUEST_FORM_CONTROL_NAMES {
+enum GUEST_FORM_CONTROL_NAMES {
   paymentMethod = 'paymentMethod',
   toAccount = 'toAccount',
   amountToDeposit = 'amountToDeposit',
- mainInput = 'mainInput'
+  mainInput = 'mainInput',
 }
- 
+
 const requiredSettings = [
   Settings.Setting.DEPOSIT_TENDERS,
   Settings.Setting.PAYMENT_TYPES,
-  Settings.Setting.BILLME_MAPPING,
   Settings.Setting.FREEFORM_DEPOSIT_ENABLED,
   Settings.Setting.PRESET_DEPOSIT_AMOUNTS_CREDITCARD,
-  Settings.Setting.BILLME_AMOUNTS,
-  Settings.Setting.BILLME_AMOUNT_MIN,
-  Settings.Setting.BILLME_AMOUNT_MAX,
-  Settings.Setting.BILLME_FREEFORM_ENABLED,
   Settings.Setting.CREDIT_PAYMENT_SYSTEM_TYPE,
   Settings.Setting.CREDITCARD_AMOUNT_MIN,
   Settings.Setting.CREDITCARD_AMOUNT_MAX,
 ];
+
+
 @Component({
   selector: 'st-guest-add-funds',
   templateUrl: './guest-add-funds.component.html',
   styleUrls: ['./guest-add-funds.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-
 export class GuestAddFundsComponent implements OnInit {
   customActionSheetOptions: { [key: string]: string } = {
     cssClass: 'custom-deposit-actionSheet',
@@ -69,8 +65,7 @@ export class GuestAddFundsComponent implements OnInit {
   destinationAccounts: Array<UserAccount>;
   private activePaymentType: PAYMENT_TYPE;
   private readonly sourceSubscription: Subscription = new Subscription();
-  
-  
+
   constructor(
     private readonly fb: FormBuilder,
     private readonly globalNav: GlobalNavService,
@@ -80,16 +75,20 @@ export class GuestAddFundsComponent implements OnInit {
     private readonly router: Router,
     private externalPaymentService: ExternalPaymentService,
     private readonly loadingService: LoadingService,
-    private readonly toastService: ToastService,
+    private readonly toastService: ToastService
   ) {}
 
   ngOnInit() {
     this.accounts$ = this.depositService.getUserAccounts();
     this.applePayEnabled$ = this.userFacadeService.isApplePayEnabled$();
-    this.depositService.getUserSettings(requiredSettings).pipe(take(1)).subscribe((result)=>{
-      this.depositSettings = result;
-    })
- 
+    this.depositService
+      .getUserSettings(requiredSettings)
+      .pipe(take(1))
+      .subscribe(result => {
+        this.depositSettings = result;
+        console.log(`required settings: ${result}`)
+      });
+
     this.initForm();
     this.initContentStrings();
     this.globalNav.hideNavBar();
@@ -106,19 +105,18 @@ export class GuestAddFundsComponent implements OnInit {
   }
 
   initForm() {
-
-   // const minMaxValidators = [amountRangeValidator(+this.minMaxOfAmmounts.minAmountOneTime, +this.minMaxOfAmmounts.maxAmountOneTime)];
     this.detailsForm = this.fb.group({
       [GUEST_FORM_CONTROL_NAMES.paymentMethod]: ['', Validators.required],
       [GUEST_FORM_CONTROL_NAMES.toAccount]: ['', Validators.required],
       [GUEST_FORM_CONTROL_NAMES.amountToDeposit]: ['', Validators.required],
-      //[GUEST_FORM_CONTROL_NAMES.mainInput]: ['', Validators.required, ...minMaxValidators,  Validators.pattern(CURRENCY_REGEXP)],
+      [GUEST_FORM_CONTROL_NAMES.mainInput]: [''],
     });
+    //this.setFormValidators();
   }
 
   onPaymentChanged({ target }) {
     this.defineDestAccounts(target.value);
-    this.resetControls(['mainSelect', 'mainInput', 'selectedAccount']);
+    this.resetControls(['amountToDeposit', 'mainInput', 'selectedAccount']);
     document.getElementById('depositBtnText').innerHTML = 'Deposit';
   }
 
@@ -126,24 +124,23 @@ export class GuestAddFundsComponent implements OnInit {
     const { value } = event.target;
     const index = value.indexOf('.');
     if (!NUM_COMMA_DOT_REGEXP.test(value)) {
-     this.detailsForm.get('mainInput').setValue(value.slice(0, value.length - 1));
+      this.detailsForm.get('mainInput').setValue(value.slice(0, value.length - 1));
     }
 
     if (index !== -1 && value.slice(index + 1).length > 1) {
-     this.detailsForm.get('mainInput').setValue(value.slice(0, index + 2));
+      this.detailsForm.get('mainInput').setValue(value.slice(0, index + 2));
     }
   }
 
   setFormValidators() {
-    const minMaxValidators =
-      this.activePaymentType !== PAYMENT_TYPE.CREDIT
-        ? [amountRangeValidator(+this.minMaxOfAmmounts.minAmountbillme, +this.minMaxOfAmmounts.maxAmountbillme)]
-        : [amountRangeValidator(+this.minMaxOfAmmounts.minAmountOneTime, +this.minMaxOfAmmounts.maxAmountOneTime)];
+    const minMaxValidators = [
+      amountRangeValidator(+this.minMaxOfAmmounts.minAmountOneTime, +this.minMaxOfAmmounts.maxAmountOneTime),
+    ];
 
     this.isFreeFormEnabled$.pipe(take(1)).subscribe(data => {
       const sourceAcc = this.detailsForm.get('sourceAccount').value;
-      this.detailsForm.controls['mainSelect'].clearValidators();
-      this.detailsForm.controls['mainSelect'].setErrors(null);
+      this.detailsForm.controls['amountToDeposit'].clearValidators();
+      this.detailsForm.controls['amountToDeposit'].setErrors(null);
       this.resolveCVVValidators(sourceAcc);
 
       if (sourceAcc === 'newCreditCard') {
@@ -180,12 +177,12 @@ export class GuestAddFundsComponent implements OnInit {
           Validators.pattern(CURRENCY_REGEXP),
         ]);
       } else {
-        this.detailsForm.controls['mainSelect'].setValidators([Validators.required]);
+        this.detailsForm.controls['amountToDeposit'].setValidators([Validators.required]);
         this.mainFormInput.clearValidators();
         this.mainFormInput.setErrors(null);
-        this.resetControls(['mainSelect', 'mainInput']);
+        this.resetControls(['amountToDeposit', 'mainInput']);
       }
-      this.detailsForm.controls['mainSelect'].setValue(0);
+      this.detailsForm.controls['amountToDeposit'].setValue(0);
     });
   }
 
@@ -204,7 +201,7 @@ export class GuestAddFundsComponent implements OnInit {
   get amountToDeposit(): AbstractControl {
     return this.detailsForm.get(GUEST_FORM_CONTROL_NAMES.amountToDeposit);
   }
- 
+
   get mainFormInput(): AbstractControl {
     return this.detailsForm.get(GUEST_FORM_CONTROL_NAMES.mainInput);
   }
@@ -216,16 +213,8 @@ export class GuestAddFundsComponent implements OnInit {
       this.isFreeFromDepositEnabled$
     );
   }
-  
+
   get minMaxOfAmmounts() {
-    const minAmountbillme = this.getSettingByName(
-      this.depositSettings,
-      Settings.Setting.BILLME_AMOUNT_MIN.split('.')[2]
-    );
-    const maxAmountbillme = this.getSettingByName(
-      this.depositSettings,
-      Settings.Setting.BILLME_AMOUNT_MAX.split('.')[2]
-    );
     const minAmountOneTime = this.getSettingByName(
       this.depositSettings,
       Settings.Setting.CREDITCARD_AMOUNT_MIN.split('.')[2]
@@ -236,14 +225,10 @@ export class GuestAddFundsComponent implements OnInit {
     );
 
     return {
-      minAmountbillme,
-      maxAmountbillme,
       minAmountOneTime,
       maxAmountOneTime,
     };
   }
-
-  
 
   get isFreeFromDepositEnabled$(): Observable<boolean> {
     return this.depositService.getUserSettings(requiredSettings).pipe(
@@ -257,36 +242,38 @@ export class GuestAddFundsComponent implements OnInit {
       })
     );
   }
-  private initContentStrings() {
-    this.topLabel =
-      'You are deposting to the account of {{ James Demo }}. If this is incorrect, go back to to Step 1 to identify the recipient';
-  }
 
   get isCreditCardPaymentTypesEnabled(): boolean {
     return true;
   }
-  
+
   get isCVVfieldShow(): boolean {
     const sourceAcc = this.detailsForm.get('sourceAccount').value;
 
     return (
       sourceAcc &&
-      (sourceAcc !== PAYMENT_TYPE.BILLME || sourceAcc !== 'newCreditCard') &&
+      sourceAcc !== 'newCreditCard' &&
       sourceAcc.accountType === ACCOUNT_TYPES.charge &&
       sourceAcc.paymentSystemType !== PAYMENT_SYSTEM_TYPE.USAEPAY
     );
   }
 
+  private initContentStrings() {
+    this.topLabel =
+      'You are deposting to the account of {{ James Demo }}. If this is incorrect, go back to to Step 1 to identify the recipient';
+  }
+
   private getAccounts() {
-    const subscription = this.depositService.getUserSettings(requiredSettings)
+    const subscription = this.depositService
+      .getUserSettings(requiredSettings)
       .pipe(
         map(settings => {
           const depositTenders = this.getSettingByName(settings, Settings.Setting.DEPOSIT_TENDERS.split('.')[2]);
           return {
-            depositTenders: parseArrayFromString(depositTenders)
+            depositTenders: parseArrayFromString(depositTenders),
           };
         }),
-        switchMap(({ depositTenders}) =>
+        switchMap(({ depositTenders }) =>
           this.depositService.accounts$.pipe(
             tap(accounts => {
               this.creditCardSourceAccounts = this.filterAccountsByPaymentSystem(accounts);
@@ -307,7 +294,7 @@ export class GuestAddFundsComponent implements OnInit {
 
   private getSettingByName(settings, property: string) {
     const depositSetting = this.depositService.getSettingByName(settings, property);
-    return depositSetting && depositSetting.value || '';
+    return (depositSetting && depositSetting.value) || '';
   }
 
   private defineDestAccounts(target) {
@@ -334,7 +321,7 @@ export class GuestAddFundsComponent implements OnInit {
   }
 
   private resolveCVVValidators(sourceAcc: UserAccount | number) {
-    if (sourceAcc !== PAYMENT_TYPE.BILLME && this.isCVVfieldShow) {
+    if (this.isCVVfieldShow) {
       this.detailsForm.controls['fromAccountCvv'].setValidators([
         Validators.required,
         Validators.minLength(3),
