44abfde37b1e844ba05826d7ad2858064df906e8
Alan Nunez <axn3@cbord.com>
2021-08-02 11:26:03 -0400

    Basic service call

--- src/app/sections/check-in/services/checkin-service-facade.ts
+++ src/app/sections/check-in/services/checkin-service-facade.ts
@@ -1,32 +1,52 @@
-import { Injectable } from "@angular/core";
-import { ContentStringCategory } from "@shared/model/content-strings/content-strings-api";
-import { CommonService } from "@shared/services/common.service";
-import { Observable } from "rxjs";
-import { CheckingContentCsModel } from "../contents-strings/checkin-content-string.model";
+import { Injectable } from '@angular/core';
+import { BarcodeScanner, BarcodeScannerOptions, BarcodeScanResult } from '@ionic-native/barcode-scanner/ngx';
+import { ContentStringCategory } from '@shared/model/content-strings/content-strings-api';
+import { CommonService } from '@shared/services/common.service';
+import { Observable } from 'rxjs';
+import { CheckingContentCsModel } from '../contents-strings/checkin-content-string.model';
 
-@Injectable({ providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class CheckingServiceFacade {
-    constructor(private readonly commonService: CommonService) {}
+  private barcodeScanResult: BarcodeScanResult;
+  constructor(private readonly commonService: CommonService, private readonly barcode: BarcodeScanner) {}
 
-
-    /**
-     * solo se llama al cargar el primer componente del flow de checking
-     * @returns 
-     */
-    loadAllContentString () :Observable<CheckingContentCsModel> {
-        return this.commonService.loadContentString(ContentStringCategory.checkin);
-    }
+  /**
+   * solo se llama al cargar el primer componente del flow de checking
+   * @returns
+   */
+  loadAllContentString(): Observable<CheckingContentCsModel> {
+    return this.commonService.loadContentString(ContentStringCategory.checkin);
+  }
 
   /**
    * llamar desde cualquier component del flow de checking
-   * @returns 
+   * @returns
    */
-    getContent(): CheckingContentCsModel {
-        return this.commonService.getString(ContentStringCategory.checkin);
+  getContent(): CheckingContentCsModel {
+    return this.commonService.getString(ContentStringCategory.checkin);
+  }
+
+  async scanBarcode(format: string = 'QR_CODE') {
+    try {
+      const options: BarcodeScannerOptions = {
+        orientation: 'portrait', // android only
+        preferFrontCamera: false,
+        prompt: 'Scan Reward',
+        showFlipCameraButton: false,
+        showTorchButton: true,
+        torchOn: false,
+        formats: format,
+        resultDisplayDuration: 0,
+      };
+      this.barcodeScanResult = await this.barcode.scan(options);
+
+      if (!this.barcodeScanResult.cancelled) {
+        alert(`result: ${this.barcodeScanResult.text}`);
+      } else {
+        alert(`result cancelled`);
+      }
+    } catch (e) {
+      alert(`error: ${e.message}`);
     }
-
-
-
-
-
+  }
 }
