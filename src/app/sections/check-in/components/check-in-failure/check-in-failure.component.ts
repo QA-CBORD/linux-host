08e29239465111480ed906df15be6373d5e5a3f4
Alan Nunez <axn3@cbord.com>
2021-08-12 09:36:56 -0400

    Display error messages

--- src/app/sections/check-in/components/check-in-failure/check-in-failure.component.ts
+++ src/app/sections/check-in/components/check-in-failure/check-in-failure.component.ts
@@ -1,11 +1,18 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+
+const CHECKIN_ERROR_CODES = {
+  OUTSIDE_OF_RANGE: '9018',
+  INVALID_BARCODE: '9020'
+};
+
 @Component({
   selector: 'st-check-in-failure',
   templateUrl: './check-in-failure.component.html',
   styleUrls: ['./check-in-failure.component.scss'],
 })
 export class CheckInFailureComponent implements OnInit {
+
   @Input() contentStrings = <any>{};
   @Input() orderId: string;
   @Input() errorMessage: string;
@@ -15,10 +22,7 @@ export class CheckInFailureComponent implements OnInit {
   constructor(private readonly modalController: ModalController) {}
 
   ngOnInit() {
-    this.displayPlayMessage = this.contentStrings.get_closer;
-    if (this.errorMessage && this.errorMessage.includes('early')) {
-      this.displayPlayMessage = this.contentStrings.too_early;
-    }
+    this.displayErrorMessage();
   }
 
   async onBack() {
@@ -26,6 +30,17 @@ export class CheckInFailureComponent implements OnInit {
   }
 
   async onScanCode() {
-      await this.modalController.dismiss({ scancode: true });
+    await this.modalController.dismiss({ scancode: true });
+  }
+
+  private displayErrorMessage() {
+    this.displayPlayMessage = this.contentStrings.too_early;
+    if (this.errorMessage) {
+      if (this.errorMessage.includes(CHECKIN_ERROR_CODES.OUTSIDE_OF_RANGE)) {
+        this.displayPlayMessage = this.contentStrings.get_closer;
+      } else if (this.errorMessage.includes(CHECKIN_ERROR_CODES.INVALID_BARCODE)) {
+        this.displayPlayMessage = this.contentStrings.invalid_scan_code;
+      }
+    }
   }
 }
