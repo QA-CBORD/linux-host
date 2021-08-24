91e44d8b2ee4e32ade20b95831091beaec521761
QISM1 <qism1@QISM1s-MacBook-Pro-2.local>
2021-08-24 14:26:03 -0400

    Moved logic to service

--- src/app/sections/guest/services/guest-dashboard.resolver.ts
+++ src/app/sections/guest/services/guest-dashboard.resolver.ts
@@ -7,19 +7,20 @@ import { GuestFacadeService } from './guest.facade.service';
 import { UserInfo } from '@core/model/user';
 import { LoadingService } from '@core/service/loading/loading.service';
 import { MessageProxy } from '@shared/services/injectable-message.proxy';
+import { ProminentDisclosureService } from '@sections/dashboard/services/prominent-disclosure.service';
 
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable()
 export class GuestDashboardResolver implements Resolve<Observable<UserInfo>> {
   constructor(
     private readonly guestFacadeService: GuestFacadeService,
     private readonly loadingService: LoadingService,
     private readonly userFacade: UserFacadeService,
-    private readonly messageProxy: MessageProxy
+    private readonly messageProxy: MessageProxy,
+    private readonly prominentDisclosureService: ProminentDisclosureService,
   ) {}
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<UserInfo> {
+    this.prominentDisclosureService.openProminentDisclosure();
     this.loadingService.showSpinner();
     const user$ = this.userFacade.getUserData$();
     const dashboardSectionsObs = this.guestFacadeService.configureGuestDashboard().pipe(take(1));
