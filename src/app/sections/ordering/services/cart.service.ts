f986488f950ab514afde63eec483812daf2f6115
Delcio Polanco <dmp@cbord.com>
2021-11-18 11:01:44 -0400

    creating a separate function to evaluate the null of barcode and also the product in the cache, also adding uppercasing to both.

--- src/app/sections/ordering/services/cart.service.ts
+++ src/app/sections/ordering/services/cart.service.ts
@@ -385,17 +385,22 @@ export class CartService {
 
   // ----------------------------------------- GETTERS BLOCK -----------------------------------------//
   getMenuItemByCode(code: string) {
-    code = code ? code.trim().toUpperCase() : '';
     return this.menuInfo$.pipe(
       filter(menu => !!menu.menuCategories),
       map(menu =>
         menu.menuCategories
           .map(cat => cat.menuCategoryItems.map(item => item.menuItem))
           .reduce((prev, curr) => [...prev, ...curr], [])
-          .find(item => item.barcode.toUpperCase() === code)
+          .find(item => this.removeLeadingZerosAndUpperCase(item.barcode) === this.removeLeadingZerosAndUpperCase(code))
       ),
       take(1)
     );
+  }
+
+  private removeLeadingZerosAndUpperCase(code: string): string {
+    if (!code) return;
+
+    return code.toUpperCase().replace(/\b0+/g, '');
   }
 }
 
