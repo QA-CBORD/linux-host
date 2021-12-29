9d612978a160a69945b52fbeec784937d618b505
Alan Nunez <raul.nunez01@gmail.com>
2021-12-29 12:47:01 -0400

    Handling check number

--- src/app/sections/ordering/services/cart.service.ts
+++ src/app/sections/ordering/services/cart.service.ts
@@ -23,7 +23,8 @@ export class CartService {
   private _clientOrderId: string = null;
   private _pendingOrderId: string = null;
   public orderIsAsap: boolean = false;
-  public checkNumber: number = null;
+  checkNumber: number;
+  currentOrderId: string;
   merchantTimeZone: string;
 
   constructor(
