08a93e794c1e5dbf98b16b280f584fdf7f89dce6
Delcio Polanco <dmp@cbord.com>
2022-03-11 01:19:27 -0400

    Fix console error when selecting payment option

--- src/app/shared/accessibility/directives/accessible-select.directive.ts
+++ src/app/shared/accessibility/directives/accessible-select.directive.ts
@@ -2,6 +2,7 @@ import { Directive, Input, HostBinding, OnDestroy } from '@angular/core';
 import { IonSelect } from '@ionic/angular';
 import { Subscription } from 'rxjs';
 import { Plugins } from '@capacitor/core';
+import { SessionFacadeService } from '@core/facades/session/session.facade.service';
 
 const { Accessibility } = Plugins;
 const READ_ALOUD_DELAY = 2000;
@@ -14,7 +15,7 @@ export class AccessibleSelectDirective implements OnDestroy {
     this.subs && this.subs.unsubscribe();
   }
 
-  @Input('attr.a11y-placeholder') a11yPlaceholder: string
+  @Input('attr.a11y-placeholder') a11yPlaceholder: string;
 
   @HostBinding('attr.aria-placeholder') get ariaPlaceholder() {
     return this.host.selectedText ? '' : this.host.placeholder || this.a11yPlaceholder;
@@ -26,15 +27,17 @@ export class AccessibleSelectDirective implements OnDestroy {
 
   subs: Subscription;
 
-  constructor(private readonly host: IonSelect) {
-    this.subs = this.host.ionChange.subscribe(() =>
-      Accessibility.isScreenReaderEnabled().then(isRunning => {
-        if (isRunning.value) {
-          setTimeout(() => {
-            Accessibility.speak({ value: this.host.selectedText || this.host.value });
-          }, READ_ALOUD_DELAY);
-        }
-      })
-    );
+  constructor(private readonly host: IonSelect, private sessionService: SessionFacadeService) {
+    this.subs = this.host.ionChange.subscribe(() => {
+      if (!this.sessionService.getIsWeb()) {
+        Accessibility.isScreenReaderEnabled().then(isRunning => {
+          if (isRunning.value) {
+            setTimeout(() => {
+              Accessibility.speak({ value: this.host.selectedText || this.host.value });
+            }, READ_ALOUD_DELAY);
+          }
+        });
+      }
+    });
   }
 }
