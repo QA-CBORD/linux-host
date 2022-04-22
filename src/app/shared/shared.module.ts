cfb89235512cf4e51db3df78ddc3344af9a5c6f3
unknown <mxb5@cbord.com>
2022-04-22 17:18:59 -0400

    remove import

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,13 +2,12 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { IonicModule } from "@ionic/angular";
-import { GenericServerErrorScreen } from "./generic-server-error-modal/generic-server-error-screen";
 import { NoConnectivityScreen } from "./no-connectivity-screen/no-connectivity-screen";
 import { StButtonModule } from "./ui-components/st-button";
 import { StHeaderModule } from "./ui-components/st-header/st-header.module";
 
 @NgModule({
-    declarations: [NoConnectivityScreen, GenericServerErrorScreen],
+    declarations: [NoConnectivityScreen],
     imports: [
         CommonModule,
         FormsModule,
