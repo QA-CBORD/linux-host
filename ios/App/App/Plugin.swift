0d367a4e9da90376a9bc96e1106abe72e62d63e4
axn3 <axn3@cbord.com>
2022-05-06 00:12:57 -0400

    preliminary changes

--- ios/App/App/Plugin.swift
+++ ios/App/App/Plugin.swift
@@ -122,4 +122,23 @@ class Plugin: CAPPlugin {
             }.resume()
         }
     }
+    
+    func checkIsConnectedToNetwork() {
+        let hostUrl = "https://google.com"
+        if let url = URL(string: hostUrl) {
+            var request = URLRequest(url: url)
+            request.httpMethod = "HEAD"
+            URLSession(configuration: .default).dataTask(with: request) { (_, response, error) -> Void in
+                guard error == nil else {print("Error: ", error ?? "")
+                    return
+                }
+                guard (response as? HTTPURLResponse)?.statusCode == 200 else {
+                    print("The network is down")
+                    return }
+                print("The network is up and running")}.resume()
+            
+        }
+        
+    }
+    
 }
--- src/app/sections/settings/pages/photo-upload/photo-upload.component.spec.ts
+++ src/app/sections/settings/pages/photo-upload/photo-upload.component.spec.ts
@@ -9,7 +9,7 @@ import { ActionSheetController } from '@ionic/angular';
 import { of } from 'rxjs';
 import { CameraService } from '../services/camera.service';
 import { PhotoCropModalService } from '../services/photo-crop.service';
-import { PhotoUploadService } from '../services/photo-upload.service';
+import { PhotoType, PhotoUploadService } from '../services/photo-upload.service';
 import { PhotoUploadComponent } from './photo-upload.component';
 
 describe('PhotoUploadComponent', () => {
@@ -34,7 +34,11 @@ describe('PhotoUploadComponent', () => {
     govtIdBack$: of(true),
     profileImage$: of(true),
     profileImagePending$: of(true),
-    governmentIdRequired$: of(true)
+    governmentIdRequired$: of(true),
+    photoUploadSettings: {
+      saveWidth: 100,
+      saveHeight: 100,
+    },
   };
 
   let loadingService = {
@@ -42,10 +46,9 @@ describe('PhotoUploadComponent', () => {
     closeSpinner: jest.fn(),
   };
   let actionSheetCtrl = {
-    clearLocalGovernmentIdPhotos: jest.fn(),
-    clearLocalProfilePhoto: jest.fn(),
+    // create: jest.fn().mockResolvedValue(({ present: jest.fn().mockResolvedValue(null), onWillDismiss: jest.fn().mockResolvedValue( { role: 'take-photo'}) })),
+    create: jest.fn(),
   };
-
   let cd = {
     detectChanges: jest.fn(),
   };
@@ -55,21 +58,26 @@ describe('PhotoUploadComponent', () => {
   };
 
   let cameraService = {
-    getPhoto: jest.fn(),
+    getPhoto: jest.fn().mockResolvedValue(true),
   };
+
+  let controller = {
+    present: jest.fn(async() => true),
+    onWillDismiss: jest.fn(async() => ({ role: 'take-photo'}))
+  }
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [PhotoUploadComponent],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
       providers: [
-        { provide: Router, useValue: router},
+        { provide: Router, useValue: router },
         { provide: DomSanitizer, useValue: domSanitizer },
         { provide: SessionFacadeService, useValue: sessionFacadeService },
         { provide: ToastService, useValue: toastService },
         { provide: PhotoUploadService, useValue: photoUploadService },
         { provide: LoadingService, useValue: loadingService },
-        { provide: ActionSheetController, useValue: actionSheetCtrl },
+        { provide: ActionSheetController },
         { provide: ChangeDetectorRef, useValue: cd },
         { provide: PhotoCropModalService, useValue: photoCropModalService },
         { provide: CameraService, useValue: cameraService },
@@ -83,7 +91,44 @@ describe('PhotoUploadComponent', () => {
     fixture.detectChanges();
   });
 
-    it('should create component', () => {
-      expect(component).toBeTruthy();
-    });
+  it('should create component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  // it('should call camera service ', () => {
+  //   let spy = jest.spyOn(cameraService, 'getPhoto');
+  //   component.onGetPhoto(PhotoType.GOVT_ID_FRONT, CameraSource.Camera);
+  //   component.onGetPhoto(PhotoType.GOVT_ID_FRONT, CameraSource.Photos);
+  //   component.onGetPhoto(PhotoType.GOVT_ID_BACK, CameraSource.Camera);
+  //   component.onGetPhoto(PhotoType.GOVT_ID_BACK, CameraSource.Photos);
+  //   component.onGetPhoto(PhotoType.PROFILE, CameraSource.Camera);
+  //   component.onGetPhoto(PhotoType.PROFILE, CameraSource.Photos);
+  //   component.onGetPhoto(PhotoType.PROFILE_PENDING, CameraSource.Camera);
+  //   component.onGetPhoto(PhotoType.PROFILE_PENDING, CameraSource.Photos);
+  //   expect(spy).toHaveBeenCalledTimes(8);
+  // });
+
+  it('should call camera service', async () => {
+    //jest.resetAllMocks();
+    //  const createdIphone = await actionSheetCtrl.create();
+    //  const c = await createdIphone.present();
+    //  const d = await createdIphone.onWillDismiss();
+    // const created = await createdIphone.present();
+    // const dismiss = await createdIphone.onWillDismiss();
+    let spy =  jest
+      .spyOn(actionSheetCtrl,'create')
+      .mockResolvedValue(Promise.resolve(controller));
+    let spy2 = jest.spyOn(component, 'onGetPhoto');
+    try {
+      await component.presentPhotoTypeSelection(PhotoType.GOVT_ID_FRONT);
+    } catch (error) {
+      console.log(error);
+    }
+
+    // component.presentPhotoTypeSelection(PhotoType.GOVT_ID_BACK);
+    // component.presentPhotoTypeSelection(PhotoType.PROFILE);
+    // component.presentPhotoTypeSelection(PhotoType.PROFILE_PENDING);
+    expect(spy).toBeCalledTimes(1);
+    expect(spy2).toBeCalled();
+  });
 });
