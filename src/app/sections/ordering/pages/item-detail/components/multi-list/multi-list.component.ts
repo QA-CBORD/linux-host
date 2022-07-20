c28019c964040c6d9feadd533eb3028ffa6b9d1a
unknown <mxb5@cbord.com>
2022-07-20 17:09:21 -0400

    GET-3806: fixed bug

--- src/app/sections/ordering/pages/item-detail/components/multi-list/multi-list.component.ts
+++ src/app/sections/ordering/pages/item-detail/components/multi-list/multi-list.component.ts
@@ -31,16 +31,7 @@ export class MultiListComponent extends DefaultValueAccessor implements OnInit {
   ngOnInit() {
     this.writeValue(this.control.value);
     this.onChange(this.control.value);
-
-    if (this.control.value.length) {
-      this.modifiedOptions = <MenuGroupItemInfoChecked[]>this.options.map(elem => {
-        const isMenuItemInclude = this.innerValue.includes(elem.menuItem);
-
-        return {...elem, checked: isMenuItemInclude};
-      });
-    } else {
-      this.modifiedOptions = <MenuGroupItemInfoChecked[]>this.options.map(elem => ({ ...elem, checked: false }));
-    }
+    this.modifiedOptions = <MenuGroupItemInfoChecked[]>this.options.map(elem => ({ ...elem, checked: false }));
   }
 
   onItemsChecked({ detail: { value } }) {
