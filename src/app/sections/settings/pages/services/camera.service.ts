69da7c6859dcfbfa6b14c0bd5781d2adfb186c1f
axn3 <axn3@cbord.com>
2022-09-02 11:09:28 -0400

    GET-4253 4.16.19

--- src/app/sections/settings/pages/services/camera.service.ts
+++ src/app/sections/settings/pages/services/camera.service.ts
@@ -1,18 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Camera, CameraPermissionType, CameraSource, ImageOptions, Photo } from '@capacitor/camera';
 import { IdentityFacadeService } from '@core/facades/identity/identity.facade.service';
-import { Platform } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CameraService {
 
-  constructor(private readonly platform: Platform,
-    private identityFacadeService: IdentityFacadeService) { }
+  constructor(private identityFacadeService: IdentityFacadeService) {}
 
   async getPhoto(options: ImageOptions): Promise<Photo> {
-    await this.requestCameraPermission(options?.source || CameraSource.Photos);
+    if (!options?.source) return;
+    await this.requestCameraPermission(options?.source);
     this.identityFacadeService.updateVaultTimeout({ extendTimeout: true, keepTimeoutExtendedOnResume: true });
     return Camera.getPhoto(options).finally(() =>
       this.identityFacadeService.updateVaultTimeout({ extendTimeout: false }));
@@ -22,7 +21,7 @@ export class CameraService {
     const permission = await Camera.checkPermissions();
     const source = cameraSource.toLocaleLowerCase();
     if (/prompt/.test(permission[source])) {
-       await Camera.requestPermissions({ permissions: [<CameraPermissionType>source] });
+      await Camera.requestPermissions({ permissions: [<CameraPermissionType>source] });
     }
   }
 }
--- src/environments/environment-data.ts
+++ src/environments/environment-data.ts
@@ -1,7 +1,7 @@
 
 export class EnvironmentData {
   public static version = {
-    versionNumber: '4.16.19',
+    versionNumber: '4.16.20',
   };
 }
 
