bd3ae6a3c89fade51be003c80e208bad2e6149bc
Julio Oviedo <jbo@cbord.com>
2022-09-15 09:26:31 -0400

    GET-3979 Angular 12 Upgrade

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -163,4 +163,4 @@ export const testCredentials: { [key: string]: UserLogin } = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
