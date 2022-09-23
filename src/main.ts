98ad5df63328b87356c7e6155a38efefaf2eaefb
Julio Oviedo <jbo@cbord.com>
2022-09-23 10:19:07 -0400

    GET-3863 Sentry Init

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,15 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from '@environments/environment';
+import * as Sentry from '@sentry/angular';
 
 if (environment.production) {
   enableProdMode();
+  Sentry.init({
+    dsn: 'https://ff4bd36ce77f48328763d7dfc546466d@o1422282.ingest.sentry.io/6769039'
+  });
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
   .catch(err => console.log(err));
