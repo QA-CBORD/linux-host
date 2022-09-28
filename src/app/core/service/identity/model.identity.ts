228c6429036e1e0fb239f8aba3f5727e91025b89
Delcio Polanco <dmp@cbord.com>
2022-09-28 08:41:09 -0400

    fix issue by decopling logic and using promise

--- src/app/core/service/identity/model.identity.ts
+++ src/app/core/service/identity/model.identity.ts
@@ -1,84 +1,89 @@
-import { Subject} from "rxjs";
+import { firstValueFrom, Subject } from 'rxjs';
 
 export const VAULT_DEFAULT_TIME_OUT_IN_MILLIS = 5000;
 
 export enum PinCloseStatus {
-    SET_SUCCESS = 'set_success',
-    LOGIN_SUCCESS = 'login_success',
-    CANCELED = 'cancelled',
-    ERROR = 'error',
-    MAX_FAILURE = 'max_failure',
-    DEVICE_MARK_LOST = 'device_marked_lost',
-    CLOSED_NO_CONNECTION = 'closed_no_connection'
+  SET_SUCCESS = 'set_success',
+  LOGIN_SUCCESS = 'login_success',
+  CANCELED = 'cancelled',
+  ERROR = 'error',
+  MAX_FAILURE = 'max_failure',
+  DEVICE_MARK_LOST = 'device_marked_lost',
+  CLOSED_NO_CONNECTION = 'closed_no_connection',
 }
 
 export enum PinAction {
-    SET_BIOMETRIC,
-    SET_PIN_ONLY,
-    CHANGE_PIN_BIOMETRIC,
-    CHANGE_PIN_ONLY,
-    LOGIN_PIN,
+  SET_BIOMETRIC,
+  SET_PIN_ONLY,
+  CHANGE_PIN_BIOMETRIC,
+  CHANGE_PIN_ONLY,
+  LOGIN_PIN,
 }
 
 export interface VaultTimeoutOptions {
-    extendTimeout: boolean;
-    estimatedTimeInMillis?: number;
-    keepTimeoutExtendedOnResume?: boolean;
+  extendTimeout: boolean;
+  estimatedTimeInMillis?: number;
+  keepTimeoutExtendedOnResume?: boolean;
 }
 
 export const TIME_OUT_WITH_EXTRA = 600000; // 10 minutes.
 
 export interface VaultMessage {
-    canLock: boolean;
-    canLockOnResume: boolean;
+  canLock: boolean;
+  canLockOnResume: boolean;
 }
 
 export enum VaultMigrateResult {
-    MIGRATION_SUCCESS,
-    MIGRATION_FAILED,
-    MIGRATION_NOT_NEEDED
+  MIGRATION_SUCCESS,
+  MIGRATION_FAILED,
+  MIGRATION_NOT_NEEDED,
 }
 
 export interface VaultSession {
-    pin?: string;
-    biometricUsed?: boolean;
-    isLocked?: boolean;
+  pin?: string;
+  biometricUsed?: boolean;
+  isLocked?: boolean;
 }
-
 
 export class VaultAuthenticator {
-    private onPinSuppliedCb: (pin: string) => void;
-    private onPinModalClosedCb: (status: PinCloseStatus) => void;
-    private pinVerifier: Subject<any> = new Subject();
+  private onPinSuppliedCb: (pin: string) => void;
+  private onPinModalClosedCb: (status: PinCloseStatus) => void;
+  private pinVerifier: Subject<any> = new Subject();
 
-    registerPinSuppliedCb(cb: (pin: string) => void) {
-        this.onPinSuppliedCb = cb;
-    }
+  registerPinSuppliedCb(cb: (pin: string) => void) {
+    this.onPinSuppliedCb = cb;
+  }
 
-    registerPinModalClosedCb(cb: (status: PinCloseStatus) => void) {
-        this.onPinModalClosedCb = cb;
-    }
+  registerPinModalClosedCb(cb: (status: PinCloseStatus) => void) {
+    this.onPinModalClosedCb = cb;
+  }
 
-    async authenticate(pin: string): Promise<void> {
-        this.onPinSuppliedCb(pin);
-        return new Promise((resolve, reject) => {
-            const subscription = this.pinVerifier.subscribe({
-                next: ({ success }) => success ? resolve(success) : reject({ success }),
-                complete: () => subscription.unsubscribe()
-            })
-        });
-    }
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  cbPromise = async (resolve, reject): Promise<void> => {
+    const { success } = await firstValueFrom(this.pinVerifier);
 
-    onPinClosed(status: PinCloseStatus) {
-        this.onPinModalClosedCb(status);
+    if (success) {
+      resolve(success);
+    } else {
+      reject({ success });
     }
+  };
 
-    onPinFailed() {
-        this.pinVerifier.next({ success: false });
-    }
+  async authenticate(pin: string): Promise<void> {
+    this.onPinSuppliedCb(pin);
 
-    onPinSuccess() {
-        this.pinVerifier.next({ success: true });
-    }
+    return new Promise(this.cbPromise);
+  }
+
+  onPinClosed(status: PinCloseStatus) {
+    this.onPinModalClosedCb(status);
+  }
+
+  onPinFailed() {
+    this.pinVerifier.next({ success: false });
+  }
+
+  onPinSuccess() {
+    this.pinVerifier.next({ success: true });
+  }
 }
-
