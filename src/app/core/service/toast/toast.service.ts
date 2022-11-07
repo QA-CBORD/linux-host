7bccd14eeba7ccb5731de80464a0a2b3e5e293eb
Delcio Polanco <dmp@cbord.com>
2022-11-07 13:11:04 -0400

    adding new toast message styles + service new variables + bumping version v4.18.2

--- src/app/core/service/toast/toast.service.ts
+++ src/app/core/service/toast/toast.service.ts
@@ -1,20 +1,19 @@
 import { Injectable } from '@angular/core';
 import { ToastController } from '@ionic/angular';
 
-
 export interface ToastConfig {
-  message: string,
-  toastButtons?: any[],
-  position?: 'top' | 'bottom' | 'middle',
-  duration?: number,
-  onDismiss?: () => any,
+  message: string;
+  toastButtons?: any[];
+  position?: 'top' | 'bottom' | 'middle';
+  duration?: number;
+  onDismiss?: () => any;
+  icon?: string;
+  cssClass?: string;
 }
-
 
 @Injectable({
   providedIn: 'root',
 })
-
 export class ToastService {
   private readonly defaultDuration: number = 3000;
 
@@ -26,21 +25,23 @@ export class ToastService {
       toastButtons: [],
       position: 'top',
       duration: this.defaultDuration,
-      onDismiss: null
+      onDismiss: null,
     };
 
-    config = {...config, ...toastConfig};
+    config = { ...config, ...toastConfig };
 
     const toast = await this.toastController.create({
       message: config.message,
       duration: config.duration,
       buttons: config.toastButtons,
       position: config.position,
+      icon: config.icon,
+      cssClass: config.cssClass,
     });
     toast.setAttribute('role', 'alert');
 
     toast.onDidDismiss().then(() => {
-      if(config.onDismiss){
+      if (config.onDismiss) {
         config.onDismiss();
       }
     });
--- src/environments/environment-data.ts
+++ src/environments/environment-data.ts
@@ -1,6 +1,6 @@
 export class EnvironmentData {
   public static version = {
-    versionNumber: '4.18.1',
+    versionNumber: '4.18.2',
   };
 }
 
