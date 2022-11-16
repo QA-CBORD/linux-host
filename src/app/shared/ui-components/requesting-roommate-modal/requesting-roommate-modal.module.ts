cefa56f151e936c47939b72fcc90c1653f052ae1
axn3 <axn3@cbord.com>
2022-11-16 16:18:40 -0400

    Fixing tag

--- src/app/shared/ui-components/requesting-roommate-modal/requesting-roommate-modal.module.ts
+++ src/app/shared/ui-components/requesting-roommate-modal/requesting-roommate-modal.module.ts
@@ -4,8 +4,9 @@ import { IonicModule } from '@ionic/angular';
 
 import { RequestingRoommateModalComponent } from './requesting-roommate-modal.component';
 import { StHeaderModule } from '../st-header/st-header.module';
+import { StButtonModule } from '../st-button/st-button.module';
 
-export const imports = [CommonModule, IonicModule, StHeaderModule];
+export const imports = [CommonModule, IonicModule, StHeaderModule, StButtonModule];
 export const declarations = [RequestingRoommateModalComponent];
 
 @NgModule({
