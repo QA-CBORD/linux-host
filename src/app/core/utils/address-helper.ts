46f5981720afa9cc4d78da282794617ddc2b44d9
Delcio Polanco <dmp@cbord.com>
2022-12-15 10:11:00 -0400

    GET-3670 fix address builder

--- src/app/core/utils/address-helper.ts
+++ src/app/core/utils/address-helper.ts
@@ -5,7 +5,7 @@ export const getAddressHeader = (address: AddressInfo): string =>
   (address.building ? (address.nickname ? '' : address.building) : '') +
   (address.address1 ? (address.nickname || address.building ? ', ' + address.address1 : address.address1) : '') +
   (address.address2 ? (address.address1 ? ', ' + address.address2 : address.address2) : '') + 
-  (getAddressSubHeader(address) ? (address.address1 ? ', ' + getAddressSubHeader(address) : getAddressSubHeader(address)) : '');
+  (getAddressSubHeader(address) ? ', ' + getAddressSubHeader(address) : '');
 
 export const getAddressSubHeader = ({ city, state, postalcode }: AddressInfo): string =>
   (city ? city : '') + (state ? (city ? ', ' + state : state) : '') + (state && postalcode ? ' ' + postalcode : '');
