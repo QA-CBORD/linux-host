ae7bd5ef74d06b119cd0382a172883351dc01c1d
Neftali Oviedo <nob@cbord.com>
2023-02-09 11:35:28 -0400

    RC-7630

--- src/app/sections/housing/questions/question.component.ts
+++ src/app/sections/housing/questions/question.component.ts
@@ -16,7 +16,7 @@ import { CameraService } from '@sections/settings/pages/services/camera.service'
 import { SessionFacadeService } from '@core/facades/session/session.facade.service';
 import { PhotoUploadService } from '@sections/settings/pages/services/photo-upload.service';
 import { DomSanitizer } from '@angular/platform-browser';
-import {monthNameDayYearHour} from '../../../shared/constants/dateFormats.constant'
+import {montDayYearHour} from '../../../shared/constants/dateFormats.constant'
 
 const IMAGE_DIR = 'stored-images';
 @Component({
@@ -30,7 +30,7 @@ export class QuestionComponent implements OnInit, OnDestroy {
   @Input() name: string;
   @Input() parentGroup: FormGroup;
   @Input() isSubmitted: boolean;
-  dateFormat = monthNameDayYearHour;
+  dateFormat = montDayYearHour;
 
   facilityTreeData: FacilityTree[];
   facilityFullName: string;
