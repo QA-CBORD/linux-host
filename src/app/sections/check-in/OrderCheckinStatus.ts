2d8d48c242897269ce4330ddc443434b542b0218
Delcio Polanco <dmp@cbord.com>
2023-02-23 18:51:37 -0400

    GET-2390 Fix OrderCheckingstatus by identifying status of the order.

--- src/app/sections/check-in/OrderCheckinStatus.ts
+++ src/app/sections/check-in/OrderCheckinStatus.ts
@@ -1,3 +1,5 @@
+import { ORDERING_STATUS_BY_LABEL } from '@sections/ordering/shared/ui-components/recent-oders-list/recent-orders-list-item/recent-orders.config';
+
 export enum CheckinStatuses {
   CHECKED_IN = 1,
   NOT_CHECKED_IN_REMINDER_SENT = 2,
@@ -6,9 +8,12 @@ export enum CheckinStatuses {
 
 export const OrderCheckinStatus = {
   isNotCheckedIn: (status: number): boolean => {
+    const isCaceled = ORDERING_STATUS_BY_LABEL.Canceled.includes(status);
+
     return (
-      status == CheckinStatuses.NOT_CHECKED_IN_REMINDER_SENT ||
-      status == CheckinStatuses.NOT_CHECKED_IN_NO_REMINDER_SENT
+      (status == CheckinStatuses.NOT_CHECKED_IN_REMINDER_SENT ||
+        status == CheckinStatuses.NOT_CHECKED_IN_NO_REMINDER_SENT) &&
+        !isCaceled
     );
   },
 };
