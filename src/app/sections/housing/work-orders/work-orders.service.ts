3a03ff03dcc5b5acec79208502a7d09f90795eb0
Neftali <nob@cbord.com>
2023-03-08 11:54:51 -0400

    GET-3796

--- src/app/sections/housing/work-orders/work-orders.service.ts
+++ src/app/sections/housing/work-orders/work-orders.service.ts
@@ -1,8 +1,8 @@
-import { Injectable, SecurityContext } from "@angular/core";
-import { EnvironmentFacadeService } from "@core/facades/environment/environment.facade.service";
-import { HousingProxyService } from "../housing-proxy.service";
+import { Injectable, SecurityContext } from '@angular/core';
+import { EnvironmentFacadeService } from '@core/facades/environment/environment.facade.service';
+import { HousingProxyService } from '../housing-proxy.service';
 import { Response } from '@sections/housing/housing.model';
-import { of, Observable } from "rxjs";
+import { of, Observable } from 'rxjs';
 import { catchError, map, withLatestFrom, switchMap, take } from 'rxjs/operators';
 import { isSuccessful } from '@sections/housing/utils/is-successful';
 import { QuestionsPage, QUESTIONS_SOURCES } from '../questions/questions.model';
@@ -22,7 +22,7 @@ import { DomSanitizer } from '@angular/platform-browser';
 
 const NOTIFY = {
   YES: 'Yes',
-  NO: 'No'
+  NO: 'No',
 };
 
 const IMAGE_DIR = 'stored-images';
@@ -30,13 +30,12 @@ const IMAGE_DIR = 'stored-images';
   providedIn: 'root',
 })
 export class WorkOrdersService {
-  private workOrderListUrl = `${this._environment.getHousingAPIURL()
-    }/patron-applications/v.1.0/work-orders`;
+  private workOrderListUrl = `${this._environment.getHousingAPIURL()}/patron-applications/v.1.0/work-orders`;
   workOrders: WorkOrder = generateWorkOrders(5);
 
   private readonly MAX_WIDTH = 320;
   private readonly MAX_HEIGHT = 180;
-  private readonly MIME_TYPE = "image/png";
+  private readonly MIME_TYPE = 'image/png';
   private readonly QUALITY = 0.7;
 
   constructor(
@@ -46,8 +45,8 @@ export class WorkOrdersService {
     private _questionsService: QuestionsService,
     private _housingProxyService: HousingProxyService,
     private _workOrderStateService: WorkOrderStateService,
-    private sanitizer: DomSanitizer,
-    ) { }
+    private sanitizer: DomSanitizer
+  ) {}
 
   getWorkOrders(): Observable<WorkOrder> {
     return of(this.workOrders);
@@ -90,23 +89,25 @@ export class WorkOrdersService {
 
   private _getQuestionsPages(workOrderDetails: WorkOrderDetails): QuestionBase[][] {
     // eslint-disable-next-line no-useless-escape
-    const questions: QuestionBase[][] = parseJsonToArray(workOrderDetails.formDefinition.applicationFormJson.slice(0, -1) + `,{\"name\": \"image\",\"type\": \"IMAGE\", \"label\": \"Image\", \"attribute\": null, \"workOrderFieldKey\" : \"IMAGE\", \"requiered\": false ,\"source\":\"WORK_ORDER\"}]`)
-      .map((question: QuestionBase) => {
-        const mappedQuestion = this._toWorkOrderListCustomType(question,workOrderDetails)
-        return [].concat(mappedQuestion);
-      });
+    const questions: QuestionBase[][] = parseJsonToArray(
+      workOrderDetails.formDefinition.applicationFormJson.slice(0, -1) +
+        `,{\"name\": \"image\",\"type\": \"IMAGE\", \"label\": \"Image\", \"attribute\": null, \"workOrderFieldKey\" : \"IMAGE\", \"requiered\": false ,\"source\":\"WORK_ORDER\"}]`
+    ).map((question: QuestionBase) => {
+      const mappedQuestion = this._toWorkOrderListCustomType(question, workOrderDetails);
+      return [].concat(mappedQuestion);
+    });
     return this._questionsService.splitByPages(flat(questions));
   }
 
-  private _toWorkOrderListCustomType(question: QuestionFormControlOptions, workOrderDetails: WorkOrderDetails){
+  private _toWorkOrderListCustomType(question: QuestionFormControlOptions, workOrderDetails: WorkOrderDetails) {
     let values = [];
 
-    if(question.workOrderFieldKey === 'TYPE'){
-      values = workOrderDetails.workOrderTypes.map((v) => {
+    if (question.workOrderFieldKey === 'TYPE') {
+      values = workOrderDetails.workOrderTypes.map(v => {
         return {
           label: v.name,
-          value: v.key
-        }
+          value: v.key,
+        };
       });
       return {
         label: question.label,
@@ -120,12 +121,11 @@ export class WorkOrdersService {
         workOrderField: true,
         workOrderFieldKey: question.workOrderFieldKey,
       };
-    }else if(question.workOrderFieldKey === WorkOrdersFields.LOCATION){
+    } else if (question.workOrderFieldKey === WorkOrdersFields.LOCATION) {
       return this.createFacilityTreeQuestion();
-    }else {
+    } else {
       return question;
     }
-
   }
 
   private _toFormControl(
@@ -138,15 +138,15 @@ export class WorkOrdersService {
 
     const validators = this._questionsService.getRequiredValidator(question);
 
-    if(question.workOrderFieldKey === 'DESCRIPTION'){
-      validators.push(Validators.maxLength(250))
+    if (question.workOrderFieldKey === 'DESCRIPTION') {
+      validators.push(Validators.maxLength(250));
     }
 
     if (question instanceof QuestionTextbox) {
       this._questionsService.addDataTypeValidator(question, validators);
     }
 
-    if(workOrderDetails.workOrderDetails){
+    if (workOrderDetails.workOrderDetails) {
       switch (question.workOrderFieldKey) {
         case WorkOrdersFields.PHONE_NUMBER:
           value = workOrderDetails.workOrderDetails.notificationPhone;
@@ -161,13 +161,13 @@ export class WorkOrdersService {
           value = workOrderDetails.workOrderDetails.facilityKey;
           break;
         case WorkOrdersFields.NOTIFY_BY_EMAIL:
-          value = workOrderDetails.workOrderDetails.notify? NOTIFY.YES : NOTIFY.NO;
+          value = workOrderDetails.workOrderDetails.notify ? NOTIFY.YES : NOTIFY.NO;
           break;
         case WorkOrdersFields.TYPE:
           value = workOrderDetails.workOrderDetails.typeKey;
           break;
         case WorkOrdersFields.IMAGE:
-          this._workOrderStateService.setWorkOrderImage(workOrderDetails.workOrderDetails.attachment)
+          this._workOrderStateService.setWorkOrderImage(workOrderDetails.workOrderDetails.attachment);
           break;
       }
       return new FormControl({ value, disabled: true }, validators);
@@ -176,52 +176,52 @@ export class WorkOrdersService {
     return new FormControl({ value, disabled }, validators);
   }
 
-
   next(): Observable<boolean> {
     return of(true);
   }
 
-  submitWorkOrder(
-    form: WorkOrderDetails,
-    formValue: FormControl): Observable<boolean> {
+  submitWorkOrder(form: WorkOrderDetails, formValue: FormControl): Observable<boolean> {
     const parsedJson: string[] = parseJsonToArray(form.formDefinition.applicationFormJson);
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const workOrdersControls: string[] = parsedJson.filter((control: any) => control && (<QuestionFormControl>control).source === QUESTIONS_SOURCES.WORK_ORDER && control.workOrderField);
+    const workOrdersControls: string[] = parsedJson.filter(
+      (control: any) =>
+        control && (<QuestionFormControl>control).source === QUESTIONS_SOURCES.WORK_ORDER && control.workOrderField
+    );
     const { body, image } = this.buildWorkOrderList(workOrdersControls, formValue);
 
     return this._housingProxyService.post<Response>(this.workOrderListUrl, body).pipe(
       catchError(() => of(false)),
       switchMap((response: Response) => {
-        if(image) return this.sendWorkOrderImage(response.data, image);
+        if (image) return this.sendWorkOrderImage(response.data, image);
 
         return of(true);
       })
     );
   }
 
-  sendWorkOrderImage(workOrderId : number, imageData: ImageData): Promise<boolean> {
+  sendWorkOrderImage(workOrderId: number, imageData: ImageData): Promise<boolean> {
     return new Promise((resolve, reject) => {
       const workOrderImageURL = `${this.workOrderListUrl}/attachments`;
 
       const img = new Image();
 
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      img.onerror = (event) => {
-        reject('error load')
+      img.onerror = event => {
+        reject('error load');
       };
       img.onload = async () => {
         const [newWidth, newHeight] = this.calculateSize(img, this.MAX_WIDTH, this.MAX_HEIGHT);
-        const canvas = document.createElement("canvas");
+        const canvas = document.createElement('canvas');
         canvas.width = newWidth;
         canvas.height = newHeight;
 
-        const context = canvas.getContext("2d");
+        const context = canvas.getContext('2d');
         context.drawImage(img, 0, 0, newWidth, newHeight);
         canvas.toBlob(
-          async (blob) => {
-            const data = await this.convertBlobToBase64(blob) as string;
+          async blob => {
+            const data = (await this.convertBlobToBase64(blob)) as string;
 
-            const attachmentFile = data.replace(/^data:(.*,)?/, '')
+            const attachmentFile = data.replace(/^data:(.*,)?/, '');
             const body = new ImageData({
               filename: imageData.filename,
               comments: 'student submitted attachment',
@@ -231,18 +231,18 @@ export class WorkOrdersService {
             });
 
             this._housingProxyService
-              .post<Response>(workOrderImageURL, body).pipe(take(1))
+              .post<Response>(workOrderImageURL, body)
+              .pipe(take(1))
               .subscribe((response: Response) => {
-                  if (isSuccessful(response.status)) {
-                    this._workOrderStateService.destroyWorkOrderImageBlob();
-                    this.deleteImage();
-                    resolve(true);
-                    return true;
-                  } else {
-                    throw new Error(response.status.message);
-                  }
-                });
-
+                if (isSuccessful(response.status)) {
+                  this._workOrderStateService.destroyWorkOrderImageBlob();
+                  this.deleteImage();
+                  resolve(true);
+                  return true;
+                } else {
+                  throw new Error(response.status.message);
+                }
+              });
           },
           this.MIME_TYPE,
           this.QUALITY
@@ -254,14 +254,15 @@ export class WorkOrdersService {
     });
   }
 
-  private convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
-    const reader = new FileReader;
-    reader.onerror = reject;
-    reader.onload = () => {
-      resolve(reader.result);
-    };
-    reader.readAsDataURL(blob);
-  });
+  private convertBlobToBase64 = (blob: Blob) =>
+    new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = () => {
+        resolve(reader.result);
+      };
+      reader.readAsDataURL(blob);
+    });
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   private calculateSize(img: any, maxWidth: number, maxHeight: number): number[] {
@@ -285,13 +286,13 @@ export class WorkOrdersService {
 
   async deleteImage() {
     await Filesystem.rmdir({
-        directory: FilesystemDirectory.Data,
-        path: `${IMAGE_DIR}`,
-        recursive: true
+      directory: FilesystemDirectory.Data,
+      path: `${IMAGE_DIR}`,
+      recursive: true,
     });
   }
 
-  private createFacilityTreeQuestion(){
+  private createFacilityTreeQuestion() {
     // eslint-disable-next-line no-useless-escape
     const facilityTreeString = `[{\"name\": \"image\",\"type\": \"FACILITY\", \"label\": \"Image\", \"attribute\": null, \"workOrderFieldKey\" : \"FACILITY\", \"requiered\": false ,\"source\":\"WORK_ORDER\"}]`;
     return parseJsonToArray(facilityTreeString);
@@ -302,10 +303,10 @@ export class WorkOrdersService {
     let image: ImageData;
     let location: number;
     const controls: { [key: string]: string } = {
-      [WorkOrdersFields.PHONE_NUMBER]: "",
-      [WorkOrdersFields.DESCRIPTION]: "",
-      [WorkOrdersFields.EMAIL]: "",
-      [WorkOrdersFields.NOTIFY_BY_EMAIL]: "",
+      [WorkOrdersFields.PHONE_NUMBER]: '',
+      [WorkOrdersFields.DESCRIPTION]: '',
+      [WorkOrdersFields.EMAIL]: '',
+      [WorkOrdersFields.NOTIFY_BY_EMAIL]: '',
       [WorkOrdersFields.TYPE]: null,
     };
 
@@ -333,18 +334,21 @@ export class WorkOrdersService {
         }
       });
 
-    return { body: {
-      notificationPhone:  controls[WorkOrdersFields.PHONE_NUMBER] ? controls[WorkOrdersFields.PHONE_NUMBER] : '',
-      notificationEmail: controls[WorkOrdersFields.EMAIL] ? controls[WorkOrdersFields.EMAIL]: '',
-      typeKey: controls[WorkOrdersFields.TYPE],
-      description: controls[WorkOrdersFields.DESCRIPTION],
-      notify: controls[WorkOrdersFields.NOTIFY_BY_EMAIL] === NOTIFY.YES,
-      facilityKey: location,
-      key: null,
-      status: '',
-      statusKey: 0,
-      type: '',
-      requestedDate: '',
-    }, image };
+    return {
+      body: {
+        notificationPhone: controls[WorkOrdersFields.PHONE_NUMBER] ? controls[WorkOrdersFields.PHONE_NUMBER] : '',
+        notificationEmail: controls[WorkOrdersFields.EMAIL] ? controls[WorkOrdersFields.EMAIL] : '',
+        typeKey: controls[WorkOrdersFields.TYPE],
+        description: controls[WorkOrdersFields.DESCRIPTION],
+        notify: controls[WorkOrdersFields.NOTIFY_BY_EMAIL] === NOTIFY.YES,
+        facilityKey: location,
+        key: null,
+        status: '',
+        statusKey: 0,
+        type: '',
+        requestedDate: '',
+      },
+      image,
+    };
   }
 }
