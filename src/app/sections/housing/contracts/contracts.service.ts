30ab64e78bf4f3074f617e1c31ab62d2e77b0190
Neftali <nob@cbord.com>
2023-03-22 12:10:40 -0400

    fix push hook errors

--- src/app/sections/housing/contracts/contracts.service.ts
+++ src/app/sections/housing/contracts/contracts.service.ts
@@ -144,8 +144,7 @@ export class ContractsService {
         value = this._questionsService.getAddressValue(contractDetails.patronAddresses, question);
       } else if (question instanceof QuestionDateSigned) {
         value = this._questionsService.getAttributeValue( question,null,contractDetails.contractInfo.dateTimeAccepted);
-      } 
-      else {
+      } else {
         value = this._questionsService.getAttributeValue( question, contractDetails.patronAttributes) || '';
       }
     }
