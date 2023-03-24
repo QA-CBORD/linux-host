9f2635d6387fb4b2f367c579b504081b65b5926b
Delcio Polanco <dmp@cbord.com>
2023-03-24 07:54:16 -0400

    get-4551 using shared path to fix pipeline

--- src/app/non-authorized/pages/institutions/institutions.module.ts
+++ src/app/non-authorized/pages/institutions/institutions.module.ts
@@ -7,7 +7,7 @@ import { IonicModule } from '@ionic/angular';
 
 import { InstitutionsPage } from './institutions.page';
 import { StHeaderModule } from '@shared/ui-components/st-header/st-header.module';
-import { SearchPipe } from '../../../shared/pipes/search-pipe/search.pipe';
+import { SearchPipe } from '@shared/pipes/search-pipe/search.pipe';
 import { StButtonModule } from '@shared/ui-components/st-button';
 
 const routes: Routes = [
