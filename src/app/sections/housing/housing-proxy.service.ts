7f1add8850730034056c55b90b2fd15035d7e73e
Alan Nunez <axn3@cbord.com>
2023-03-31 18:46:09 -0400

    Cleaning up calls

--- src/app/sections/housing/housing-proxy.service.ts
+++ src/app/sections/housing/housing-proxy.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { map, switchMap } from 'rxjs/operators';
+import { map, switchMap, shareReplay, tap } from 'rxjs/operators';
 import { Response, ResponseStatus } from './housing.model';
 import { AuthFacadeService } from '@core/facades/auth/auth.facade.service';
 
@@ -9,6 +9,7 @@ import { AuthFacadeService } from '@core/facades/auth/auth.facade.service';
   providedIn: 'root',
 })
 export class HousingProxyService {
+  private token$: Observable<string>;
   constructor(private _http: HttpClient, private readonly _authFacadeService: AuthFacadeService) {}
 
   request<T>(apiUrl: string, callback: (headers: HttpHeaders, apiUrl: string) => Observable<T>): Observable<T> {
@@ -23,13 +24,26 @@ export class HousingProxyService {
     );
   }
 
-  get<T>(apiURL: string): Observable<T> {
-    return this.request<T>(apiURL, (headers, apiUrl) =>
-      this._http
-        .get(apiUrl, {
-          headers,
+  getTokenReplay(): Observable<string> {
+    if (!this.token$) {
+      this.token$ = this._authFacadeService.getExternalAuthenticationToken$().pipe(
+        shareReplay(1),
+        tap(() => {
+          setTimeout(() => {
+            this.token$ = null;
+          }, 2000);
         })
-        .pipe(map((response: Response) => response.data))
+      );
+    }
+    return this.token$;
+  }
+
+  get<T>(apiURL: string): Observable<T> {
+    return this.getTokenReplay().pipe(
+      switchMap(token => {
+        const headers = { Authorization: `Bearer ${token}` };
+        return this._http.get(apiURL, { headers }).pipe(map((response: Response) => response.data));
+      })
     );
   }
 
