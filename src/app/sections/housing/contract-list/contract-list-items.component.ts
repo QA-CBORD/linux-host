d7f9c28b844c64e1c205929472705537419a8921
Alan Nunez <axn3@cbord.com>
2023-04-11 13:39:08 -0400

    RC-7853
    The year was getting round up & casual clean up

--- src/app/sections/housing/contract-list/contract-list-items.component.ts
+++ src/app/sections/housing/contract-list/contract-list-items.component.ts
@@ -1,33 +1,23 @@
 import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { ContractSummary } from '@sections/housing/contract-list/contractSummary.model';
 import { ContractListStateService } from '@sections/housing/contract-list/contract-list-state.service';
-import { ActivatedRoute, Router } from '@angular/router';
-import { ToastService } from '@core/service/toast/toast.service';
 import { monthDayYear, hourMinTime } from '@shared/constants/dateFormats.constant'
 
 @Component({
   selector: 'st-contract-list-items',
   templateUrl: './contract-list-items.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrls: ['./contract-list-items.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ContractListItemsComponent implements AfterViewInit {
   @ViewChild('container') divContainer: ElementRef;
-  @Input() contractSummaries: ContractSummary[]
+  @Input() contractSummaries: ContractSummary[];
   dateFormat = monthDayYear;
   timeFormat = hourMinTime;
 
-
-
- ngAfterViewInit() {
-    //helps load ionList that doesnt load unless an event is fired
-   if (this.contractSummaries.length > 0)
-    this.divContainer.nativeElement.click();
- }
-
-  constructor(public roomsStateService: ContractListStateService,
-              private _router: Router,
-              private _activeRoute: ActivatedRoute,
-              private _toastService: ToastService) {
+  ngAfterViewInit() {
+    if (this.contractSummaries.length > 0) this.divContainer.nativeElement.click();
   }
+
+  constructor(public roomsStateService: ContractListStateService) {}
 }
