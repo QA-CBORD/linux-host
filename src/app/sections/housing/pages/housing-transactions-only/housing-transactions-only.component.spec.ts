420082f90299d1ec67f1aaa76657c85b897b5036
AlanNunez <raul.nunez01@gmail.com>
2023-06-12 09:24:18 -0400

    Transaction only unit tests

--- src/app/sections/housing/pages/housing-transactions-only/housing-transactions-only.component.spec.ts
+++ src/app/sections/housing/pages/housing-transactions-only/housing-transactions-only.component.spec.ts
@@ -1,31 +1,56 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { HousingTransactionsOnlyComponent } from './housing-transactions-only.component';
-import { TransactionService } from '@sections/accounts/services/transaction.service';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { AccountService } from '@sections/accounts/services/accounts.service';
-import { AngularDelegate, ModalController, PopoverController } from '@ionic/angular';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ToastService } from '@core/service/toast/toast.service';
+import { AccountService } from '@sections/accounts/services/accounts.service';
+import { TransactionService } from '@sections/accounts/services/transaction.service';
+import { DateUtilObject } from '@sections/accounts/shared/ui-components/filter/date-util';
+import { HousingTransactionsOnlyComponent } from './housing-transactions-only.component';
 
 describe('HousingTransactionsOnlyComponent', () => {
   let component: HousingTransactionsOnlyComponent;
   let fixture: ComponentFixture<HousingTransactionsOnlyComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ HousingTransactionsOnlyComponent ],
-      imports: [HttpClientTestingModule],
-      providers: [TransactionService, AccountService, HttpTestingController, ModalController, AngularDelegate, PopoverController],
-      schemas: [NO_ERRORS_SCHEMA]
-    })
-    .compileComponents();
-
+  beforeEach(() => {
+    const toastServiceStub = () => ({ showToast: object => ({}) });
+    const accountServiceStub = () => ({
+      getUserSettings: requiredSettings => ({}),
+      getUserAccounts: () => ({})
+    });
+    const transactionServiceStub = () => ({
+      getNextTransactionsByAccountId: aLL_ACCOUNTS => ({
+        pipe: () => ({ subscribe: () => ({}) })
+      })
+    });
+    TestBed.configureTestingModule({
+      schemas: [NO_ERRORS_SCHEMA],
+      declarations: [HousingTransactionsOnlyComponent],
+      providers: [
+        { provide: ToastService, useFactory: toastServiceStub },
+        { provide: AccountService, useFactory: accountServiceStub },
+        { provide: TransactionService, useFactory: transactionServiceStub }
+      ]
+    });
     fixture = TestBed.createComponent(HousingTransactionsOnlyComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('can load instance', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('getNextTransactionPackage', () => {
+    it('makes expected calls', () => {
+      const transactionServiceStub: TransactionService = fixture.debugElement.injector.get(
+        TransactionService
+      );
+      jest.spyOn(
+        transactionServiceStub,
+        'getNextTransactionsByAccountId'
+      );
+      component.getNextTransactionPackage();
+      expect(
+        transactionServiceStub.getNextTransactionsByAccountId
+      ).toHaveBeenCalled();
+    });
   });
 });
