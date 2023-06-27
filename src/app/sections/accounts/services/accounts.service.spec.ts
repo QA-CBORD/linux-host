c96a60747453ff87f8e2f8384eade00468e5453d
Delcio Polanco <dmp@cbord.com>
2023-06-27 15:37:10 -0400

    get-5005 adding coverage test

---
+++ src/app/sections/accounts/services/accounts.service.spec.ts
@@ -0,0 +1,297 @@
+import { TestBed } from '@angular/core/testing';
+import { ContentStringsFacadeService } from '@core/facades/content-strings/content-strings.facade.service';
+import { SettingsFacadeService } from '@core/facades/settings/settings-facade.service';
+import { UserAccount } from '@core/model/account/account.model';
+import { SettingInfo } from '@core/model/configuration/setting-info.model';
+import { CommerceApiService } from '@core/service/commerce/commerce-api.service';
+import { BehaviorSubject, Observable, firstValueFrom, of } from 'rxjs';
+import { Settings } from '../../../app.global';
+import { AccountService } from './accounts.service';
+import { ContentStringInfo } from '@core/model/content/content-string-info.model';
+
+describe('AccountService', () => {
+  let service: AccountService;
+  let commerceApiService: CommerceApiService;
+  let contentStringsFacadeService: ContentStringsFacadeService;
+  let settingsFacadeService: SettingsFacadeService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        AccountService,
+        {
+          provide: CommerceApiService,
+          useValue: { getUserAccounts: jest.fn() },
+        },
+        {
+          provide: ContentStringsFacadeService,
+          useValue: { retrieveContentStringListByRequest: jest.fn(), fetchContentString$: jest.fn() },
+        },
+        {
+          provide: SettingsFacadeService,
+          useValue: { getSettings: jest.fn() },
+        },
+      ],
+    });
+
+    service = TestBed.inject(AccountService);
+    commerceApiService = TestBed.inject(CommerceApiService);
+    contentStringsFacadeService = TestBed.inject(ContentStringsFacadeService);
+    settingsFacadeService = TestBed.inject(SettingsFacadeService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  describe('getUserAccounts', () => {
+    it('should call commerceApiService.getUserAccounts and set the accounts', () => {
+      const accounts = [
+        { id: '1', accountDisplayName: 'Account 1', paymentSystemType: 1 },
+        { id: '2', accountDisplayName: 'Account 2', paymentSystemType: 2 },
+      ] as UserAccount[];
+      const getUserAccountsSpy = jest.spyOn(commerceApiService, 'getUserAccounts').mockReturnValue(of(accounts));
+      const setAccountsSpy = jest.spyOn(service['_accounts$'], 'next');
+
+      service.getUserAccounts().subscribe();
+
+      expect(getUserAccountsSpy).toHaveBeenCalled();
+      expect(setAccountsSpy).toHaveBeenCalledWith(accounts);
+    });
+  });
+
+  describe('getUserSettings', () => {
+    it('should call settingsFacadeService.getSettings and set the settings', () => {
+      const settings = [
+        { name: Settings.Setting.ADDRESS_RESTRICTION, value: 'Value 1' },
+        { name: Settings.Setting.ANDROID_MOBILE_CREDENTIAL_ENABLED, value: 'Value 2' },
+      ];
+      const getSettingsSpy = jest.spyOn(settingsFacadeService, 'getSettings').mockReturnValue(of(settings));
+      const setSettingsSpy = jest.spyOn(service['_settings$'], 'next');
+
+      service
+        .getUserSettings([Settings.Setting.ADDRESS_RESTRICTION, Settings.Setting.ANDROID_MOBILE_CREDENTIAL_ENABLED])
+        .subscribe();
+
+      expect(getSettingsSpy).toHaveBeenCalledWith([
+        Settings.Setting.ADDRESS_RESTRICTION,
+        Settings.Setting.ANDROID_MOBILE_CREDENTIAL_ENABLED,
+      ]);
+      expect(setSettingsSpy).toHaveBeenCalledWith(settings);
+    });
+  });
+
+  it('should return accounts as Observable', async () => {
+    const accounts = [
+      { id: '1', accountDisplayName: 'Account 1', paymentSystemType: 1 },
+      { id: '2', accountDisplayName: 'Account 2', paymentSystemType: 2 },
+    ] as UserAccount[];
+    service.accounts$['_accounts$'] = of(accounts);
+
+    const result = service.accounts$;
+    expect(result).toBeInstanceOf(Observable);
+  });
+
+  it('should return settings as Observable', () => {
+    const settings: SettingInfo[] = [
+      { name: 'Setting 1', value: 'Value 1' },
+      { name: 'Setting 2', value: 'Value 2' },
+    ];
+    service._settings$['_settings$'] = of(settings);
+
+    const result: Observable<SettingInfo[]> = service._settings$;
+
+    expect(result).toBeInstanceOf(Observable);
+  });
+
+  it('should return the account with the matching ID', async () => {
+    const accountId = '123';
+    const account: UserAccount = {
+      id: accountId,
+      paymentSystemId: '1',
+      accountDisplayName: 'John Doe',
+      paymentSystemType: 1,
+      accountTender: 'test',
+      accountType: 1,
+      balance: 1,
+      billingAddressId: '1',
+      userId: '2',
+      isActive: true,
+      depositAccepted: true,
+      expirationMonth: 'January',
+      expirationYear: '2019',
+      institutionId: '1234',
+      lastFour: '345',
+      nameOnMedia: 'Test',
+    };
+    const accounts$ = new BehaviorSubject<UserAccount[]>([account]);
+    jest.spyOn(service, 'accounts$', 'get').mockReturnValue(accounts$);
+
+    const result = await firstValueFrom(service.getAccountById(accountId));
+
+    expect(result).toEqual(account);
+  });
+
+  it('should return undefined if no account matches the ID', async () => {
+    const accountId = '123';
+    const accounts$ = new BehaviorSubject<UserAccount[]>([]);
+    jest.spyOn(service, 'accounts$', 'get').mockReturnValue(accounts$);
+
+    const result = await firstValueFrom(service.getAccountById(accountId));
+
+    expect(result).toBeUndefined();
+  });
+
+  it('should return the filtered content strings', () => {
+    const names = ['string1', 'string2', 'string3'];
+    const contentString = {
+      string1: 'Value 1',
+      string2: 'Value 2',
+      string4: 'Value 4',
+    };
+
+    service['contentString'] = contentString;
+
+    const expectedResult = {
+      string1: 'Value 1',
+      string2: 'Value 2',
+    };
+
+    const result = service.getContentStrings(names);
+
+    expect(result).toEqual(expectedResult);
+  });
+
+  it('should return an empty object when no content strings match', () => {
+    const names = ['string4', 'string5', 'string6'];
+    const contentString = {
+      string1: 'Value 1',
+      string2: 'Value 2',
+    };
+
+    service['contentString'] = contentString;
+
+    const expectedResult = {};
+
+    const result = service.getContentStrings(names);
+
+    expect(result).toEqual(expectedResult);
+  });
+
+  it('should return the combined content strings', async () => {
+    const contentString1: ContentStringInfo = {
+      name: 'string1',
+      value: 'Value 1',
+      domain: 'Test',
+      category: 'Test',
+      id: 'Test',
+      description: 'Test',
+      contentMediaType: 1,
+      locale: 'us',
+    };
+    const contentString2: ContentStringInfo = {
+      name: 'string2',
+      value: 'Value 2',
+      domain: 'Test',
+      category: 'Test',
+      id: 'Test',
+      description: 'Test',
+      contentMediaType: 1,
+      locale: 'us',
+    };
+    const contentString3: ContentStringInfo = {
+      name: 'string3',
+      value: 'Value 3',
+      domain: 'Test',
+      category: 'Test',
+      id: 'Test',
+      description: 'Test',
+      contentMediaType: 1,
+      locale: 'us',
+    };
+
+    jest
+      .spyOn(contentStringsFacadeService, 'retrieveContentStringListByRequest')
+      .mockReturnValue(of([contentString1, contentString2]));
+    jest.spyOn(contentStringsFacadeService, 'fetchContentString$').mockReturnValue(of(contentString3));
+
+    const result = await firstValueFrom(service.initContentStringsList());
+    expect(result.length).toEqual(5);
+  });
+
+  it('should return the content value by name', () => {
+    const contentString = {
+      string1: 'Value 1',
+      string2: 'Value 2',
+    };
+    service['contentString'] = contentString;
+
+    const result1 = service.getContentValueByName('string1');
+    const result2 = service.getContentValueByName('string2');
+    const result3 = service.getContentValueByName('string3');
+
+    expect(result1).toEqual('Value 1');
+    expect(result2).toEqual('Value 2');
+    expect(result3).toEqual('');
+  });
+
+  it('should return the setting by name', () => {
+    const settings = [
+      { name: 'setting1', value: 'Value 1' },
+      { name: 'setting2', value: 'Value 2' },
+    ];
+
+    const result1 = service.getSettingByName(settings, 'setting1');
+    const result2 = service.getSettingByName(settings, 'setting2');
+    const result3 = service.getSettingByName(settings, 'setting3');
+
+    expect(result1).toEqual({ name: 'setting1', value: 'Value 1' });
+    expect(result2).toEqual({ name: 'setting2', value: 'Value 2' });
+    expect(result3).toBeUndefined();
+  });
+
+  it('should transform string to array', () => {
+    const value1 = '["item1", "item2", "item3"]';
+    const value2 = '[]';
+    const value3 = 'null';
+
+    const result1 = service.transformStringToArray(value1);
+    const result2 = service.transformStringToArray(value2);
+    const result3 = service.transformStringToArray(value3);
+
+    expect(result1).toEqual(['item1', 'item2', 'item3']);
+    expect(result2).toEqual([]);
+    expect(result3).toEqual([]);
+  });
+
+  it('should filter accounts by display tenders', async () => {
+    const settingsValue = ['tender1', 'tender2'];
+    const settings: SettingInfo[] = [];
+    const accounts = [
+      { id: '1', accountDisplayName: 'Account 1', paymentSystemType: 1, accountTender: 'tender1' },
+      { id: '2', accountDisplayName: 'Account 2', paymentSystemType: 2, accountTender: 'tender2' },
+    ] as UserAccount[];
+
+    jest.spyOn(service, 'getSettingByName').mockReturnValue({ value: JSON.stringify(settingsValue) } as SettingInfo);
+    jest.spyOn(service, 'getSettingByName').mockReturnValue({ value: JSON.stringify(settingsValue) } as SettingInfo);
+    service.settings$['setting$'] = of(settings);
+    service.accounts$['_accounts$'] = of(accounts);
+
+    const results = await firstValueFrom(service.getAccountsFilteredByDepositTenders());
+
+    expect(results.length).toEqual(0);
+  });
+
+  it('should filter accounts by display tenders', async () => {
+    const settings: SettingInfo[] = [
+      { name: 'displayTenders', value: 'tender1' },
+      { name: 'displayTenders', value: 'tender2' },
+    ];
+    jest.spyOn(settingsFacadeService, 'getSettings').mockReturnValue(of(settings));
+    jest.spyOn(service, 'getSettingByName').mockReturnValue(settings[0]);
+    jest.spyOn(service, 'transformStringToArray').mockReturnValue([settings[0]]);
+    const result = await firstValueFrom(service.getAccountsFilteredByDisplayTenders());
+
+    expect(result.length).toEqual(0);
+  });
+});
--- src/app/sections/accounts/services/deposit.service.spec.ts
+++ src/app/sections/accounts/services/deposit.service.spec.ts
@@ -1,69 +1,220 @@
 import { TestBed } from '@angular/core/testing';
-import { CommerceApiService } from 'src/app/core/service/commerce/commerce-api.service';
-import { UserAccount } from 'src/app/core/model/account/account.model';
-import { SettingsFacadeService } from '@core/facades/settings/settings-facade.service';
 import { ContentStringsFacadeService } from '@core/facades/content-strings/content-strings.facade.service';
+import { SettingsFacadeService } from '@core/facades/settings/settings-facade.service';
+import { CommerceApiService } from 'src/app/core/service/commerce/commerce-api.service';
 import { DepositService } from './deposit.service';
+import { firstValueFrom, of } from 'rxjs';
+import { UserAccount } from '@core/model/account/account.model';
+import { Settings } from 'src/app/app.global';
+import { BillMeMapping } from '@core/model/settings/billme-mapping.model';
+import { ContentStringInfo } from '@core/model/content/content-string-info.model';
+import { SettingInfo } from '@core/model/configuration/setting-info.model';
 
 describe('DepositService', () => {
   let service: DepositService;
+  let commerceApiService: CommerceApiService;
+  let contentServiceFacadeService: ContentStringsFacadeService;
+  let settingsFacadeService: SettingsFacadeService;
 
   beforeEach(() => {
-    const commerceApiServiceStub = () => ({
-      getUserAccounts: () => ({ pipe: () => ({}) }),
-      calculateDepositFee: (fromAccountId, toAccountId, amount) => ({}),
-      deposit: (fromAccountId, toAccountId, amount, fromAccountCvv) => ({}),
-      sale: (fromAccountId, amount) => ({})
-    });
-    const settingsFacadeServiceStub = () => ({ getSetting: setting => ({}) });
-    const contentStringsFacadeServiceStub = () => ({
-      retrieveContentStringByConfig: object => ({})
-    });
     TestBed.configureTestingModule({
       providers: [
         DepositService,
-        { provide: CommerceApiService, useFactory: commerceApiServiceStub },
         {
-          provide: SettingsFacadeService,
-          useFactory: settingsFacadeServiceStub
+          provide: CommerceApiService,
+          useValue: {
+            sale: jest.fn(),
+            getUserAccounts: jest.fn(),
+            deposit: jest.fn(),
+            calculateDepositFee: jest.fn()
+          },
         },
         {
           provide: ContentStringsFacadeService,
-          useFactory: contentStringsFacadeServiceStub
-        }
-      ]
+          useValue: {
+            retrieveContentStringByConfig: jest.fn(),
+          },
+        },
+        {
+          provide: SettingsFacadeService,
+          useValue: {
+            getSetting: jest.fn(),
+          },
+        },
+      ],
     });
+
     service = TestBed.inject(DepositService);
+    commerceApiService = TestBed.inject(CommerceApiService);
+    contentServiceFacadeService = TestBed.inject(ContentStringsFacadeService);
+    settingsFacadeService = TestBed.inject(SettingsFacadeService);
   });
 
-  it('can load instance', () => {
+  it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
-  describe('getUserAccounts', () => {
-    it('makes expected calls', () => {
-      const commerceApiServiceStub: CommerceApiService = TestBed.inject(
-        CommerceApiService
-      );
-     jest.spyOn(commerceApiServiceStub, 'getUserAccounts');
-      service.getUserAccounts();
-      expect(commerceApiServiceStub.getUserAccounts).toHaveBeenCalled();
+  it('should get user accounts', () => {
+    const accounts = [
+      { id: '1', accountDisplayName: 'Account 1', paymentSystemType: 1, accountTender: 'tender1' },
+      { id: '2', accountDisplayName: 'Account 2', paymentSystemType: 2, accountTender: 'tender1' },
+    ] as UserAccount[];
+
+    const spy = jest.spyOn(commerceApiService, 'getUserAccounts').mockReturnValue(of(accounts));
+
+    service.getUserAccounts().subscribe(() => {
+      expect(service.accounts$).toBeTruthy();
+      expect(service.accounts$).toEqual(of(accounts));
     });
+
+    expect(spy).toHaveBeenCalled();
   });
 
-  describe('initContentStringsList', () => {
-    it('makes expected calls', () => {
-      const contentStringsFacadeServiceStub: ContentStringsFacadeService = TestBed.inject(
-        ContentStringsFacadeService
-      );
-     jest.spyOn(
-        contentStringsFacadeServiceStub,
-        'retrieveContentStringByConfig'
-      );
-      service.initContentStringsList();
-      expect(
-        contentStringsFacadeServiceStub.retrieveContentStringByConfig
-      ).toHaveBeenCalled();
+  it('should get user settings', () => {
+    const settings = [Settings.Setting.ADDRESS_RESTRICTION, Settings.Setting.APPLE_PAY_ENABLED] as Settings.Setting[];
+    const spy = jest
+      .spyOn(settingsFacadeService, 'getSetting')
+      .mockReturnValue(of({ category: 'text', name: 'text', value: 'text' } as SettingInfo));
+
+    service.getUserSettings(settings).subscribe(() => {
+      expect(service.settings$).toBeTruthy();
+      expect(service.settings$).toEqual(of(settings));
     });
+
+    expect(spy).toHaveBeenCalledTimes(2);
+  });
+
+  it('should filter accounts by payment system type', () => {
+    const accounts = [
+      { id: '1', accountDisplayName: 'Account 1', paymentSystemType: 1, accountTender: 'tender1' },
+      { id: '2', accountDisplayName: 'Account 2', paymentSystemType: 2, accountTender: 'tender1' },
+    ] as UserAccount[];
+
+    const filteredAccounts = service.filterAccountsByPaymentSystem(accounts);
+    expect(filteredAccounts).toBeTruthy();
+    // Perform assertions based on the expected filtered accounts
+  });
+
+  it('should filter credit card destination accounts', () => {
+    const tendersId: string[] = ['tender1', 'tenders2'];
+    const accounts = [
+      { id: '1', accountDisplayName: 'Account 1', paymentSystemType: 1, accountTender: 'tender1' },
+      { id: '2', accountDisplayName: 'Account 2', paymentSystemType: 2, accountTender: 'tender1' },
+    ] as UserAccount[];
+
+    const filteredAccounts = service.filterCreditCardDestAccounts(tendersId, accounts);
+    expect(filteredAccounts).toBeTruthy();
+    // Perform assertions based on the expected filtered accounts
+  });
+
+  it('should filter billMe destination accounts', () => {
+    const billmeMappingArr: BillMeMapping[] = [{ source: 'test', destination: 'destination' }];
+    const accounts = [
+      { id: '1', accountDisplayName: 'Account 1', paymentSystemType: 1 },
+      { id: '2', accountDisplayName: 'Account 2', paymentSystemType: 2 },
+    ] as UserAccount[];
+
+    const filteredAccounts = service.filterBillmeDestAccounts(billmeMappingArr, accounts);
+    expect(filteredAccounts).toBeTruthy();
+    // Perform assertions based on the expected filtered accounts
+  });
+
+  it('should filter billMe source accounts', () => {
+    const billmeMappingArr: BillMeMapping[] = [];
+    const accounts = [
+      { id: '1', accountDisplayName: 'Account 1', paymentSystemType: 1 },
+      { id: '2', accountDisplayName: 'Account 2', paymentSystemType: 2 },
+    ] as UserAccount[];
+
+    const filteredAccounts = service.filterBillmeSourceAccounts(billmeMappingArr, accounts);
+    expect(filteredAccounts).toBeTruthy();
+    // Perform assertions based on the expected filtered accounts
+  });
+
+  it('should get source account for billMe deposit', () => {
+    const selectedAccount = {
+      id: '1',
+      accountDisplayName: 'Account 1',
+      paymentSystemType: 1,
+      accountTender: '1',
+    } as UserAccount;
+    const billmeMappingArr: BillMeMapping[] = [{ source: 'test', destination: 'destination' }];
+
+    service.accounts$['_accounts$'] = of([
+      { id: '1', accountDisplayName: 'Account 1', paymentSystemType: 1 },
+      { id: '2', accountDisplayName: 'Account 2', paymentSystemType: 2 },
+    ] as UserAccount[])
+
+    service.sourceAccForBillmeDeposit(selectedAccount, billmeMappingArr).subscribe(sourceAccount => {
+      expect(sourceAccount).toBeTruthy()
+    });
+
+    expect(service.accounts$).toBeTruthy();
+  });
+
+  it('should calculate deposit fee', () => {
+    const fromAccountId: string = 'fromAccountId';
+    const toAccountId: string = 'toAccountId';
+    const amount: number = 100;
+    const spy = jest.spyOn(commerceApiService, 'calculateDepositFee').mockReturnValue(of(10));
+
+    service.calculateDepositFee(fromAccountId, toAccountId, amount).subscribe(fee => {
+      expect(fee).toBeTruthy();
+      // Perform assertions based on the expected deposit fee
+    });
+
+    expect(spy).toHaveBeenCalledWith(fromAccountId, toAccountId, amount);
+  });
+
+  it('should deposit', () => {
+    const fromAccountId: string = 'fromAccountId';
+    const toAccountId: string = 'toAccountId';
+    const amount: number = 100;
+    const fromAccountCvv: string = '123';
+    const spy = jest.spyOn(commerceApiService, 'deposit').mockReturnValue(of('success'));
+
+    service.deposit(fromAccountId, toAccountId, amount, fromAccountCvv).subscribe(result => {
+      expect(result).toBeTruthy();
+      // Perform assertions based on the expected result
+    });
+
+    expect(spy).toHaveBeenCalledWith(fromAccountId, toAccountId, amount, fromAccountCvv);
+  });
+
+  it('should initialize content strings list', () => {
+    const contentStringInfo: ContentStringInfo[] = [
+      {
+        category: 'text1',
+        description: 'text',
+        name: 'text',
+        value: 'text',
+        id: '1',
+        contentMediaType: 1,
+        domain: 'text',
+        locale: 'es',
+      },
+    ];
+    const spy = jest
+      .spyOn(contentServiceFacadeService, 'retrieveContentStringByConfig')
+      .mockReturnValue(of(contentStringInfo[0]));
+
+    service.initContentStringsList().subscribe(result => {
+      expect(result).toBeTruthy();
+      expect(service.contentString).toBeTruthy();
+      // Perform assertions based on the expected result and content strings
+    });
+
+    expect(spy).toHaveBeenCalledTimes(2);
+  });
+
+  it('should make payment', async () => {
+    const fromAccountId: string = 'fromAccountId';
+    const amount: string = '100';
+    const spy = jest.spyOn(commerceApiService, 'sale').mockReturnValue(of('success'));
+
+    const payment = await firstValueFrom(service.makePayment(fromAccountId, amount));
+
+    expect(payment).toBeTruthy();
+    expect(spy).toHaveBeenCalledWith(fromAccountId, amount);
   });
 });
