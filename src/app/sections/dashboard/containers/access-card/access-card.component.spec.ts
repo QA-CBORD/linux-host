b64502380a37632b50d3ac4243728c8c94c7ae3b
Neftali Oviedo <neftalioviedo@Neftalis-MacBook-Pro.local>
2023-08-01 15:02:34 -0400

    Fixing UT errors

--- src/app/sections/dashboard/containers/access-card/access-card.component.spec.ts
+++ src/app/sections/dashboard/containers/access-card/access-card.component.spec.ts
@@ -1,16 +1,31 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { AccessCardComponent } from "./access-card.component";
+import { AccessCardService } from './services/access-card.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ModalController, PopoverController } from '@ionic/angular';
+import { AndroidPermissions } from '@awesome-cordova-plugins/android-permissions/ngx';
+import { Network } from '@awesome-cordova-plugins/network/ngx';
+import { MobileCredentialFacade } from '@shared/ui-components/mobile-credentials/service/mobile-credential-facade.service';
 
 describe("AccessCardComponent", () => {
   let component: AccessCardComponent;
   let fixture: ComponentFixture<AccessCardComponent>;
-
+  let myService: AccessCardService;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AccessCardComponent],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
-      imports: []
+      providers: [
+        { provide: ModalController, useValue: {} },
+        { provide: PopoverController, useValue: {} },
+        MobileCredentialFacade,
+        AndroidPermissions,
+        Network,
+        AccessCardService
+
+      ],
+      imports: [HttpClientTestingModule]
     }).compileComponents();
   });
 
@@ -19,10 +34,11 @@ describe("AccessCardComponent", () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
 
+    myService = TestBed.inject(AccessCardService);
   });
 
-  describe('Component', () => {
-    it('should exist', () => {
+  describe('method1', () => {
+    it('should ...', () => {
       expect(component).toBeTruthy();
     });
   });
