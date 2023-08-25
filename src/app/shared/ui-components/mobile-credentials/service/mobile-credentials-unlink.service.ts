f41ce0faa66f5a537e1815361a3ad2af5d0bd1bb
Julio Oviedo <jbo@cbord.com>
2023-08-25 11:35:49 -0400

    GET-4418 Retrieve API Key from backend

--- src/app/shared/ui-components/mobile-credentials/service/mobile-credentials-unlink.service.ts
+++ src/app/shared/ui-components/mobile-credentials/service/mobile-credentials-unlink.service.ts
@@ -1,7 +1,11 @@
 import { Injectable, inject } from '@angular/core';
 import { MobileCredentialFacade } from './mobile-credential-facade.service';
-import { Subject, firstValueFrom, map, tap } from 'rxjs';
+import { Subject, catchError, delay, firstValueFrom, map, of, switchMap, tap } from 'rxjs';
 import { MobileCredentialDataService } from '../model/shared/mobile-credential-data.service';
+import { SettingsFacadeService } from '@core/facades/settings/settings-facade.service';
+import { Settings } from 'src/app/app.global';
+
+const UNLINK_REFRESH_DELAY = 3000;
 
 @Injectable({
   providedIn: 'root',
@@ -9,6 +13,7 @@ import { MobileCredentialDataService } from '../model/shared/mobile-credential-d
 export class MobileCredentialsUnlinkService {
   private readonly mobileCredentialFacade: MobileCredentialFacade = inject(MobileCredentialFacade);
   private readonly mobileCredentialDataService: MobileCredentialDataService = inject(MobileCredentialDataService);
+  private readonly settingsFacadeService: SettingsFacadeService = inject(SettingsFacadeService);
 
   private readonly unlinkCredentialsSubject = new Subject<boolean>();
   public readonly unlinkCredentialsRequested$ = this.unlinkCredentialsSubject.asObservable();
@@ -21,14 +26,21 @@ export class MobileCredentialsUnlinkService {
       );
   }
 
-  unlinkCredentials() {
+  unlinkCredentials(): Promise<boolean> {
     return firstValueFrom(
-      this.mobileCredentialDataService
-        .unlinkCredentials$()
-        .pipe(tap(unlinked => {
+      this.settingsFacadeService.getSetting(Settings.Setting.CREDENTIALS_UNLINK_API_KEY).pipe(
+        switchMap(({ value: apiKey }) => this.mobileCredentialDataService.unlinkCredentials$(apiKey)),
+        tap(unlinked => {
           this.mobileCredentialFacade.refreshCredentials();
           this.unlinkCredentialsSubject.next(unlinked);
-        }))
+        }),
+        // Give some time for the Cashless system async call to complete
+        delay(UNLINK_REFRESH_DELAY),
+        catchError(() => {
+          this.unlinkCredentialsSubject.next(false);
+          return of(false);
+        })
+      )
     );
   }
 }
