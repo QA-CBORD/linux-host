808fe6c1158d62411abc176e8a5e89fcfc01e9b2
Neftali Oviedo <neftalioviedo@Neftalis-MacBook-Pro.local>
2023-10-16 13:49:15 -0400

    Finishing first test case

--- tests/pageobjects/accounts.page.ts
+++ tests/pageobjects/accounts.page.ts
@@ -10,6 +10,9 @@ class AccountsPage extends Page {
     get AddFundsButton(){
         return Ionic$.$('/html/body/app-root/ion-app/ion-router-outlet/st-sections/ion-router-outlet/st-accounts/ion-content/div/div/section/st-menu-receiving-funds/nav/ul/li[1]');
     }
+    get AutoDepositsButton(){
+        return Ionic$.$('/html/body/app-root/ion-app/ion-router-outlet/st-sections/ion-router-outlet/st-accounts/ion-content/div/div/section/st-menu-receiving-funds/nav/ul/li[2]');
+    }
 }
 
 export default new AccountsPage();
