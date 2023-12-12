0dfd52e5bf34119b628437f573bfe2442141b727
AlanNunez <raul.nunez01@gmail.com>
2023-12-12 14:26:24 -0400

    Setting up service

--- src/app/core/service/user-notification/user-notification-api.service.ts
+++ src/app/core/service/user-notification/user-notification-api.service.ts
@@ -4,6 +4,23 @@ import { RPCQueryConfig } from '@core/interceptors/query-config.model';
 import { MessageResponse } from '@core/model/service/message-response.model';
 import { Observable, map } from 'rxjs';
 
+export interface UserNotificationLog {
+  institutionId: string;
+  userId: string;
+  title: string;
+  content: string;
+  domain: string;
+  category: UserNotificationLogCategory | number;
+  viewedDate: Date;
+  dismissedDate: Date;
+}
+
+export enum UserNotificationLogCategory {
+  ORDERING = 1,
+  ACCOUNT = 2,
+  ADMIN_NOTICE = 3,
+}
+
 @Injectable({
   providedIn: 'root',
 })
@@ -11,6 +28,43 @@ export class UserNotificationApiService {
   private readonly serviceUrl = '/json/userNotification';
 
   constructor(private readonly http: HttpClient) {}
+
+  createNotification(notification: Partial<UserNotificationLog>): Observable<UserNotificationLog> {
+    const postParams = {
+      title: notification.title,
+      content: notification.content,
+      domain: notification.domain,
+      category: notification.category,
+    };
+    const queryConfig = new RPCQueryConfig('createUserNotificationLog', postParams, true, true);
+    return this.http
+      .post<MessageResponse<UserNotificationLog>>(this.serviceUrl, queryConfig)
+      .pipe(map(({ response }) => response));
+  }
+
+  retrieveAll(): Observable<UserNotificationLog[]> {
+    const queryConfig = new RPCQueryConfig('retrieveAll', { includeViewed: false, includeDismissed: false }, true);
+    return this.http
+      .post<MessageResponse<UserNotificationLog[]>>(this.serviceUrl, queryConfig)
+      .pipe(map(({ response }) => response));
+  }
+
+  retrive(userNotificationLogId: string): Observable<UserNotificationLog> {
+    const queryConfig = new RPCQueryConfig('retrive', { userNotificationLogId }, true);
+    return this.http
+      .post<MessageResponse<UserNotificationLog>>(this.serviceUrl, queryConfig)
+      .pipe(map(({ response }) => response));
+  }
+
+  markAsViewed(userNotificationLogId: string): Observable<Boolean> {
+    const queryConfig = new RPCQueryConfig('markUserNotificationLogAsViewed', { userNotificationLogId }, true);
+    return this.http.post<MessageResponse<Boolean>>(this.serviceUrl, queryConfig).pipe(map(({ response }) => response));
+  }
+
+  markAsDismissed(userNotificationLogId: string): Observable<Boolean> {
+    const queryConfig = new RPCQueryConfig('markUserNotificationLogAsDismissed', { userNotificationLogId }, true);
+    return this.http.post<MessageResponse<Boolean>>(this.serviceUrl, queryConfig).pipe(map(({ response }) => response));
+  }
 
   getUnreadCount(): Observable<number> {
     const queryConfig = new RPCQueryConfig(
--- src/app/sections/notifications/notifications.component.ts
+++ src/app/sections/notifications/notifications.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  UserNotificationApiService,
+  UserNotificationLogCategory,
+} from '@core/service/user-notification/user-notification-api.service';
 import { TranslateService } from '@ngx-translate/core';
+import { first } from 'rxjs';
 
 @Component({
   selector: 'st-notifications',
@@ -7,7 +12,6 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./notifications.component.scss'],
 })
 export class NotificationsComponent implements OnInit {
-
   notificationPeriod = {
     today: this.translateService.instant('patron-ui.notifications.period.today'),
     yesterday: this.translateService.instant('patron-ui.notifications.period.yesterday'),
@@ -16,49 +20,118 @@ export class NotificationsComponent implements OnInit {
   };
 
   //  with Rewards! See what is available to redeem within the Rewards tab.
-   
+
   // ly changed to Dining Cash
   notifications: Notification[] = [
-    { title: 'Breakfast Express', message: 'Order #23424', date: new Date(), icon: NotificationIcon.order, status: true },
-    { title: 'Reward Claimed', message: 'Contratulations you have reached level 5.', date: new Date(), icon: NotificationIcon.reward, status: true },
-    { title: 'Meal Plan', message: 'Your meal plan Meal Account has been succesful.', date: new Date(Date.now() - 24 * 60 * 60 * 1000), icon: NotificationIcon.meal },
-    { title: 'Breakfast Express', message: 'It is going great', date: new Date(Date.now() - 2 * 24 * 60 * 60 * 1000), icon: NotificationIcon.order },
+    {
+      title: 'Breakfast Express',
+      content: 'Order #23424',
+      date: new Date(),
+      icon: NotificationType.order,
+      domain: NotificationType.order,
+      category: UserNotificationLogCategory.ORDERING,
+      status: true,
+    },
+    {
+      title: 'Reward Claimed',
+      content: 'Contratulations you have reached level 5.',
+      date: new Date(),
+      icon: NotificationType.reward,
+      domain: NotificationType.reward,
+      category: UserNotificationLogCategory.ADMIN_NOTICE,
+      status: true,
+    },
+    {
+      title: 'Meal Plan',
+      content: 'Your meal plan Meal Account has been succesful.',
+      date: new Date(Date.now() - 24 * 60 * 60 * 1000),
+      icon: NotificationType.meal,
+      domain: NotificationType.meal,
+      category: UserNotificationLogCategory.ADMIN_NOTICE,
+    },
+    {
+      title: 'Breakfast Express',
+      content: 'It is going great',
+      date: new Date(Date.now() - 2 * 24 * 60 * 60 * 1000),
+      icon: NotificationType.order,
+      domain: NotificationType.order,
+      category: UserNotificationLogCategory.ORDERING,
+    },
     {
       title: 'Photo upload',
       date: new Date(Date.now() - 7 * 24 * 60 * 60 * 1000),
-      icon: NotificationIcon.photoUpload, message: 'Your uploaded photo has been rejected.'
-      
+      icon: NotificationType.photoUpload,
+      content: 'Your uploaded photo has been rejected.',
+      domain: NotificationType.photoUpload,
+      category: UserNotificationLogCategory.ADMIN_NOTICE,
     },
     {
       title: 'Automatic Deposit',
       date: new Date(Date.now() - 11 * 24 * 60 * 60 * 1000),
-      icon: NotificationIcon.automaticDeposit, message: '$100 has been added to your Dining Dollars account.'
+      icon: NotificationType.automaticDeposit,
+      content: '$100 has been added to your Dining Dollars account.',
+      domain: NotificationType.automaticDeposit,
+      category: UserNotificationLogCategory.ORDERING,
     },
     {
       title: 'Guest Deposit Successful',
       date: new Date(Date.now() - 11 * 24 * 60 * 60 * 1000),
-      icon: NotificationIcon.guestDeposit, message: 'A guest deposit of $100 was completed.'
+      icon: NotificationType.guestDeposit,
+      content: 'A guest deposit of $100 was completed.',
+      domain: NotificationType.guestDeposit,
+      category: UserNotificationLogCategory.ORDERING,
     },
     {
       title: 'Low Balance',
       date: new Date(Date.now() - 11 * 24 * 60 * 60 * 1000),
-      icon: NotificationIcon.lowBalance, message: 'Your Dinig Dollars account has low balance'
+      icon: NotificationType.lowBalance,
+      content: 'Your Dinig Dollars account has low balance',
+      domain: NotificationType.lowBalance,
+      category: UserNotificationLogCategory.ORDERING,
     },
     {
       title: 'Breakfast Express',
       date: new Date(Date.now() - 11 * 24 * 60 * 60 * 1000),
-      icon: NotificationIcon.walkOut, message: 'Order Number #334224'
+      icon: NotificationType.walkOut,
+      content: 'Order Number #334224',
+      domain: NotificationType.walkOut,
+      category: UserNotificationLogCategory.ORDERING,
     },
   ];
 
   notificationGroups: NotificationGroup[] = [];
   notificationTitle: string;
 
-  constructor(private readonly translateService: TranslateService) {}
+  constructor(
+    private readonly translateService: TranslateService,
+    private userNotificationApiService: UserNotificationApiService
+  ) {}
 
   ngOnInit() {
     this.groupNotifications();
     this.notificationTitle = this.translateService.instant('patron-ui.notifications.title');
+
+    for (let value of this.notifications) {
+      // this.userNotificationApiService
+      //   .createNotification({
+      //     title: value.title,
+      //     content: value.content,
+      //     domain: value.domain,
+      //     category: value.category,
+      //   })
+      //   .pipe(first())
+
+        // .subscribe(value => {
+        //   console.log('createNotification? ', value);
+        // });
+    }
+
+    this.userNotificationApiService
+      .retrieveAll()
+      .pipe(first())
+      .subscribe(value => {
+        console.log('retrieveAll? ', value);
+      });
   }
 
   groupNotifications() {
@@ -95,7 +168,7 @@ export class NotificationsComponent implements OnInit {
   }
 
   onIonInfinite($event: any) {
-    throw new Error('Method not implemented.');
+    //throw new Error('Method not implemented.');
   }
 
   canShowPeriod(notifications: Notification[]) {
@@ -103,7 +176,7 @@ export class NotificationsComponent implements OnInit {
   }
 }
 
-enum NotificationIcon {
+enum NotificationType {
   order = 'order',
   meal = 'meal',
   reward = 'reward',
@@ -116,9 +189,11 @@ enum NotificationIcon {
 
 interface Notification {
   title: string;
-  message: string;
+  content: string;
+  category: UserNotificationLogCategory;
+  domain: string;
   date: Date;
-  icon?: NotificationIcon;
+  icon?: NotificationType;
   status?: boolean;
 }
 
