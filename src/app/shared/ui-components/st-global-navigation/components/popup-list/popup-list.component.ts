8ed4f58535d1155e60bfe37e8c34c558afdb6b9e
Neftali Oviedo <nob@cbord.com>
2023-12-12 15:09:13 -0400

    GET-4468: Fixing focus event for screenReader on more options popup list

--- src/app/shared/ui-components/st-global-navigation/components/popup-list/popup-list.component.ts
+++ src/app/shared/ui-components/st-global-navigation/components/popup-list/popup-list.component.ts
@@ -1,10 +1,16 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'st-popup-list',
   templateUrl: './popup-list.component.html',
   styleUrls: ['./popup-list.component.scss'],
 })
-export class PopupListComponent {
+export class PopupListComponent implements AfterViewInit {
+  @ViewChild('verticalList') verticalList: ElementRef;
+
   @Output() onBackdropClicked: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+  ngAfterViewInit(): void {
+    this.verticalList.nativeElement.focus();
+  }
 }
