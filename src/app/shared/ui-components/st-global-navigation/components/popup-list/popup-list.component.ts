76c625b0e3cab7b39b2d142d0a5aa76dfe972154
Neftali Oviedo <nob@cbord.com>
2023-12-13 01:01:56 -0400

    GET-4468: Adding coverage for ios devices

--- src/app/shared/ui-components/st-global-navigation/components/popup-list/popup-list.component.ts
+++ src/app/shared/ui-components/st-global-navigation/components/popup-list/popup-list.component.ts
@@ -1,4 +1,5 @@
 import { AfterViewInit, Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { AccessibilityService } from '@shared/accessibility/services/accessibility.service';
 
 @Component({
   selector: 'st-popup-list',
@@ -10,7 +11,13 @@ export class PopupListComponent implements AfterViewInit {
 
   @Output() onBackdropClicked: EventEmitter<boolean> = new EventEmitter<boolean>();
 
+  constructor(private readonly a11yService: AccessibilityService) {}
   ngAfterViewInit(): void {
     this.verticalList.nativeElement.focus();
+  }
+  async isIos() {
+    let isIos = false;
+    await this.a11yService.isVoiceOverEnabled$.then(val => (isIos = val));
+    return isIos;
   }
 }
