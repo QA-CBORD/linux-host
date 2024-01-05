91df4785bc5e862da66f001c50bda344d80e802b
AlanNunez <raul.nunez01@gmail.com>
2024-01-05 14:06:29 -0400

    Handling push notifications

---
+++ android/app/src/main/java/com/cbord/get/RemoteNotification.java
@@ -0,0 +1,20 @@
+package com.cbord.get;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class RemoteNotification extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        Log.d("isSilent: ", remoteMessage.getData().get("isSilent"));
+
+        if (!Boolean.valueOf(remoteMessage.getData().get("isSilent"))) {
+            LocalNotification.showNotifications(this);
+        }
+    }
+}
