39c6253e2fc5ea44227ef9d1846190d38d77eced
Neftali Oviedo <nob@cbord.com>
2024-02-23 10:22:51 -0400

    GET-5686:Fixing modal issue

--- src/app/sections/ordering/shared/ui-components/order-options.action-sheet/order-options.action-sheet.component.ts
+++ src/app/sections/ordering/shared/ui-components/order-options.action-sheet/order-options.action-sheet.component.ts
@@ -246,7 +246,6 @@ export class OrderOptionsActionSheetComponent implements OnInit {
       )
       .subscribe(
         () => {
-          
           this.modalsService.lastActionSheetModal &&
           this.modalsService.dismiss(
             {
