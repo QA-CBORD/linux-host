48efefc477222165108709708a11f6cc46e3df04
Delcio Polanco <dmp@cbord.com>
2024-02-23 09:09:07 -0400

    get-5524 fix unit tests

--- src/app/sections/accounts/pages/deposit-page/deposit-page.component.spec.ts
+++ src/app/sections/accounts/pages/deposit-page/deposit-page.component.spec.ts
@@ -1,86 +1,88 @@
-import { firstValueFrom, of } from "rxjs";
-import { DepositPageComponent } from "./deposit-page.component";
-import { SettingInfo } from "@core/model/configuration/setting-info.model";
-import { FormBuilder, FormGroup } from "@angular/forms";
+import { of } from 'rxjs';
+import { DepositPageComponent } from './deposit-page.component';
+import { SettingInfo } from '@core/model/configuration/setting-info.model';
+import { FormBuilder } from '@angular/forms';
 
 describe('DepositPageComponent', () => {
-    let fixture: DepositPageComponent;
+  let fixture: DepositPageComponent;
 
-    let depositService,
-        fb,
-        popoverCtrl,
-        modalController,
-        toastService,
-        router,
-        loadingService,
-        cdRef,
-        userFacadeService,
-        externalPaymentService,
-        a11yService,
-        commonService,
-        orderingService;
+  let depositService,
+    fb,
+    popoverCtrl,
+    modalController,
+    toastService,
+    router,
+    loadingService,
+    cdRef,
+    userFacadeService,
+    externalPaymentService,
+    commonService,
+    orderingService,
+    appRateService;
 
-    beforeEach(() => {
-        (fb = new FormBuilder()),
-            (orderingService = {}),
-            (a11yService = {}),
-            (commonService = {
-                getString: jest.fn(()=>{
-                    return 'test'
-                })
-            }),
-            (depositService = {
-                getSettingByName: jest.fn(() => {
-                    return 'test'
-                }),
-                settings$: of([{
-                    name: 'test',
-                    value: '1',
-                    category: 'cat'
-                },
-                {
-                    name: 'test2',
-                    value: '3',
-                    category: 'cat 2'
-                }] as SettingInfo[])
-            }),
-            (userFacadeService = {
-                isApplePayEnabled$: jest.fn(() => {
-                    return of(true)
-                }),
-            }),
-            (externalPaymentService = {}),
-            (cdRef = {}),
-            (modalController = {}),
-            (router = {}),
-            (toastService = {}),
-            (popoverCtrl = {}),
-            (loadingService = {
-                showSpinner: jest.fn(),
-                closeSpinner: jest.fn(),
-            });
-        fixture = new DepositPageComponent(
-            depositService,
-            fb,
-            popoverCtrl,
-            modalController,
-            toastService,
-            router,
-            loadingService,
-            cdRef,
-            userFacadeService,
-            externalPaymentService,
-            a11yService,
-            commonService,
-            orderingService
-        );
-        fixture.ngOnInit();
-    });
+  beforeEach(() => {
+    (fb = new FormBuilder()),
+      (orderingService = {}),
+      (commonService = {
+        getString: jest.fn(() => {
+          return 'test';
+        }),
+      }),
+      (depositService = {
+        getSettingByName: jest.fn(() => {
+          return 'test';
+        }),
+        settings$: of([
+          {
+            name: 'test',
+            value: '1',
+            category: 'cat',
+          },
+          {
+            name: 'test2',
+            value: '3',
+            category: 'cat 2',
+          },
+        ] as SettingInfo[]),
+      }),
+      (userFacadeService = {
+        isApplePayEnabled$: jest.fn(() => {
+          return of(true);
+        }),
+      }),
+      (externalPaymentService = {}),
+      (cdRef = {}),
+      (modalController = {}),
+      (router = {}),
+      (toastService = {}),
+      (popoverCtrl = {}),
+      (appRateService = {
+        evaluateToRequestRateApp: jest.fn(),
+      }),
+      (loadingService = {
+        showSpinner: jest.fn(),
+        closeSpinner: jest.fn(),
+      });
+    fixture = new DepositPageComponent(
+      depositService,
+      fb,
+      popoverCtrl,
+      modalController,
+      toastService,
+      router,
+      loadingService,
+      cdRef,
+      userFacadeService,
+      externalPaymentService,
+      commonService,
+      orderingService,
+      appRateService
+    );
+    fixture.ngOnInit();
+  });
 
-
-    describe('DepositPage', () => {
-        it('Should retreive the list of deposit settings', async () => {
-            expect(fixture.depositSettings.length).toBeGreaterThan(0);
-        });
-    });
+  it('Should retreive the list of deposit settings', async () => {
+    expect(fixture.depositSettings.length).toBeGreaterThan(0);
+  });
+  
 });
