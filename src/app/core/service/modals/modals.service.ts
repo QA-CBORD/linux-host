ca02e0706b5c2a5d19c710a99702c4fcde242f78
Neftali Oviedo <nob@cbord.com>
2024-02-23 10:38:24 -0400

    Implementing CR suggestons

--- src/app/core/service/modals/modals.service.ts
+++ src/app/core/service/modals/modals.service.ts
@@ -2,26 +2,36 @@ import { Injectable } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { ModalOptions } from '@ionic/core';
 import { GlobalNavService } from '@shared/ui-components/st-global-navigation/services/global-nav.service';
+import { LoadingService } from '../loading/loading.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ModalsService {
-  constructor(private readonly modalController: ModalController, private readonly globalNav: GlobalNavService) {}
-  private _lastActionSheetModal: HTMLIonModalElement;
-
-  get lastActionSheetModal(): HTMLIonModalElement {
-    return this._lastActionSheetModal;
-  }
+  constructor(
+    private readonly modalController: ModalController,
+    private readonly globalNav: GlobalNavService,
+    private readonly loadingService: LoadingService
+  ) {}
 
   async create(opts: ModalOptions, handleNavBarState?: boolean): Promise<HTMLIonModalElement> {
-    const modal = await this.modalController.create({ handle: false, backdropDismiss: false, cssClass: 'sc-modal', ...opts });
+    const modal = await this.modalController.create({
+      handle: false,
+      backdropDismiss: false,
+      cssClass: 'sc-modal',
+      ...opts,
+    });
     this.bindModalListeners(modal, handleNavBarState);
     return modal;
   }
 
   async createAlert(opts: ModalOptions, handleNavBarState?: boolean): Promise<HTMLIonModalElement> {
-    const modal = await this.modalController.create({ handle: false, backdropDismiss: false, cssClass: 'sc-modal sc-modal-alert', ...opts });
+    const modal = await this.modalController.create({
+      handle: false,
+      backdropDismiss: false,
+      cssClass: 'sc-modal sc-modal-alert',
+      ...opts,
+    });
     this.bindModalListeners(modal, handleNavBarState);
     return modal;
   }
@@ -29,13 +39,13 @@ export class ModalsService {
   async createActionSheet(opts: ModalOptions, handleNavBarState?: boolean): Promise<HTMLIonModalElement> {
     const modal = await this.modalController.create({ handle: false, breakpoints: [1], initialBreakpoint: 1, ...opts });
     this.bindModalListeners(modal, handleNavBarState);
-    this._lastActionSheetModal = modal;
     return modal;
   }
 
-  dismiss(data?: object, role?: string, id?: string): Promise<boolean> {
-    this._lastActionSheetModal = null;
-    return this.modalController.dismiss(data, role, id);
+  async dismiss(data?: object, role?: string, id?: string): Promise<boolean> {
+    this.loadingService.closeSpinner();
+    let topModal = await this.modalController.getTop();
+    return topModal && this.modalController.dismiss(data, role, id);
   }
 
   private bindModalListeners(modal: HTMLIonModalElement, handleNavBarState?: boolean): void {
