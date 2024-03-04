bebd4fd63ea59389098dbabca1ba436b572f91d5
Julio Oviedo <jbo@cbord.com>
2024-03-04 12:25:26 -0400

    GET-4881 Increase coverage for core/services

--- src/app/core/service/barcode/barcode.facade.service.spec.ts
+++ src/app/core/service/barcode/barcode.facade.service.spec.ts
@@ -1,44 +1,70 @@
-import { TestBed } from "@angular/core/testing";
-import { SettingsFacadeService } from "@core/facades/settings/settings-facade.service";
-import { StorageStateService } from "@core/states/storage/storage-state.service";
-import { BarcodeService } from "./barcode.service";
-import { BarcodeFacadeService } from "./barcode.facade.service";
-import { AuthFacadeService } from "@core/facades/auth/auth.facade.service";
+import { TestBed } from '@angular/core/testing';
+import { SettingsFacadeService } from '@core/facades/settings/settings-facade.service';
+import { StorageStateService } from '@core/states/storage/storage-state.service';
+import { BarcodeService } from './barcode.service';
+import { BarcodeFacadeService } from './barcode.facade.service';
+import { AuthFacadeService } from '@core/facades/auth/auth.facade.service';
+import { firstValueFrom, of } from 'rxjs';
 
-describe("BarcodeFacadeService", () => {
+describe('BarcodeFacadeService', () => {
+  const barcodeFromServer = '123456';
+  const barcodeFromCache = '654321';
   let service: BarcodeFacadeService;
-
+  const storageStateServiceStub = {
+    updateStateEntity: jest.fn(),
+    getStateEntityByKey$: jest.fn().mockReturnValue(of(null)),
+  };
   beforeEach(() => {
-    const settingsFacadeServiceStub = () => ({
-      getUserSetting: setting => ({ pipe: () => ({}) }),
-      getSetting: setting => ({ pipe: () => ({}) })
-    });
-    const storageStateServiceStub = () => ({
-      updateStateEntity: (setting, s, object) => ({}),
-      getStateEntityByKey$: key => ({ pipe: () => ({}) })
-    });
+    const settingsFacadeServiceStub = {
+      getUserSetting: jest.fn().mockReturnValue(of({})),
+      getSetting: jest.fn().mockReturnValue(of({})),
+    };
+
     const barcodeServiceStub = () => ({
-      generateBarcode: (userSetting, setting, arg0) => ({})
+      generateBarcode: (userSetting, setting, arg0) => of('654321'),
     });
     const authFacadeMock = {
-      generateBarcodeFromServer: jest.fn().mockReturnValue('123456'),
+      generateBarcodeFromServer: jest.fn().mockReturnValue(of(barcodeFromServer)),
     };
     TestBed.configureTestingModule({
       providers: [
         BarcodeFacadeService,
         {
           provide: SettingsFacadeService,
-          useFactory: settingsFacadeServiceStub
+          useValue: settingsFacadeServiceStub,
         },
         { provide: AuthFacadeService, useValue: authFacadeMock },
-        { provide: StorageStateService, useFactory: storageStateServiceStub },
-        { provide: BarcodeService, useFactory: barcodeServiceStub }
-      ]
+        { provide: StorageStateService, useValue: storageStateServiceStub },
+        { provide: BarcodeService, useFactory: barcodeServiceStub },
+      ],
     });
     service = TestBed.inject(BarcodeFacadeService);
   });
 
-  it("can load instance", () => {
+  it('can load instance', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('generateBarcode with interval', async () => {
+    storageStateServiceStub.getStateEntityByKey$.mockReturnValueOnce(of({ value: false }));
+    const barcode = await firstValueFrom(service.generateBarcode(true));
+    expect(barcode).toBe(barcodeFromServer);
+  });
+
+  it('generateBarcode from server', async () => {
+    storageStateServiceStub.getStateEntityByKey$.mockReturnValueOnce(of({ value: false }));
+    const barcode = await firstValueFrom(service.generateBarcode(false));
+    expect(barcode).toBe(barcodeFromServer);
+  });
+
+  it('generateBarcode without interval', async () => {
+    const barcode = await firstValueFrom(service.generateBarcode(false));
+    expect(barcode).toBe(barcodeFromCache);
+  });
+
+  it('generateBarcode from chached settings', async () => {
+    storageStateServiceStub.getStateEntityByKey$.mockReturnValue(of({ value: true }));
+    const barcode = await firstValueFrom(service.generateBarcode(false));
+    expect(barcode).toBe(barcodeFromCache);
   });
 });
--- src/app/core/service/toast/toast.service.spec.ts
+++ src/app/core/service/toast/toast.service.spec.ts
@@ -4,26 +4,33 @@ import { ToastService } from './toast.service';
 
 describe('ToastService', () => {
   let service: ToastService;
-
+  const toastControllerStub = {
+    create: jest.fn().mockReturnValue({
+      setAttribute: () => ({}),
+      onDidDismiss: jest.fn().mockResolvedValue({}),
+      present: () => ({}),
+      dismiss: () => ({}),
+    }),
+  };
   beforeEach(() => {
-    const toastControllerStub = () => ({
-      create: object => ({
-        setAttribute: () => ({}),
-        onDidDismiss: () => ({ then: () => ({}) }),
-        present: () => ({}),
-        dismiss: () => ({})
-      })
-    });
     TestBed.configureTestingModule({
-      providers: [
-        ToastService,
-        { provide: ToastController, useFactory: toastControllerStub }
-      ]
+      providers: [ToastService, { provide: ToastController, useValue: toastControllerStub }],
     });
     service = TestBed.inject(ToastService);
   });
 
   it('can load instance', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should create toast', async () => {
+    const onDismiss = jest.fn();
+    await service.showSuccessToast({ message: 'test', onDismiss });
+    expect(toastControllerStub.create).toHaveBeenCalled();
+  });
+
+  it('should create error toast', async () => {
+    await service.showError('test');
+    expect(toastControllerStub.create).toHaveBeenCalled();
   });
 });
