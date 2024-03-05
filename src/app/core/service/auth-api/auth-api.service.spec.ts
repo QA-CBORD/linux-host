30f3defcb559157d1caada8f53ee197bc32c0b24
Julio Oviedo <jbo@cbord.com>
2024-03-05 15:35:32 -0400

    GET-4881 Increase coverage for core/services

--- src/app/core/service/auth-api/auth-api.service.spec.ts
+++ src/app/core/service/auth-api/auth-api.service.spec.ts
@@ -1,20 +1,22 @@
 import { TestBed } from '@angular/core/testing';
-import {
-  HttpClientTestingModule,
-  HttpTestingController
-} from '@angular/common/http/testing';
-import { UserLogin } from '@core/model/user';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { AuthApiService } from './auth-api.service';
+import { simpleServiceApiToAssert } from 'src/app/testing/helpers/api-helpers';
 
 describe('AuthApiService', () => {
   let service: AuthApiService;
+  let httpTestingController: HttpTestingController;
+  let serviceAssert: (method: keyof AuthApiService, params?: any[], serviceURL?: string, httpMethod?: string) => void;
+  const serviceUrl = '/json/authentication';
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers: [AuthApiService]
+      providers: [AuthApiService],
     });
     service = TestBed.inject(AuthApiService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+    serviceAssert = simpleServiceApiToAssert(httpTestingController, service);
   });
 
   it('can load instance', () => {
@@ -45,5 +47,56 @@ describe('AuthApiService', () => {
       req.flush([]);
       httpTestingController.verify();
     });
+  });
+
+  it('makes authenticateUser calls', () => {
+    serviceAssert(
+      'authenticateUser',
+      [
+        {
+          userName: 'userCredentials.userName',
+          password: 'userCredentials.password',
+          domain: 'userCredentials.domain',
+          institutionId: 'userCredentials.institutionId',
+        },
+      ],
+      serviceUrl
+    );
+  });
+
+  it('makes authenticateUser for guests calls', () => {
+    serviceAssert(
+      'authenticateUser',
+      [
+        {
+          userName: 'userCredentials.userName',
+          password: 'userCredentials.password',
+          domain: 'userCredentials.domain',
+          institutionId: 'userCredentials.institutionId',
+        },
+        true,
+      ],
+      serviceUrl
+    );
+  });
+
+  it('makes authenticatePin calls', () => {
+    serviceAssert('authenticatePin', [], serviceUrl);
+  });
+
+  it('makes authenticateSessionToken calls', () => {
+    serviceAssert('authenticateSessionToken', [], serviceUrl);
+  });
+
+  it('makes authenticateSessionToken calls', () => {
+    serviceAssert('retrievePatronBarcodePayload', [], serviceUrl);
+  });
+
+  it('makes getExternalAuthenticationToken calls', () => {
+    serviceAssert('getExternalAuthenticationToken', [], serviceUrl);
+  });
+
+  it('makes retrieveAuthorizationBlob calls', () => {
+    serviceAssert('retrieveAuthorizationBlob', [], serviceUrl);
   });
 });
--- src/app/core/service/user-notification/user-notification-api.service.spec.ts
+++ src/app/core/service/user-notification/user-notification-api.service.spec.ts
@@ -1,10 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
 import { UserNotificationApiService } from './user-notification-api.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { simpleApiMethodAssert } from 'src/app/testing/helpers/api-helpers';
 
 describe('UserNotificationApiService', () => {
   let service: UserNotificationApiService;
+  let httpTestingController: HttpTestingController;
+  const serviceURL = '/json/userNotification';
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -12,9 +15,55 @@ describe('UserNotificationApiService', () => {
       providers: [UserNotificationApiService],
     });
     service = TestBed.inject(UserNotificationApiService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('makes createNotification calls', () => {
+    let data = {
+      id: 'notificationId',
+      institutionId: 'institutionId',
+      userId: 'userId',
+      title: 'Notification Title',
+      subtitle: 'Notification Subtitle',
+      content: 'Notification Content',
+      domain: 'Notification Domain',
+      viewedDate: new Date(),
+      dismissedDate: new Date(),
+      insertTime: new Date(),
+      isPinned: true,
+    };
+    simpleApiMethodAssert<UserNotificationApiService>(
+      httpTestingController,
+      service,
+      'createNotification',
+      [data],
+      serviceURL
+    );
+  });
+
+  it('makes retrieveAll calls', () => {
+    simpleApiMethodAssert<UserNotificationApiService>(httpTestingController, service, 'retrieveAll', [], serviceURL);
+  });
+  it('makes retrive calls', () => {
+    simpleApiMethodAssert<UserNotificationApiService>(httpTestingController, service, 'retrive', [], serviceURL);
+  });
+  it('makes markAsViewed calls', () => {
+    simpleApiMethodAssert<UserNotificationApiService>(httpTestingController, service, 'markAsViewed', [], serviceURL);
+  });
+  it('makes markAsDismissed calls', () => {
+    simpleApiMethodAssert<UserNotificationApiService>(httpTestingController, service, 'markAsDismissed', [], serviceURL);
+  });
+  it('makes getUnreadCount calls', () => {
+    simpleApiMethodAssert<UserNotificationApiService>(httpTestingController, service, 'getUnreadCount', [], serviceURL);
+  });
+  it('makes markAllUserNotificationLogAsViewed calls', () => {
+    simpleApiMethodAssert<UserNotificationApiService>(httpTestingController, service, 'markAllUserNotificationLogAsViewed', [], serviceURL);
+  });
+  it('makes markUserNotificationLogAsPinned calls', () => {
+    simpleApiMethodAssert<UserNotificationApiService>(httpTestingController, service, 'markUserNotificationLogAsPinned', [], serviceURL);
   });
 });
