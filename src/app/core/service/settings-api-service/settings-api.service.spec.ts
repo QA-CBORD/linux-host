ddfbcf28ac5ed73b4623236949c018cf74e3762a
Julio Oviedo <jbo@cbord.com>
2024-03-05 15:35:32 -0400

    GET-4881 Increase coverage for core/services

--- src/app/core/service/settings-api-service/settings-api.service.spec.ts
+++ src/app/core/service/settings-api-service/settings-api.service.spec.ts
@@ -1,22 +1,46 @@
 import { TestBed } from '@angular/core/testing';
-import {
-  HttpClientTestingModule,
-  HttpTestingController
-} from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { SettingsApiService } from './settings-api.service';
+import { simpleApiMethodAssert, simpleServiceApiToAssert } from 'src/app/testing/helpers/api-helpers';
 
 describe('SettingsApiService', () => {
   let service: SettingsApiService;
+  let httpTestingController: HttpTestingController;
+  const serviceURL = '/json/user';
+  const serviceConfigURL = '/json/configuration';
+  let serviceAssert: (method: keyof typeof service, params?: any[], serviceURL?: string, httpMethod?: string) => void;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers: [SettingsApiService]
+      providers: [SettingsApiService],
     });
     service = TestBed.inject(SettingsApiService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+    serviceAssert = simpleServiceApiToAssert(httpTestingController, service);
   });
 
   it('can load instance', () => {
     expect(service).toBeTruthy();
+  });
+  it('makes getSetting calls', () => {
+    serviceAssert('getSetting', ['test.test.test', '123456', '123456'], serviceConfigURL);
+  });
+  it('makes getSettings calls', () => {
+    serviceAssert('getSettings', [['test.test.test']], serviceConfigURL);
+  });
+  it('makes getSettingList calls', () => {
+    serviceAssert('getSettingList', [['test.test.test'], '123456', '123456'], serviceConfigURL);
+  });
+  it('makes deleteUserSetting calls', () => {
+    serviceAssert('deleteUserSetting', [], serviceURL);
+  });
+
+  it('makes saveUserSetting calls', () => {
+    serviceAssert('saveUserSetting', [], serviceURL);
+  });
+
+  it('makes getUserSetting calls', () => {
+    serviceAssert('getUserSetting', [], serviceURL);
   });
 });
--- src/app/core/service/user-api/user-api.service.spec.ts
+++ src/app/core/service/user-api/user-api.service.spec.ts
@@ -1,22 +1,20 @@
 import { TestBed } from '@angular/core/testing';
-import {
-  HttpClientTestingModule,
-  HttpTestingController
-} from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { UserInfo } from '@core/model/user/user-info.model';
-import { UserPhotoInfo } from '@core/model/user';
-import { UserNotificationInfo } from '@core/model/user';
 import { UserApiService } from './user-api.service';
+import { simpleApiMethodAssert } from 'src/app/testing/helpers/api-helpers';
 
 describe('UserApiService', () => {
   let service: UserApiService;
-
+  let httpTestingController: HttpTestingController;
+  const serviceURL = '/json/user';
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers: [UserApiService]
+      providers: [UserApiService],
     });
     service = TestBed.inject(UserApiService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('can load instance', () => {
@@ -26,11 +24,10 @@ describe('UserApiService', () => {
   describe('updateUserInfo$', () => {
     it('makes expected calls', () => {
       const userInfoStub: UserInfo = <any>{};
-      const httpTestingController = TestBed.inject(HttpTestingController);
       service.updateUserInfo$(userInfoStub).subscribe(res => {
         expect(res).toEqual(userInfoStub);
       });
-      const req = httpTestingController.expectOne('/json/user');
+      const req = httpTestingController.expectOne(serviceURL);
       expect(req.request.method).toEqual('POST');
       req.flush(userInfoStub);
       httpTestingController.verify();
@@ -39,11 +36,10 @@ describe('UserApiService', () => {
 
   describe('getUser', () => {
     it('makes expected calls', () => {
-      const httpTestingController = TestBed.inject(HttpTestingController);
       service.getUser().subscribe(res => {
         expect(res).toEqual({});
       });
-      const req = httpTestingController.expectOne('/json/user');
+      const req = httpTestingController.expectOne(serviceURL);
       expect(req.request.method).toEqual('POST');
       req.flush([]);
       httpTestingController.verify();
@@ -52,16 +48,87 @@ describe('UserApiService', () => {
 
   describe('getUserAddresses', () => {
     it('makes expected calls', () => {
-      const httpTestingController = TestBed.inject(HttpTestingController);
-     jest.spyOn(service, 'getUser');
+      jest.spyOn(service, 'getUser');
       service.getUserAddresses().subscribe(res => {
         expect(res).toEqual({});
       });
       expect(service.getUser).toHaveBeenCalled();
-      const req = httpTestingController.expectOne('/json/user');
+      const req = httpTestingController.expectOne(serviceURL);
       expect(req.request.method).toEqual('POST');
       req.flush([]);
       httpTestingController.verify();
     });
+  });
+
+  it('makes getUserSettings calls', () => {
+    simpleApiMethodAssert<UserApiService>(
+      httpTestingController,
+      service,
+      'getUserSettings',
+      [{ settingName: '' }, ''],
+      serviceURL
+    );
+  });
+  it('makes saveUserSetting calls', () => {
+    simpleApiMethodAssert<UserApiService>(
+      httpTestingController,
+      service,
+      'saveUserSetting',
+      [{ settingName: '' }, ''],
+      serviceURL
+    );
+  });
+  it('makes getUserPhoto calls', () => {
+    simpleApiMethodAssert<UserApiService>(httpTestingController, service, 'getUserPhoto', [], serviceURL);
+  });
+  it('makes addUserPhoto calls', () => {
+    simpleApiMethodAssert<UserApiService>(httpTestingController, service, 'addUserPhoto', [], serviceURL);
+  });
+  it('makes updateUserPhotoStatus calls', () => {
+    simpleApiMethodAssert<UserApiService>(httpTestingController, service, 'updateUserPhotoStatus', [], serviceURL);
+  });
+  it('makes createUserPin calls', () => {
+    simpleApiMethodAssert<UserApiService>(httpTestingController, service, 'createUserPin', [], serviceURL);
+  });
+  it('makes requestDeposit calls', () => {
+    simpleApiMethodAssert<UserApiService>(httpTestingController, service, 'requestDeposit', [], serviceURL);
+  });
+  it('makes getPhotoListByUserId calls', () => {
+    simpleApiMethodAssert<UserApiService>(httpTestingController, service, 'getPhotoListByUserId', [], serviceURL);
+  });
+  it('makes getFullPhotoListByUserId calls', () => {
+    simpleApiMethodAssert<UserApiService>(httpTestingController, service, 'getFullPhotoListByUserId', [], serviceURL);
+  });
+  it('makes saveNotification$ calls', () => {
+    simpleApiMethodAssert<UserApiService>(httpTestingController, service, 'saveNotification$', [], serviceURL);
+  });
+  it('makes logoutAndRemoveUserNotification$ calls', () => {
+    simpleApiMethodAssert<UserApiService>(
+      httpTestingController,
+      service,
+      'logoutAndRemoveUserNotification$',
+      [],
+      serviceURL
+    );
+  });
+  it('makes reportCard as found calls', () => {
+    simpleApiMethodAssert<UserApiService>(httpTestingController, service, 'reportCard$', [false], serviceURL);
+  });
+  it('makes reportCard as found calls', () => {
+    simpleApiMethodAssert<UserApiService>(httpTestingController, service, 'reportCard$', [true], serviceURL);
+  });
+
+  it('makes changePassword$ calls', () => {
+    simpleApiMethodAssert<UserApiService>(httpTestingController, service, 'changePassword$', ['', ''], serviceURL);
+  });
+
+  it('makes retrieveUserIdByCashlessFields calls', () => {
+    simpleApiMethodAssert<UserApiService>(
+      httpTestingController,
+      service,
+      'retrieveUserIdByCashlessFields',
+      ['', ''],
+      serviceURL
+    );
   });
 });
