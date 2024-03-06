293d88eca7f2b16baa2d0200540f71ea3d840038
Julio Oviedo <jbo@cbord.com>
2024-03-06 08:37:25 -0400

    GET-5763 Update pronouns list items

--- src/app/shared/ui-components/phone-email/components/personal-info-pronouns/personal-info-pronouns.component.spec.ts
+++ src/app/shared/ui-components/phone-email/components/personal-info-pronouns/personal-info-pronouns.component.spec.ts
@@ -1,14 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PersonalInfoPronounsComponent } from './personal-info-pronouns.component';
+import { SessionFacadeService } from '@core/facades/session/session.facade.service';
 
 describe('PersonalInfoPronounsComponent', () => {
   let component: PersonalInfoPronounsComponent;
   let fixture: ComponentFixture<PersonalInfoPronounsComponent>;
-
+  let _sessionService = {
+    getIsWeb: jest.fn(() => true),
+  };
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [PersonalInfoPronounsComponent]
+      imports: [PersonalInfoPronounsComponent],
+      providers: [{ provide: SessionFacadeService, useValue: _sessionService }],
     });
     fixture = TestBed.createComponent(PersonalInfoPronounsComponent);
     component = fixture.componentInstance;
