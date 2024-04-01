c2f7a2574c5de939649690e894391c600bbd7f9f
Neftali Oviedo <nob@cbord.com>
2024-04-01 16:07:33 -0400

    Adding UI and mucked values to cart-preview compoenent

--- src/app/sections/ordering/components/cart-preview/cart-preview.component.ts
+++ src/app/sections/ordering/components/cart-preview/cart-preview.component.ts
@@ -1,16 +1,46 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { ModalsService } from '@core/service/modals/modals.service';
 import { IonicModule } from '@ionic/angular';
 import { TranslateModule } from '@ngx-translate/core';
-import { CartService } from '@sections/ordering/services';
-import { OrderInfo, OrderItem } from '@sections/ordering/shared';
+// import { CartService } from '@sections/ordering/services';
+import {  OrderItem } from '@sections/ordering/shared';
 import { PriceUnitsResolverModule } from '@sections/ordering/shared/pipes/price-units-resolver/price-units-resolver.module';
 import { PriceUnitsResolverPipe } from '@sections/ordering/shared/pipes/price-units-resolver/price-units-resolver.pipe';
 import { OrderItemDetailsModule } from '@sections/ordering/shared/ui-components/order-item-details/order-item-details.module';
 import { StHeaderModule } from '@shared/ui-components';
-import { Observable } from 'rxjs';
 
+const orderItem: OrderItem = {
+  id: '1',
+  orderId: '2',
+  menuItemId: '3',
+  parentOrderItemId: '5',
+  quantity: 4,
+  displayRank: 5,
+  salePrice: 10,
+  specialInstructions: '',
+  keyedName: 'Orange Juice',
+  status: 1,
+  name: 'Orange Juice',
+  reportingCategory: '',
+  optionType: '',
+  orderItemOptions: [{
+    id: '1',
+    orderId: '2',
+    menuItemId: '3',
+    parentOrderItemId: '5',
+    quantity: 4,
+    displayRank: 5,
+    salePrice: 10,
+    specialInstructions: '',
+    keyedName: 'Oranges',
+    status: 1,
+    name: 'Oranges',
+    reportingCategory: '',
+    optionType: '',
+    orderItemOptions: [],
+  }],
+};
 @Component({
   standalone: true,
   providers: [PriceUnitsResolverPipe],
@@ -26,9 +56,9 @@ import { Observable } from 'rxjs';
     CommonModule,
   ],
 })
-export class CartPreviewComponent {
+export class CartPreviewComponent implements OnInit{
   private readonly modalService = inject(ModalsService);
-  private readonly cartService = inject(CartService);
+  // private readonly cartService = inject(CartService);
 
   orderItems: OrderItem[] = [];
   mealBased: boolean;
@@ -39,43 +69,8 @@ export class CartPreviewComponent {
   };
 
   ngOnInit(): void {
-    // this.cartService.orderInfo$.subscribe((orderInfo: OrderInfo) => {
-    //   this.merchantName = orderInfo.merchantName;
-    // });
     this.merchantName = 'Breaskfast Express';
     this.mealBased = false;
-    let p: OrderItem = {
-      id: '1',
-      orderId: '2',
-      menuItemId: '3',
-      parentOrderItemId: '5',
-      quantity: 4,
-      displayRank: 5,
-      salePrice: 10,
-      specialInstructions: '',
-      keyedName: 'Orange Juice',
-      status: 1,
-      name: 'Orange Juice',
-      reportingCategory: '',
-      optionType: '',
-      orderItemOptions: [{
-        id: '1',
-        orderId: '2',
-        menuItemId: '3',
-        parentOrderItemId: '5',
-        quantity: 4,
-        displayRank: 5,
-        salePrice: 10,
-        specialInstructions: '',
-        keyedName: 'Oranges',
-        status: 1,
-        name: 'Oranges',
-        reportingCategory: '',
-        optionType: '',
-        orderItemOptions: [],
-      }],
-    };
-
-    this.orderItems = [p, p, p, p, p];
+    this.orderItems = [orderItem, orderItem, orderItem, orderItem, orderItem];
   }
 }
