d52fb253e01b4ba2c145ed40472b2f34d820f967
Delcio Polanco <dmp@cbord.com>
2024-04-04 14:19:55 -0400

    get-5753 adding sevendays constant

--- src/app/shared/constants/dateFormats.constant.ts
+++ src/app/shared/constants/dateFormats.constant.ts
@@ -2,4 +2,4 @@ export const montDayYearHour = 'MM/dd/yy h:mm a';
 export const monthDayYear = 'MM/dd/yy';
 export const monthDayFullYear = 'M/d/yyyy';
 export const hourMinTime = 'h:mm a';
-
+export const sevenDays = 7 * 24 * 60 * 60 * 1000;
