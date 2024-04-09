6ce121b00dfc73466e9f8ac896ef1e2d220faa2a
Delcio Polanco <dmp@cbord.com>
2024-04-09 08:07:57 -0400

    get-5895 adding controlled to the omitedErrors list

--- src/app/core/utils/global-error-handler.ts
+++ src/app/core/utils/global-error-handler.ts
@@ -5,7 +5,8 @@ import * as Sentry from '@sentry/angular-ivy';
 
 const OmitedErrorsForSentry = [
   'Error: Invalid session',
-  'Non-Error exception captured with keys found when loading the app'
+  'Non-Error exception captured with keys found when loading the app',
+  '9017,Order can not be processed for the given due time, it exceeds the merchants order capacity',
 ];
 
 @Injectable()
