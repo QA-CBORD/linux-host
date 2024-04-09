7df097e8862b687a0547b71a304b3cef1d1d85f2
Neftali Oviedo <nob@cbord.com>
2024-04-09 16:45:04 -0400

    GET-5821:Reaching 80% of coverage for ordering/pages/item-detail

--- src/app/sections/ordering/pages/item-detail/item-detail.component.spec.ts
+++ src/app/sections/ordering/pages/item-detail/item-detail.component.spec.ts
@@ -16,7 +16,7 @@ import { OrderingService } from '@sections/ordering/services/ordering.service';
 import { NavigationService } from '@shared/services/navigation.service';
 import { PriceUnitsResolverModule } from '@sections/ordering/shared/pipes/price-units-resolver/price-units-resolver.module';
 import { PriceUnitsResolverPipe } from '@sections/ordering/shared/pipes/price-units-resolver/price-units-resolver.pipe';
-import { of } from 'rxjs';
+import { of, throwError } from 'rxjs';
 import { CartService } from '@sections/ordering/services';
 import { ItemDetailModalComponent } from './components/item-detail-modal/item-detail-modal.component';
 import { LOCAL_ROUTING } from '@sections/ordering/ordering.config';
@@ -25,7 +25,7 @@ import { MenuGroupInfo, MenuItemInfo, MenuItemOptionInfo, OrderItem } from '@sec
 import { TranslateService } from '@ngx-translate/core';
 // Mock Pipe decorator
 let menuItem: MenuItemInfo = {
-  id: '12',
+  id: '1',
   merchantId: '',
   externalSystemRef: '',
   name: '',
@@ -54,7 +54,11 @@ let menuItem: MenuItemInfo = {
         priceOverride: 0,
         visible: false,
         active: false,
-        menuGroupItems: [],
+        menuGroupItems: [
+          {
+            menuItem: { id: '1', name: 'Option 1', price: 10 },
+          },
+        ] as any[],
       },
       menuItemId: '1',
       displayRank: 0,
@@ -123,7 +127,7 @@ const activatedRouteMock = {
   data: of({
     menuItem: {
       menuItem: { id: '1', name: 'Test Item', price: 10 },
-      queryParams: { orderItemId: 'mockOrderItemId' },
+      queryParams: { orderItemId: '123' },
     },
   }),
 };
@@ -138,6 +142,7 @@ const cartServiceMock = {
   cartsErrorMessage: null,
   menuItems$: of(5),
   orderInfo$: of({ checkNumber: 123 }),
+  removeLastOrderItem: jest.fn(),
 };
 
 const popoverControllerMock = {
@@ -153,8 +158,13 @@ let translateService = {
 describe('ItemDetailComponent', () => {
   let component: ItemDetailComponent;
   let fixture: ComponentFixture<ItemDetailComponent>;
+  let itemOrderFormMock: Partial<FormGroup>;
 
   beforeEach(async () => {
+    itemOrderFormMock = {
+      invalid: false,
+      value: {},
+    };
     TestBed.configureTestingModule({
       declarations: [ItemDetailComponent],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
@@ -180,6 +190,7 @@ describe('ItemDetailComponent', () => {
       option1: new FormControl(''),
     });
     component.order = { counter: 1, totalPrice: 0, optionsPrice: 0 };
+    // component.itemOrderForm = itemOrderFormMock as FormGroup;
     fixture.detectChanges();
   });
 
@@ -262,6 +273,12 @@ describe('ItemDetailComponent', () => {
     expect(component.order.counter).toBe(1); // Assuming initial counter is 1
     // Assuming calculateTotalPrice() updates the total price
   });
+  it('should decrement counter and recalculate total price when removing items and order counter bigger than 1', () => {
+    component.order.counter = 2;
+    component.removeItems();
+    expect(component.order.counter).toBe(1); // Assuming initial counter is 1
+    // Assuming calculateTotalPrice() updates the total price
+  });
 
   it('should increment counter and recalculate total price when adding items', () => {
     component.addItems();
@@ -272,13 +289,17 @@ describe('ItemDetailComponent', () => {
   it('should configure menu item correctly', () => {
     const menuItemId = 'menuItemId';
     const quantity = 2;
+    const itemName = 'Banana';
+    const itemPrice = 10;
 
-    const configuredMenuItem = component['configureMenuItem'](menuItemId, quantity);
+    const configuredMenuItem = component['configureMenuItem'](menuItemId, quantity, itemName, itemPrice);
 
     expect(configuredMenuItem).toEqual({
       menuItemId: 'menuItemId',
       orderItemOptions: [],
       quantity: 2,
+      name: 'Banana',
+      salePrice: 10,
     });
   });
   it('should call showAddedItemsQuantity with correct value', () => {
@@ -383,7 +404,52 @@ describe('ItemDetailComponent', () => {
       expect(calculateTotalPriceMock).toHaveBeenCalledTimes(1);
       expect(calculateTotalPriceMock).toHaveBeenCalledWith();
     });
+    it('should evaluate null formValue', () => {
+      const calculateTotalPriceMock = jest.fn();
+      const sourceSubscriptionMock = {
+        add: jest.fn(),
+      };
+      const formValue = {
+        option1: [{ price: 10 }, { price: 20 }, { price: 30 }],
+        option2: null,
+      };
 
+      const itemOrderFormMock = {
+        valueChanges: of(formValue),
+      };
+
+      component['valueChanges'].call({
+        itemOrderForm: itemOrderFormMock,
+        calculateTotalPrice: calculateTotalPriceMock,
+        sourceSubscription: sourceSubscriptionMock,
+        order: { optionsPrice: 0 },
+      });
+
+      expect(calculateTotalPriceMock).toHaveBeenCalledTimes(1);
+    });
+    it('should evaluate id containing formValue', () => {
+      const calculateTotalPriceMock = jest.fn();
+      const sourceSubscriptionMock = {
+        add: jest.fn(),
+      };
+      const formValue = {
+        option1: [{ price: 10 }, { price: 20 }, { price: 30 }],
+        option2: { id: '1', price: 15 },
+      };
+
+      const itemOrderFormMock = {
+        valueChanges: of(formValue),
+      };
+
+      component['valueChanges'].call({
+        itemOrderForm: itemOrderFormMock,
+        calculateTotalPrice: calculateTotalPriceMock,
+        sourceSubscription: sourceSubscriptionMock,
+        order: { optionsPrice: 0 },
+      });
+
+      expect(calculateTotalPriceMock).toHaveBeenCalledTimes(1);
+    });
     it('should update order optionsPrice when form value contains array of strings', () => {
       const formValue = {
         option1: ['string1', 'string2', 'string3'],
@@ -443,25 +509,56 @@ describe('ItemDetailComponent', () => {
     component.showAddedItemsQuantity(5);
     expect(toastServiceMock.showSuccessToast).toHaveBeenCalledTimes(2);
   });
+  it('should show success toast with correct message when one item is added', () => {
+    component.showAddedItemsQuantity(1);
+    expect(toastServiceMock.showSuccessToast).toHaveBeenCalled();
+  });
+  it('should display first message ', () => {
+    cartServiceMock.orderInfo$ = of({ checkNumber: null });
+    component.showAddedItemsQuantity(1);
+    expect(toastServiceMock.showSuccessToast).toHaveBeenCalled();
+  });
 
-  it('should initForm correctly', () => {
+  it('should initForm correctly else', () => {
     component.routesData = {
       menuItem,
       queryParams: { orderItemId: 'mockOrderItemId', isItemExistsInCart: true },
     } as RoutesData;
     component.menuItem = menuItem;
+    component.cartSelectedItem = { id: '1', name: 'Test Item', price: 10,specialInstructions: 'instruction 1' } as unknown as OrderItem;
     component.cartOrderItemOptions = [
       {
         id: '1',
         name: 'Option 1',
+        specialInstructions: 'instruction 2',
         menuGroup: { name: 'option1', minimum: 1, maximum: 1 } as MenuGroupInfo,
       },
       {
         id: '2',
         name: 'Option 2',
+        
         menuGroup: { name: 'option2', minimum: 1, maximum: 2 } as MenuGroupInfo,
+        
       },
     ] as unknown as OrderItem[];
+
+    component['initForm']();
+
+    expect(component.itemOrderForm).toBeTruthy();
+    expect(component.itemOrderForm.get('option1')).toBeTruthy();
+    expect(component.itemOrderForm.get('option2')).toBeTruthy();
+  });
+  it('should initForm correctly initial if', () => {
+    component.routesData = {
+      menuItem,
+      queryParams: { orderItemId: 'mockOrderItemId', isItemExistsInCart: true },
+    } as RoutesData;
+    component.menuItem = menuItem;
+    component.cartOrderItemOptions = {
+      id: '1',
+      name: 'Option 1',
+      menuGroup: { name: 'option1', minimum: 1, maximum: 1 } as MenuGroupInfo,
+    } as unknown as OrderItem[];
 
     component['initForm']();
 
@@ -488,5 +585,149 @@ describe('ItemDetailComponent', () => {
     component['initForm']();
 
     expect(component.itemOrderForm).toBeTruthy();
+  });
+
+  it('should call the necessary methods when onSubmit is called', async () => {
+    const menuItem = {};
+
+    component.routesData = {
+      queryParams: { orderItemId: '123' },
+    } as RoutesData;
+
+    cartServiceMock.validateOrder.mockReturnValueOnce(of(null));
+
+    await component['onSubmit'](menuItem);
+
+    expect(cartServiceMock.addOrderItems).toHaveBeenCalledWith(menuItem);
+    expect(cartServiceMock.validateOrder).toHaveBeenCalled();
+    expect(loadingServiceMock.showSpinner).toHaveBeenCalled();
+    expect(loadingServiceMock.closeSpinner).toHaveBeenCalled();
+  });
+
+  it('should handle errors returned from validateOrder', async () => {
+    const menuItem = {};
+
+    component.routesData = {
+      queryParams: { orderItemId: '123' },
+    } as RoutesData;
+
+    const errorMessage = 'Validation error';
+    cartServiceMock.validateOrder.mockReturnValueOnce(throwError(errorMessage));
+
+    await component['onSubmit'](menuItem);
+
+    expect(cartServiceMock.removeLastOrderItem).toHaveBeenCalled();
+    expect(loadingServiceMock.closeSpinner).toHaveBeenCalled();
+  });
+
+  it('should set errorState to true if itemOrderForm is invalid', async () => {
+    component.menuItem = { id: '1', name: 'MenuItem' } as MenuItemInfo;
+    component.routesData = { queryParams: { orderItemId: '123' } } as RoutesData;
+    itemOrderFormMock = { ...itemOrderFormMock, invalid: true };
+    component.itemOrderForm = itemOrderFormMock as FormGroup;
+    await component.onFormSubmit();
+    expect(component.errorState).toBe(true);
+  });
+
+  it('should populate menuItem correctly and call onSubmit', async () => {
+    component.menuItem = { id: '1', name: 'MenuItem', price: 10 } as MenuItemInfo;
+    component.order = { counter: 1, totalPrice: 0, optionsPrice: 0 };
+    component.routesData = { queryParams: { orderItemId: '123' } } as RoutesData;
+
+    const specialInstructionValue = 'Special instructions';
+    const optionValue = { id: 2, name: 'Option', price: 5 };
+    itemOrderFormMock = {
+      ...itemOrderFormMock,
+      value: { specialInstructions: specialInstructionValue, options: [optionValue] },
+    };
+
+    const configureMenuItemMock = jest.fn((id, counter, name, price) => {
+      return { id, counter, name, price, orderItemOptions: [] };
+    });
+    component['configureMenuItem'] = configureMenuItemMock;
+
+    const onSubmitMock = jest.fn();
+    component['onSubmit'] = onSubmitMock;
+
+    component.itemOrderForm = itemOrderFormMock as FormGroup;
+    itemOrderFormMock = { ...itemOrderFormMock, invalid: false };
+
+    await component.onFormSubmit();
+
+    expect(configureMenuItemMock).toHaveBeenCalledWith('1', 1, 'MenuItem', 10);
+    expect(configureMenuItemMock).toHaveBeenCalledWith(optionValue.id, 1, optionValue.name, optionValue.price);
+    expect(onSubmitMock).toHaveBeenCalled();
+  });
+
+  it('should not call onSubmit if specialInstructions and options are empty', async () => {
+    component.menuItem = { id: '1', name: 'MenuItem', price: 10 } as MenuItemInfo;
+    component.order = { counter: 1, totalPrice: 0, optionsPrice: 0 };
+    component.routesData = { queryParams: { orderItemId: '123' } } as RoutesData;
+    itemOrderFormMock = { ...itemOrderFormMock, value: { specialInstructions: '', options: [] } };
+    itemOrderFormMock = { ...itemOrderFormMock, invalid: true };
+    component.itemOrderForm = itemOrderFormMock as FormGroup;
+
+    const onSubmitMock = jest.fn();
+    component['onSubmit'] = onSubmitMock;
+
+    await component.onFormSubmit();
+
+    expect(onSubmitMock).not.toHaveBeenCalled();
+  });
+
+  it('should handle array option values correctly', async () => {
+    component.menuItem = { id: '1', name: 'MenuItem', price: 10 } as MenuItemInfo;
+    component.order = { counter: 1, totalPrice: 0, optionsPrice: 0 };
+    component.routesData = { queryParams: { orderItemId: '123' } } as RoutesData;
+    const optionValue = [
+      { id: '2', name: 'Option1', price: 5 },
+      { id: '3', name: 'Option2', price: 7 },
+    ];
+    itemOrderFormMock = { ...itemOrderFormMock, value: { specialInstructions: '', options: optionValue } };
+    component.itemOrderForm = itemOrderFormMock as FormGroup;
+
+    const configureMenuItemMock = jest.fn((id, counter, name, price) => {
+      return { id, counter, name, price, orderItemOptions: [] };
+    });
+    component['configureMenuItem'] = configureMenuItemMock;
+
+    await component.onFormSubmit();
+
+    expect(configureMenuItemMock).toHaveBeenCalledWith('2', 1, 'Option1', 5);
+    expect(configureMenuItemMock).toHaveBeenCalledWith('3', 1, 'Option2', 7);
+  });
+  it('should push to orderItemOptions if value and value.id exist', async () => {
+    component.menuItem = { id: '1', name: 'MenuItem', price: 10 } as MenuItemInfo;
+    component.order = { counter: 1, totalPrice: 0, optionsPrice: 0 };
+    component.routesData = { queryParams: { orderItemId: '123' } } as RoutesData;
+    const value = { id: 2, name: 'Option1', price: 5 };
+    itemOrderFormMock = { ...itemOrderFormMock, value: { specialInstructions: '', options: value } };
+    component.itemOrderForm = itemOrderFormMock as FormGroup;
+
+    const configureMenuItemMock = jest.fn((id, counter, name, price) => {
+      return { id, counter, name, price, orderItemOptions: [] };
+    });
+    component['configureMenuItem'] = configureMenuItemMock;
+
+    await component.onFormSubmit();
+
+    expect(configureMenuItemMock).toHaveBeenCalledWith(value.id, 1, value.name, value.price);
+  });
+
+  it('should handle array errors returned from validateOrder', async () => {
+    const menuItem = {};
+
+    component.routesData = {
+      queryParams: { orderItemId: '123' },
+    } as RoutesData;
+
+    const errorCode = 9006;
+    const errorMessage = 'Validation error';
+    cartServiceMock.validateOrder.mockReturnValueOnce(throwError([errorCode, errorMessage]));
+
+    await component['onSubmit'](menuItem);
+
+    expect(cartServiceMock.removeLastOrderItem).toHaveBeenCalled();
+    expect(loadingServiceMock.closeSpinner).toHaveBeenCalled();
   });
 });
