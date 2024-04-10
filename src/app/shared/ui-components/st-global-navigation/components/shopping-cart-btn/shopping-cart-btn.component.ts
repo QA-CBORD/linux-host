872a50f84f19163cf0bf2cdd03358225a22bbb1f
Neftali Oviedo <nob@cbord.com>
2024-04-10 18:32:52 -0400

    GET-5904: Setting cart values from storage and loading contentString from cart-preview

--- src/app/shared/ui-components/st-global-navigation/components/shopping-cart-btn/shopping-cart-btn.component.ts
+++ src/app/shared/ui-components/st-global-navigation/components/shopping-cart-btn/shopping-cart-btn.component.ts
@@ -1,8 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
+import { ContentStringsFacadeService } from '@core/facades/content-strings/content-strings.facade.service';
+import { LoadingService } from '@core/service/loading/loading.service';
 import { IonicModule } from '@ionic/angular';
 import { CartService } from '@sections/ordering';
-import { Observable } from 'rxjs';
+import { Observable, lastValueFrom } from 'rxjs';
+import { CONTENT_STRINGS_DOMAINS, CONTENT_STRINGS_CATEGORIES } from 'src/app/content-strings';
 
 @Component({
   standalone: true,
@@ -13,13 +16,26 @@ import { Observable } from 'rxjs';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ShoppingCartBtnComponent {
+  private readonly loadingService: LoadingService = inject(LoadingService);
+  private readonly contentStringsFacadeService: ContentStringsFacadeService = inject(ContentStringsFacadeService);
   constructor(private readonly cartService: CartService) {}
 
   get itemsCount(): Observable<number> {
     return this.cartService.menuItems$;
   }
 
-  openCart() {
+  async openCart() {
+    await this.initContentStrings();
     this.cartService.openCartpreview();
+  }
+  async initContentStrings() {
+    this.loadingService.showSpinner();
+    await lastValueFrom(
+      this.contentStringsFacadeService.fetchContentStrings$(
+        CONTENT_STRINGS_DOMAINS.patronUi,
+        CONTENT_STRINGS_CATEGORIES.ordering
+      )
+    );
+    this.loadingService.closeSpinner();
   }
 }
