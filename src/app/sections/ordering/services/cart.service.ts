409c7aef12b9caac4104c54cd593f37f0ceb8a16
AlanNunez <axn3@cbord.com>
2024-05-24 10:08:39 -0400

    GET-6027 - Fixing merge conflict

--- src/app/sections/ordering/services/cart.service.ts
+++ src/app/sections/ordering/services/cart.service.ts
@@ -558,63 +558,12 @@ export class CartService {
     this.storageStateService.deleteStateEntityByKey(this.CARTIDKEY);
     this.cartSubscription.unsubscribe();
   }
-<<<<<<< HEAD
-=======
-
-  async showActiveCartWarning(openOrderOptions: () => void) {
-    const alert = await this.alertController.create({
-      cssClass: 'active_cart',
-      header: this.translateService.instant('patron-ui.ordering.active_cart_alert_change_title'),
-      message: this.translateService.instant('patron-ui.ordering.active_cart_alert_change_msg'),
-      buttons: [
-        {
-          text: this.translateService.instant('patron-ui.ordering.active_cart_alert_change_cancel'),
-          role: 'cancel',
-          cssClass: 'button__option_cancel',
-          handler: () => {
-            alert.dismiss();
-          },
-        },
-        {
-          text: this.translateService.instant('patron-ui.ordering.active_cart_alert_change_proceed'),
-          role: 'confirm',
-          cssClass: 'button__option_confirm',
-          handler: async () => {
-            this.clearActiveOrder();
-            openOrderOptions();
-          },
-        },
-      ],
-    });
-
-    await alert.present();
-    return alert.onDidDismiss();
-  }
-
-  async preValidateOrderFlow(merchantId: string, onContinue: () => void) {
-    const hasItemsInCart = (await lastValueFrom(this.menuItems$.pipe(first()))) > 0;
-    const merchant = await lastValueFrom(this.merchant$.pipe(first()));
-    const merchantHasChanged = merchant && merchant.id !== merchantId;
-
-    if (hasItemsInCart && merchantHasChanged) {
-      this.showActiveCartWarning(onContinue);
-      return;
-    }
-
-    if (hasItemsInCart && !merchantHasChanged) {
-      this.routingService.navigate([APP_ROUTES.ordering, LOCAL_ROUTING.fullMenu], {
-        queryParams: { isExistingOrder: true },
-      });
-      return;
-    }
-    onContinue();
-  }
 
   saveOrderSnapshot() {
     if (this.cart?.order)
       this.orderSnapshot = JSON.parse(JSON.stringify(this.cart.order));
   }
-
+ 
   setOrderToSnapshot() {
     if (this.orderSnapshot) {
       this.cart.order = this.orderSnapshot;
@@ -623,7 +572,7 @@ export class CartService {
       this.removeLastOrderItem();
     }
   }
-
+ 
   resetOrderSnapshot() {
     this.orderSnapshot = null;
   }
