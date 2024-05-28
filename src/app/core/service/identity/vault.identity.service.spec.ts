ee3a226308fe24aba98ee83d8735289d7b879d72
Julio Oviedo <jbo@cbord.com>
2024-05-28 13:56:50 -0400

    GET-6074 Item unavailable popup on Active Cart

--- src/app/core/service/identity/vault.identity.service.spec.ts
+++ src/app/core/service/identity/vault.identity.service.spec.ts
@@ -4,6 +4,7 @@ import { DeviceSecurityType, VaultErrorCodes, VaultType, Device } from '@ionic-e
 import { UserPreferenceService } from '@shared/services/user-preferences/user-preference.service';
 import { VaultFactory } from './vault-factory.service';
 import { VaultIdentityService } from './vault.identity.service';
+import sinon from 'sinon';
 import {
   PinCloseStatus,
   VaultMigrateResult,
@@ -155,6 +156,7 @@ describe('VaultIdentityService', () => {
   });
 
   describe('Vault login, unlock and logout', () => {
+    afterEach(() => sinon.restore());
 
     it('should configure vault with biometrics', async () => {
       const spy = jest.spyOn(service, 'patchVaultConfig');
@@ -216,6 +218,8 @@ describe('VaultIdentityService', () => {
 
     it('should logout user when pin retry fails', async () => {
       const session = { pin: '1111', biometricUsed: true };
+      sinon.stub(service, 'closeAllModals').resolves();
+      sinon.stub(vault, 'unlock').rejects({ message: 'biometric auth failed' });
       const retryPinUnlockStub = jest.spyOn(service, 'retryPinUnlock');
       const pinModalSpy = jest
         .spyOn(service.pinAuthenticator, 'tryUnlock0')
