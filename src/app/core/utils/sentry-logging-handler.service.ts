e3032dda9d8e00db9a74d50ead31455579d6b164
Julio Oviedo <jbo@cbord.com>
2024-06-25 09:10:55 -0400

    GET-6214 Reusing Sentry logic to handle http errors

--- src/app/core/utils/sentry-logging-handler.service.ts
+++ src/app/core/utils/sentry-logging-handler.service.ts
@@ -2,6 +2,7 @@ import * as Sentry from '@sentry/angular-ivy';
 import { EnvironmentData } from '@environments/environment-data';
 import { Injectable } from '@angular/core';
 import { environment } from '@environments/environment';
+import { extractError } from './sentry-helper';
 
 const OmittedErrorsForSentry = [
   'Error: Invalid session',
@@ -9,33 +10,31 @@ const OmittedErrorsForSentry = [
   'Invalid user session',
   'Non-Error exception captured with keys found when loading the app',
   '9017,Order can not be processed for the given due time, it exceeds the merchants order capacity',
-  'There was an issue with the transaction'
+  'There was an issue with the transaction',
 ];
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SentryLoggingHandlerService {
-
-  constructor() { }
+  constructor() {}
 
   logError(error: Error): void {
-    Sentry.captureException(error);
+    const extractedError = extractError(error) || 'Handled unknown error';
+    Sentry.captureException(extractedError);
   }
 
   initProdMode(prodDsn: boolean = true): void {
     if (prodDsn) {
       if (environment.production) {
-        this.init("https://bff607c85207d1045f7e872594a3eb7d@o4505981022568448.ingest.sentry.io/4506004113457152");
+        this.init('https://bff607c85207d1045f7e872594a3eb7d@o4505981022568448.ingest.sentry.io/4506004113457152');
       }
     } else {
-      this.init("https://147d65f03f51061ffccf73dcc0aea126@o4507153434411008.ingest.us.sentry.io/4507154456772608");
+      this.init('https://147d65f03f51061ffccf73dcc0aea126@o4507153434411008.ingest.us.sentry.io/4507154456772608');
     }
   }
 
   isOmittableError(errorMsg: string): boolean {
-    return OmittedErrorsForSentry.some(err =>
-      errorMsg && (err.includes(errorMsg) || errorMsg.includes(err))
-    );
+    return OmittedErrorsForSentry.some(err => errorMsg && (err.includes(errorMsg) || errorMsg.includes(err)));
   }
 
   private init(dsn: string) {
@@ -43,7 +42,7 @@ export class SentryLoggingHandlerService {
     Sentry.init({
       dsn,
       release: 'get-mobile@' + EnvironmentData.version.versionNumber,
-      ignoreErrors: ignoreErrorsPatterns
+      ignoreErrors: ignoreErrorsPatterns,
     });
   }
 }
