3fe0f05b05f521ecfbd91a05cad7acfda36f2374
Julio Oviedo <jbo@cbord.com>
2024-07-25 09:52:59 -0400

    GET-6307 Listening to AppState and add it to Sentry context

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from '@environments/environment';
 import { SentryLoggingHandlerService } from '@core/utils/sentry-logging-handler.service';
+import { SentryAppStateListenerService } from '@core/utils/sentry-app-state-listener.service';
 
 if (environment.production) {
   enableProdMode();
@@ -11,10 +12,12 @@ if (environment.production) {
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
-  .then((moduleRef) => {
+  .then(moduleRef => {
     runInInjectionContext(moduleRef.injector, () => {
       const sentryLoggingService = inject(SentryLoggingHandlerService);
+      const sentryAppStateListenerService = inject(SentryAppStateListenerService);
       sentryLoggingService.initProdMode();
+      sentryAppStateListenerService.listenToStateChanges();
     });
   })
   .catch(err => console.error(err));
