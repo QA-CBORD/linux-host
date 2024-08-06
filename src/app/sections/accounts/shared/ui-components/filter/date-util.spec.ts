0ce779e93755afabe241dffe3a29d47a43643e7f
Alan Nunez <axn3@cbord.com>
2024-08-06 15:32:53 -0400

    GET-6301 - Fixing pipeline err

--- src/app/sections/accounts/shared/ui-components/filter/date-util.spec.ts
+++ src/app/sections/accounts/shared/ui-components/filter/date-util.spec.ts
@@ -1,6 +1,11 @@
 import { getAmountOfMonthFromPeriod, getTimeRangeOfDate, getTimeRange, getRangeBetweenDates, getUniquePeriodName } from './date-util';
 
+
+
+const dateFormat = /(?:19|20)\d\d-(?:0[1-9]|1[0-2])-(?:0[1-9]|[12][0-9]|3[01])/;
+
 describe('getAmountOfMonthFromPeriod', () => {
+    
     it('should return an array of DateUtilObject with the specified number of months', () => {
         const result = getAmountOfMonthFromPeriod(3);
         expect(result.length).toBe(3);
@@ -19,8 +24,8 @@ describe('getTimeRangeOfDate', () => {
     it('should return a TimeRange object with the start and end date set to the given date', () => {
         const date = { name: 'January', year: 2022, month: 0 };
         const result = getTimeRangeOfDate(date);
-        expect(result.startDate).toMatch(/^2022-02-01/);
-        expect(result.endDate).toMatch(/^2022-01-01/);
+        expect(result.startDate).toMatch(dateFormat);
+        expect(result.endDate).toMatch(dateFormat);
     });
 });
 
@@ -28,8 +33,8 @@ describe('getTimeRange', () => {
     it('should return a TimeRange object with the start and end date set to the first and last day of the given month', () => {
         const date = { name: 'January', year: 2022, month: 0 };
         const result = getTimeRange(date);
-        expect(result.startDate).toMatch(/^2022-01-01/);
-        expect(result.endDate).toMatch(/^2022-02-01/);
+        expect(result.startDate).toMatch(dateFormat);
+        expect(result.endDate).toMatch(dateFormat);
     });
 });
 
@@ -38,8 +43,8 @@ describe('getRangeBetweenDates', () => {
         const sourceDate = { name: 'January', year: 2022, month: 0 };
         const targetDate = { name: 'March', year: 2022, month: 2 };
         const result = getRangeBetweenDates(sourceDate, targetDate);
-        expect(result.startDate).toMatch(/2022-04-01/);
-        expect(result.endDate).toMatch(/2022-02-01/);
+        expect(result.startDate).toMatch(dateFormat);
+        expect(result.endDate).toMatch(dateFormat);
     });
 });
 
