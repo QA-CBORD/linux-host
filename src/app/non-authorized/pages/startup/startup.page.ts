be7c9fd130a4074011e3acc48c81033e6e438140
Neftali Oviedo <nob@cbord.com>
2024-08-12 14:31:30 -0400

    Deletting comment

--- src/app/non-authorized/pages/startup/startup.page.ts
+++ src/app/non-authorized/pages/startup/startup.page.ts
@@ -51,7 +51,6 @@ export class StartupPage {
   }
 
   startAuthFlow({ skipLoginFlow, biometricEnabled }) {
-    //Try case without setTIemout
     if (skipLoginFlow) {
       this.unlockVault(biometricEnabled);
     } else {
