515cb1c929c15653a58e7e5e9465ceb6a5672c44
Julio Oviedo <jbo@cbord.com>
2024-08-29 09:52:57 -0400

    Increasing coverage

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,32 +3,43 @@ import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { is } from 'date-fns/locale';
 
 describe('AppComponent', () => {
   let component: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
+  const mockTranslateService = {
+    currentLang: 'en',
+    setTranslation: jest.fn(),
+  };
+
+  const platformStub = {
+    ready: jest.fn().mockReturnValue(Promise.resolve()),
+    is: jest.fn(),
+  };
 
   beforeEach(async () => {
-    const platformStub = () => ({ ready: () => ({}), is: string => ({}) });
-    const mockTranslateService = {
-      currentLang: 'en',
-      setTranslation: jest.fn(),
-    };
-  
     await TestBed.configureTestingModule({
       imports: [TranslateModule],
       schemas: [NO_ERRORS_SCHEMA],
       declarations: [AppComponent],
       providers: [
         { provide: TranslateService, useValue: mockTranslateService },
-        { provide: Platform, useFactory: platformStub }
-      ]
+        { provide: Platform, useValue: platformStub },
+      ],
     }).compileComponents();
     fixture = TestBed.createComponent(AppComponent);
     component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it('can load instance', () => {
+    component.isBackdropShown$;
+    expect(component).toBeTruthy();
+  });
+
+  it('should custom load for Android', () => {
+    platformStub.is.mockReturnValueOnce(true);
     expect(component).toBeTruthy();
   });
 });
