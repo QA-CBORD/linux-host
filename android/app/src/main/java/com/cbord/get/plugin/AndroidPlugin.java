c9adebeae91b11b7b00bb4820ddf6ae6e648e8e4
Alan Nunez <axn3@cbord.com>
2024-08-29 14:01:38 -0400

    GET-6424 - Making Android emit events to photo upload

--- android/app/src/main/java/com/cbord/get/plugin/AndroidPlugin.java
+++ android/app/src/main/java/com/cbord/get/plugin/AndroidPlugin.java
@@ -1,17 +1,58 @@
 package com.cbord.get.plugin;
 
+import com.getcapacitor.Bridge;
 import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
+import com.getcapacitor.PluginCall;
+import com.getcapacitor.PluginHandle;
+import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
 
 @CapacitorPlugin(name = "AndroidPlugin")
 public class AndroidPlugin extends Plugin {
 
-    public void notifyListeners(JSObject response) {
-        this.notifyListeners(
-                EventType.PHOTO_UPLOAD_UPDATE.toString(),
-                response,
-                true
-        );
+    public static Bridge staticBridge = null;
+
+    public void load() {
+        staticBridge = this.bridge;
+    }
+
+    @PluginMethod
+    public void emitEvent(PluginCall call) {
+
+        System.out.println("emitEvent " +   EventType.PHOTO_UPLOAD_UPDATE.toString());
+        // Prepare the data to send
+        JSObject ret = new JSObject();
+        ret.put("message", "Hello from Java!");
+
+        // Emit the event to the JavaScript side
+        notifyListeners(EventType.PHOTO_UPLOAD_UPDATE.toString(), ret);
+
+        // Complete the call
+        call.resolve();
+    }
+
+     public static void notifyListeners(JSObject response) {
+        System.out.println("notifyListeners " +   EventType.PHOTO_UPLOAD_UPDATE.toString());
+         AndroidPlugin pushPlugin = AndroidPlugin.getAndroidPluginInstance();
+         if (pushPlugin != null) {
+             pushPlugin.notifyListeners(
+                     EventType.PHOTO_UPLOAD_UPDATE.toString(),
+                     response,
+                     true
+             );
+         }
+
+    }
+
+    public static AndroidPlugin getAndroidPluginInstance() {
+        if (staticBridge != null && staticBridge.getWebView() != null) {
+            PluginHandle handle = staticBridge.getPlugin("AndroidPlugin");
+            if (handle == null) {
+                return null;
+            }
+            return (AndroidPlugin) handle.getInstance();
+        }
+        return null;
     }
 }
