6fbab133ae2dfae9cdd5d6a465c8d6de66f668c0
Alan Nunez <axn3@cbord.com>
2024-08-30 08:03:06 -0400

    GET-6424 - Removing explicit local notification

--- android/app/src/main/java/com/cbord/get/MainActivity.java
+++ android/app/src/main/java/com/cbord/get/MainActivity.java
@@ -1,27 +1,15 @@
 package com.cbord.get;
-import static com.cbord.get.plugin.EventType.PHOTO_UPLOAD_UPDATE;
 
 import android.os.Bundle;
-import android.os.Handler;
 
 import com.cbord.get.mcredential.MobileCredentialStatusPlugin;
-import com.cbord.get.notification.ConfigurationFactory;
-import com.cbord.get.notification.LocalNotification;
-import com.cbord.get.notification.RemoteNotification;
 import com.cbord.get.plugin.AndroidPermissionsPlugin;
 import com.cbord.get.plugin.AndroidPlugin;
 import com.cbord.get.plugin.GooglePayPlugin;
 import com.cbord.get.plugin.HIDPlugin;
 import com.getcapacitor.BridgeActivity;
-import com.getcapacitor.JSObject;
-import com.google.firebase.messaging.RemoteMessage;
 
 public class MainActivity extends BridgeActivity {
-
-    private Handler handler;
-    private Runnable runnable;
-    private final int DELAY = 5000; // 5000 milliseconds = 5 seconds
-    private AndroidPlugin notifier = new AndroidPlugin();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,33 +19,5 @@ public class MainActivity extends BridgeActivity {
         registerPlugin(AndroidPermissionsPlugin.class);
         registerPlugin(AndroidPlugin.class);
         super.onCreate(savedInstanceState);
-
-        LocalNotification.createNotificationChannel(this);
-
-        handler = new Handler();
-        runnable = new Runnable() {
-            @Override
-            public void run() { //"category": "PHOTO_UPLOAD_UPDATE"
-                System.out.println("running");
-                JSObject response = new JSObject();
-                response.put("title", "test" );
-                response.put("status", "ACCEPTED" );
-                response.put("userId", "7053d917-4e32-4981-874b-a9396205e187" );
-               // response.put("category", "PHOTO_UPLOAD_UPDATE" );
-                notifier.notifyListeners(response);
-                // Schedule the task to run again after DELAY milliseconds
-                handler.postDelayed(this, DELAY);
-            }
-        };
-
-        // Start the initial execution of the task
-        handler.postDelayed(runnable, DELAY);
-   }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        // Remove callbacks to prevent memory leaks
-        handler.removeCallbacks(runnable);
     }
 }
--- android/app/src/main/java/com/cbord/get/notification/RemoteNotification.java
+++ android/app/src/main/java/com/cbord/get/notification/RemoteNotification.java
@@ -1,22 +1,14 @@
 package com.cbord.get.notification;
 
+import android.net.Uri;
 
 import androidx.annotation.NonNull;
 
+import com.capacitorjs.plugins.pushnotifications.PushNotificationsPlugin;
 import com.cbord.get.plugin.AndroidPlugin;
-import com.cbord.get.plugin.EventType;
-import com.getcapacitor.Bridge;
-import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.JSObject;
-import com.getcapacitor.Plugin;
-import com.getcapacitor.PluginCall;
-import com.getcapacitor.PluginMethod;
-import com.getcapacitor.annotation.CapacitorPlugin;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
-
-import java.util.HashMap;
-import java.util.Map;
 
  public class RemoteNotification extends FirebaseMessagingService {
 
@@ -25,8 +17,33 @@ import java.util.Map;
         super.onMessageReceived(remoteMessage);
 
         System.out.println("remoteMessage = " + remoteMessage);
-        JSObject response = ConfigurationFactory.createConfiguration(remoteMessage);
-        AndroidPlugin.notifyListeners(response);
+
+       JSObject response = ConfigurationFactory.createConfiguration(remoteMessage);
+       if (response != null) {
+           AndroidPlugin.notifyAndroidListeners(response);
+       }
+
+        JSObject remoteMessageData = new JSObject();
+        remoteMessageData.put("id", remoteMessage.getMessageId());
+        JSObject data = new JSObject();
+        for (String key : remoteMessage.getData().keySet()) {
+            Object value = remoteMessage.getData().get(key);
+            data.put(key, value);
+        }
+        remoteMessageData.put("data", data);
+        RemoteMessage.Notification notification = remoteMessage.getNotification();
+        if(notification != null) {
+            String title = notification.getTitle();
+            String body = notification.getBody();
+            remoteMessageData.put("title", title);
+            remoteMessageData.put("body", body);
+            remoteMessageData.put("click_action", notification.getClickAction());
+            Uri link = notification.getLink();
+            if (link != null) {
+                remoteMessageData.put("link", link.toString());
+            }
+        }
+
+        AndroidPlugin.notifyPushListeners(remoteMessageData);
     }
 }
-
