d20e52490a172c03feba8a7e2a2d4058fe15e7cd
Alan Nunez <axn3@cbord.com>
2024-09-16 11:47:11 -0400

    GET-6232 - signKeystore script to include signing process

--- SignKeystore.sh
+++ SignKeystore.sh
@@ -5,6 +5,8 @@ key_alias=provision_app_keyAlias
 store_password=provision_app_storePassword
 key_password=provision_app_keyPassword
 
+echo "Signing keystore"
+
 sed -E -i '' -e "s/${key_alias}=/${key_alias}=${KEY_ALIAS}/" \
              -e "s/${store_password}=/${store_password}=${KEYSTORE_PASSWORD}/" \
              -e "s/${key_password}=/${key_password}=${KEY_PASSWORD}/" \
