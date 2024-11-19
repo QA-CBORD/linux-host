b1d7c24319d74310d97eaa7964f75b811fda26d0
Alan Nunez <axn3@cbord.com>
2024-11-19 16:45:14 -0400

    Refactor GitHub Actions workflow to update the name of the UI test configuration

--- run-automation.sh
+++ run-automation.sh
@@ -2,8 +2,25 @@
 
 if [ -z "$1" ]
   then
-    echo "You must pass the suite name"
-    exit
-fi
+    # File containing the WebdriverIO configuration
+    INPUT_FILE="./tests/config/wdio.shared.config.ts"
 
-npm run ionic-e2e:build:android && SUITE=$1 npm run ionic-e2e:run:android
+  # Check if the input file exists
+  if [[ ! -f "$INPUT_FILE" ]]; then
+    echo "Error: File '$INPUT_FILE' not found."
+    exit 1
+  fi
+
+  # Extract suite names
+  echo "Extracting suite names from $INPUT_FILE..."
+  suites=$(grep -oP '(?!specs|capabilities|services|reporters)\b\w*?(?=[:]\s\[)' "$INPUT_FILE") 
+  echo "$suites" 
+
+  npm run ionic-e2e:build:android
+  for suite in $suites; do
+     SUITE=$suite npm run ionic-e2e:run:android
+  done
+else
+    echo "Running suite: $1"
+    npm run ionic-e2e:build:android && SUITE=$1 npm run ionic-e2e:run:android
+fi
