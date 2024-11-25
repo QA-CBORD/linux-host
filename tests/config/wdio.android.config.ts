1468092154e1261c5c6fe4749d545a44e228faac
Alan Nunez <axn3@cbord.com>
2024-11-25 14:48:20 -0400

    Refactor emulator configuration: Adjust memory settings and enhance Appium capabilities for improved testing

--- tests/config/wdio.android.config.ts
+++ tests/config/wdio.android.config.ts
@@ -14,15 +14,50 @@ config.capabilities = [
     port: 4723,
     path: '/',
     platformName: 'Android',
+    maxInstances: 2,
+    // For W3C the appium capabilities need to have an extension prefix
+    // http://appium.io/docs/en/writing-running-appium/caps/
+    // This is `appium:` for all Appium Capabilities which can be found here
+    'appium:deviceName': 'emulator-5554',
+    'appium:platformVersion': '13',
+    'appium:orientation': 'PORTRAIT',
+    'appium:automationName': 'UiAutomator2',
+    "appium:udid": "emulator-5554",
+    // The path to the app
+    'appium:app': 'android/app/build/outputs/apk/debug/app-debug.apk',
+    'appium:appWaitActivity': 'com.cbord.get.MainActivity',
+    'appium:newCommandTimeout': 240,
+    // This will automatically start the iOS app in a webview context,
+    // if your app starts in a native context then please put this to false and handle your own
+    // context switching
+    'appium:autoWebview': true,
+    // Read the reset strategies very well, they differ per platform, see
+    // http://appium.io/docs/en/writing-running-appium/other/reset-strategies/
+    // When enabling the noReset the App will NOT be re-installed between sessions
+    // This means that every test needs to maintain it's own state
+    // `"appium:noReset":false` means that the app is removed and installed
+    // between each test
+    'appium:noReset': true,
+    // This will prevent appium to restart the app between the sessions,
+    // meaning between each spec file
+    // @ts-ignore
+    'appium:dontStopAppOnReset': true,
+  },
+  {
+    // The defaults you need to have in your config
+    hostname: '127.0.0.1',
+    port: 4724,
+    path: '/',
+    platformName: 'Android',
     maxInstances: 1,
     // For W3C the appium capabilities need to have an extension prefix
     // http://appium.io/docs/en/writing-running-appium/caps/
     // This is `appium:` for all Appium Capabilities which can be found here
-    'appium:deviceName': 'Pixel_5_API_30',
+    'appium:deviceName': 'emulator-5555',
     'appium:platformVersion': '13',
     'appium:orientation': 'PORTRAIT',
     'appium:automationName': 'UiAutomator2',
-    //"appium:udid": "d278263c",
+    "appium:udid": "emulator-5555",
     // The path to the app
     'appium:app': 'android/app/build/outputs/apk/debug/app-debug.apk',
     'appium:appWaitActivity': 'com.cbord.get.MainActivity',
